ca6b4081d3a3ffba2f3edf0f70d057c32c300b17
David Anguita <david@davidanguita.name>
2016-11-09 08:54:35 +0100

    Enhance Admin user decorations based on authorization levels

---
+++ app/helpers/admin/application_helper.rb
@@ -0,0 +1,13 @@
+module Admin::ApplicationHelper
+  def current_admin_label
+    return unless current_admin
+
+    admin_label_for(current_admin)
+  end
+
+  def admin_label_for(admin)
+    return admin.name if admin.regular?
+
+    "#{admin.name} (#{admin.authorization_level})"
+  end
+end

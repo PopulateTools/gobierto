780cc73da82b69d61f775936d12afa65b934e4ba
David Anguita <david@davidanguita.name>
2016-11-14 07:47:04 +0100

    Revert visibility check at site constraint level

--- lib/constraints/gobierto_site_constraint.rb
+++ lib/constraints/gobierto_site_constraint.rb
@@ -5,8 +5,7 @@ class GobiertoSiteConstraint
   def matches?(request)
     full_domain = (request.env['HTTP_HOST'] || request.env['SERVER_NAME'] || request.env['SERVER_ADDR']).split(':').first
 
-    # FIXME. This is a quick and dirty way of hiding draft Sites
-    if site = Site.active.find_by(domain: full_domain)
+    if site = Site.find_by(domain: full_domain)
       request.env['gobierto_site'] = site
       return true
     end

9dee031ee6b1cc0a18d982a2e4ce628884697ae9
David Anguita <david@davidanguita.name>
2016-11-15 10:14:10 +0100

    Update schema version by running pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114161441) do
+ActiveRecord::Schema.define(version: 20161114174131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

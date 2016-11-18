aa39b346cbcf70a98063dc13d16157eaf1042f7a
David Anguita <david@davidanguita.name>
2016-11-18 17:18:06 +0100

    Implement Admin::LayoutPolicy object and its starting behavior
    - Integrate it to determine which layout sections are being rendered
      on each request.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,14 @@
 class Admin::BaseController < ApplicationController
   include Admin::SessionHelper
   include Admin::SiteSessionHelper
+  include Admin::LayoutPolicyHelper
 
   before_action :authenticate_admin!
 
   helper_method :current_admin, :admin_signed_in?
   helper_method :current_site, :managing_site?
   helper_method :managed_sites
+  helper_method :can_manage_sites?
 
   layout "admin/application"
 end
---
+++ app/policies/admin/layout_policy.rb
@@ -0,0 +1,21 @@
+class Admin::LayoutPolicy
+  attr_reader :admin, :action
+
+  def initialize(admin, action)
+    @admin = admin
+    @action = action
+  end
+
+  def can?
+    case action
+    when :manage_sites then manage?
+    else false
+    end
+  end
+
+  private
+
+  def manage?
+    admin.managing_user?
+  end
+end

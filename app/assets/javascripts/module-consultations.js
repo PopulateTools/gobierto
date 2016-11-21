1207b10cda57387ae48077bbd441d353d7a02d79
Alvaro Ortiz <alvaro@furilo.com>
2016-11-21 17:35:58 +0100

    HTML for front consultations, user login, user confirmation...

---
+++ app/assets/javascripts/module-consultations.js
@@ -0,0 +1,25 @@
+$(document).on('turbolinks:load', function() {
+
+	function deficitEnter() {
+		// e.preventDefault();
+		$('.debt_marker').addClass('debt_warning');	
+		$('.debt_marker .qty')
+			.velocity({backgroundColor: '#AC3E3E'})
+			.velocity("callout.flash");
+		$('.warning_text').velocity("transition.slideDownBigIn");
+	}
+	function deficitExit() {
+		$('.debt_marker').removeClass('debt_warning');	
+		$('.debt_marker .qty').velocity({backgroundColor: '#F0F0F0'});
+		$('.warning_text').velocity("transition.slideUpBigOut");
+	}
+	$('.consultation_marker .augment').click(function(e) {
+		// e.preventDefault();
+		deficitEnter();
+	});
+	$('.consultation_marker .reduce').click(function(e) {
+		e.preventDefault();
+		deficitExit();
+	});
+
+});

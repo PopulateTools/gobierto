e803e09ce229f73b20bd97f35892c27877e12c2c
David Anguita <david@davidanguita.name>
2016-12-07 16:53:18 +0100

    Implement `GobiertoCommon::Sortable` behavior and integrate it

---
+++ test/support/concerns/gobierto_common/sortable_test.rb
@@ -0,0 +1,11 @@
+module GobiertoCommon::SortableTest
+  def test_update_positions
+    positions_from_params = { "0" => { "id" => sortable_object.id, "position" => 1 } }
+
+    assert subject_class.update_positions(positions_from_params)
+  end
+
+  def test_reset_position!
+    assert subject_class.reset_position!
+  end
+end

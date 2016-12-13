0bf9459517ea56c09993ee8a64e34f668588f126
David Anguita <david@davidanguita.name>
2016-12-13 12:21:12 +0100

    Skip redundant assertion in `has_message?` matcher helper

--- test/support/integration/matcher_helpers.rb
+++ test/support/integration/matcher_helpers.rb
@@ -2,7 +2,7 @@ module Integration
   module MatcherHelpers
     def has_message?(text)
       within ".flash-message" do
-        assert has_content?(text)
+        has_content?(text)
       end
     end
   end

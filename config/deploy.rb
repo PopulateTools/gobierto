7ec33b27f1d9b44401403ec1899ef1087f2b73a3
Fernando Blat <ferblape@gmail.com>
2016-12-27 10:04:58 +0100

    Update repository URL

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock '3.6.1'
 # Sidekiq known bug - https://github.com/seuros/capistrano-sidekiq#usage
 set :pty,  false
 set :application, 'gobierto'
-set :repo_url, 'git@github.com:PopulateTools/gobierto-dev.git'
+set :repo_url, 'git@github.com:PopulateTools/gobierto.git'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', '.rbenv-vars', "config/settings/#{fetch(:stage)}.yml")
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/cache')
 set :rbenv_type, :user

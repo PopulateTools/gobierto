465c7478a3f9632bb586d11a6a7aae3213032668
David Anguita <david@davidanguita.name>
2016-12-29 20:20:39 +0100

    Manage People’s `avatar_url` and `bio_url` file uploading

--- test/integration/gobierto_admin/gobierto_people/person_create_test.rb
+++ test/integration/gobierto_admin/gobierto_people/person_create_test.rb
@@ -1,10 +1,12 @@
 require "test_helper"
 require "support/integration/dynamic_content_helpers"
+require "support/file_uploader_helpers"
 
 module GobiertoAdmin
   module GobiertoPeople
     class PersonCreateTest < ActionDispatch::IntegrationTest
       include Integration::DynamicContentHelpers
+      include FileUploaderHelpers
 
       def setup
         super
@@ -29,10 +31,19 @@ module GobiertoAdmin
             visit @path
 
             within "form.new_person" do
+              within ".avatar_file_field" do
+                refute has_selector?("img")
+                attach_file "person_avatar_file", "test/fixtures/files/gobierto_people/people/avatar.jpg"
+              end
+
               fill_in "person_name", with: "Person Name"
               fill_in "person_charge", with: "Person Charge"
               fill_in "person_bio", with: "Person Bio"
-              fill_in "person_bio_url", with: "Person Bio URL"
+
+              within ".bio_file_field" do
+                refute has_selector?("a")
+                attach_file "person_bio_file", "test/fixtures/files/gobierto_people/people/bio.pdf"
+              end
 
               within ".person-visibility-level-radio-buttons" do
                 choose "Active"
@@ -40,13 +51,33 @@ module GobiertoAdmin
 
               fill_in_content_blocks
 
-              click_button "Create Person"
+              with_stubbed_s3_file_upload do
+                click_button "Create Person"
+              end
             end
 
             assert has_message?("Person was successfully created")
             assert has_selector?("h1", text: "Person Name")
 
-            assert_content_blocks_have_the_right_values
+            within "form.edit_person" do
+              within ".avatar_file_field" do
+                assert has_selector?("img")
+              end
+
+              assert has_field?("person_name", with: "Person Name")
+              assert has_field?("person_charge", with: "Person Charge")
+              assert has_field?("person_bio", with: "Person Bio")
+
+              within ".bio_file_field" do
+                assert has_selector?("a")
+              end
+
+              within ".person-visibility-level-radio-buttons" do
+                assert has_checked_field?("Active")
+              end
+
+              assert_content_blocks_have_the_right_values
+            end
           end
         end
       end
--- test/integration/gobierto_admin/gobierto_people/person_update_test.rb
+++ test/integration/gobierto_admin/gobierto_people/person_update_test.rb
@@ -1,10 +1,12 @@
 require "test_helper"
 require "support/integration/dynamic_content_helpers"
+require "support/file_uploader_helpers"
 
 module GobiertoAdmin
   module GobiertoPeople
     class PersonUpdateTest < ActionDispatch::IntegrationTest
       include Integration::DynamicContentHelpers
+      include FileUploaderHelpers
 
       def setup
         super
@@ -30,10 +32,17 @@ module GobiertoAdmin
             visit @path
 
             within "form.edit_person" do
+              within ".avatar_file_field" do
+                attach_file "person_avatar_file", "test/fixtures/files/gobierto_people/people/avatar.jpg"
+              end
+
               fill_in "person_name", with: "Person Name"
               fill_in "person_charge", with: "Person Charge"
               fill_in "person_bio", with: "Person Bio"
-              fill_in "person_bio_url", with: "Person Bio URL"
+
+              within ".bio_file_field" do
+                attach_file "person_bio_file", "test/fixtures/files/gobierto_people/people/bio.pdf"
+              end
 
               within ".person-visibility-level-radio-buttons" do
                 choose "Draft"
@@ -41,16 +50,25 @@ module GobiertoAdmin
 
               fill_in_content_blocks
 
-              click_button "Update Person"
+              with_stubbed_s3_file_upload do
+                click_button "Update Person"
+              end
             end
 
             assert has_message?("Person was successfully updated")
 
             within "form.edit_person" do
+              within ".avatar_file_field" do
+                assert has_selector?("img")
+              end
+
               assert has_field?("person_name", with: "Person Name")
               assert has_field?("person_charge", with: "Person Charge")
               assert has_field?("person_bio", with: "Person Bio")
-              assert has_field?("person_bio_url", with: "Person Bio URL")
+
+              within ".bio_file_field" do
+                assert has_selector?("a")
+              end
 
               within ".person-visibility-level-radio-buttons" do
                 assert has_checked_field?("Draft")

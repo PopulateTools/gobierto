56ce05d40d01f3960e285bd745c8ae7a5723c0d7
Martín González <m@martingonzalez.net>
2017-01-02 13:56:00 +0100

    Implement cards percentage variations

--- app/assets/javascripts/indicators.js
+++ app/assets/javascripts/indicators.js
@@ -21,19 +21,40 @@ $(document).on('turbolinks:load', function() {
     d3.selectAll('.indicator_widget').each(function(d, i) {
       var dataSlug = d3.select(this).attr('data-slug');
       var div = d3.select(this);
-      var url = 'https://tbi.populate.tools/gobierto/datasets/' + dataSlug + '.json?include=municipality&sort_desc_by=date&filter_by_location_id=' + cityId;
+      var url = 'https://tbi.populate.tools/gobierto/datasets/' + dataSlug + '.json?sort_desc_by=date&filter_by_location_id=' + cityId;
       
       d3.json(url)
         .header('authorization', 'Bearer ' + window.tbiToken)
         .get(function(error, data) {
           if (error) throw error;
-          
+
           // Paint the figure
           div.select('.widget_figure')
             .datum(data)
-            .text(function(d) { console.log(d); return accounting.formatNumber(d[0].value, 0) });
+            .text(function(d) { return accounting.formatNumber(d[0].value, 0) });
+          
+          // Only calculated if we want to show a pct variation in the indicator
+          if ($('.widget_pct').length > 0) {
+            var pctChange = (data[0].value / data[1].value * 100) - 100;
+            var pctFormat = accounting.formatNumber(pctChange, 1);
+            
+            // If is a positive change, attach a plus sign to the number
+            div.select('.widget_pct')
+              .datum(data)
+              .text(function() {
+                return pctChange > 0 ? '+' + pctFormat : pctFormat;
+              });
+            
+            // Return the correct icon
+            div.select('.widget_pct')
+              .append('i')
+              .attr('aria-hidden', 'true')
+              .attr('class', function() {
+                return pctChange > 0 ? 'fa fa-caret-up' : 'fa fa-caret-down';
+              });
+          }
         });
-    });    
+    });
   }
   
 });

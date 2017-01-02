ef29bff88f060c2433be3aa4027d1466b8d9b4c9
Martín González <m@martingonzalez.net>
2017-01-02 19:35:27 +0100

    Populate the indicator cards and handle different data types

--- app/assets/javascripts/vis_card_indicators.js
+++ app/assets/javascripts/vis_card_indicators.js
@@ -5,30 +5,41 @@ var CardIndicators = Class.extend({
     d3.selectAll(divClass).each(function(d) {
       var div = d3.select(this);
       var dataSlug = d3.select(this).attr('data-slug');
+      var dataType = d3.select(this).attr('data-type');
       var url = 'https://tbi.populate.tools/gobierto/datasets/' + dataSlug + '.json?sort_desc_by=date&filter_by_location_id=' + city_id;
       
       d3.json(url)
         .header('authorization', 'Bearer ' + window.tbiToken)
         .get(function(error, data) {
           if (error) throw error;
-
+          
           // Paint the figure
           div.select('.widget_figure')
             .datum(data)
-            .text(function(d) { return accounting.formatNumber(d[0].value, 0) });
+            .text(function(d) {
+              // Switch between different figure types
+              switch (dataType) {
+                case 'percentage':
+                  return accounting.formatNumber(d[0].value, 1) + '%';
+                  break;
+                case 'currency':
+                  return accounting.formatNumber(d[0].value, 0) + '€';
+                  break;
+                default:
+                  return accounting.formatNumber(d[0].value, 0);
+              }
+            });
           
           // Only calculated if we want to show a pct variation in the indicator
           if ($('.widget_pct').length > 0) {
-            var pctChange = (data[0].value / data[1].value * 100) - 100;
-            var pctFormat = accounting.formatNumber(pctChange, 1);
             var isPositive = pctChange > 0;
+            var pctChange = (data[0].value / data[1].value * 100) - 100;
+            var pctFormat = accounting.formatNumber(pctChange, 1) + '%';
             
             // If is a positive change, attach a plus sign to the number
             div.select('.widget_pct')
               .datum(data)
-              .text(function() {
-                return isPositive ? '+' + pctFormat : pctFormat;
-              });
+              .text(function() { return isPositive ? '+' + pctFormat : pctFormat; });
             
             // Return the correct icon
             div.select('.widget_pct')

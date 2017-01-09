26269a60c453d4a0f5c156c389e3d886ea51ddcd
Martín González <m@martingonzalez.net>
2017-01-09 17:36:38 +0100

    Remove Spanish locale, as its global now

--- app/assets/javascripts/vis_card_indicators.js
+++ app/assets/javascripts/vis_card_indicators.js
@@ -2,21 +2,6 @@
 
 var CardIndicators = Class.extend({
   init: function(divClass, city_id) {
-    var es_ES = {
-      "decimal": ",",
-      "thousands": ".",
-      "grouping": [3],
-      "currency": ["€", ""],
-      "dateTime": "%a %b %e %X %Y",
-      "date": "%d/%m/%Y",
-      "time": "%H:%M:%S",
-      "periods": ["AM", "PM"],
-      "days": ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
-      "shortDays": ["Dom", "Lun", "Mar", "Mi", "Jue", "Vie", "Sab"],
-      "months": ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-      "shortMonths": ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]
-    };
-    
     // Set formats
     d3.timeFormatDefaultLocale(es_ES);
     var parseDate = d3.timeParse("%Y-%m-%d");

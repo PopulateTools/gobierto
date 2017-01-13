20bfc5aa596ba19a729199244a7f49c64316ed5e
Fernando Blat <ferblape@gmail.com>
2017-01-13 18:17:12 +0100

    Fix typo

--- lib/populate_data/gobierto/client.rb
+++ lib/populate_data/gobierto/client.rb
@@ -7,7 +7,7 @@ module PopulateData
     class Client
       include Logging
 
-      BASE_URI = APP_CONFIG["populate_data"]["endpoint"] %>
+      BASE_URI = APP_CONFIG["populate_data"]["endpoint"]
 
       def initialize(options = {})
         Client.logger.debug("Initializing #{self.class.name} with options #{options}")

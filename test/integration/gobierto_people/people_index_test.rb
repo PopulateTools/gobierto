7cfe342ac07cbf631c337fc919c373b0a1cec59f
David Anguita <david@davidanguita.name>
2017-01-17 12:25:46 +0100

    People's index page catch-up and test coverage

--- test/integration/gobierto_people/people_index_test.rb
+++ test/integration/gobierto_people/people_index_test.rb
@@ -11,12 +11,67 @@ module GobiertoPeople
       @site ||= sites(:madrid)
     end
 
+    def people
+      @people ||= [
+        gobierto_people_people(:richard),
+        gobierto_people_people(:tamara)
+      ]
+    end
+
+    def political_groups
+      @political_groups ||= [
+        gobierto_people_political_groups(:marvel),
+        gobierto_people_political_groups(:dc)
+      ]
+    end
+
     def test_people_index
       with_current_site(site) do
         visit @path
 
         assert has_selector?("h1", text: "#{site.name}'s organization chart")
-        assert has_selector?(".people-summary")
+      end
+    end
+
+    def test_people_filter
+      with_current_site(site) do
+        visit @path
+
+        within ".people-filter" do
+          assert has_link?("Government")
+          assert has_link?("Opposition")
+          assert has_link?("Executive")
+          assert has_link?("All")
+
+          assert has_content?("Political groups")
+
+          political_groups.each do |political_group|
+            assert has_link?(political_group.name)
+          end
+        end
+      end
+    end
+
+    def test_people_summary
+      with_current_site(site) do
+        visit @path
+
+        within ".people-summary" do
+          people.each do |person|
+            assert has_selector?(".person-item", text: person.name)
+            assert has_link?(person.name)
+          end
+        end
+      end
+    end
+
+    def test_subscription_block
+      with_current_site(site) do
+        visit @path
+
+        within ".subscribable-box" do
+          assert has_button?("Subscribe")
+        end
       end
     end
   end

afb3a2e1807a5f32b76fcec5e4dfb3908e120a5c
David Anguita <david@davidanguita.name>
2017-01-17 12:39:10 +0100

    People Event’s index page catch-up and test coverage

--- test/integration/gobierto_people/person_events_index_test.rb
+++ test/integration/gobierto_people/person_events_index_test.rb
@@ -11,13 +11,102 @@ module GobiertoPeople
       @site ||= sites(:madrid)
     end
 
+    def upcoming_events
+      @upcoming_events ||= [
+        gobierto_people_person_events(:richard_published)
+      ]
+    end
+
+    def upcoming_event
+      @upcoming_event ||= upcoming_events.first
+    end
+
+    def people
+      @people ||= [
+        gobierto_people_people(:richard),
+        gobierto_people_people(:tamara)
+      ]
+    end
+
+    def political_groups
+      @political_groups ||= [
+        gobierto_people_political_groups(:marvel),
+        gobierto_people_political_groups(:dc)
+      ]
+    end
+
     def test_person_events_index
       with_current_site(site) do
         visit @path
 
-        assert has_selector?("h3", text: "Upcoming events")
-        assert has_selector?("h3", text: "Past events")
-        assert has_selector?(".events-summary")
+        assert has_selector?("h1", text: "#{site.name}'s member agendas")
+      end
+    end
+
+    def test_person_events_filter
+      with_current_site(site) do
+        visit @path
+
+        within ".person-events-filter" do
+          assert has_link?("Government")
+          assert has_link?("Opposition")
+          assert has_link?("Executive")
+          assert has_link?("All")
+
+          assert has_content?("Political groups")
+
+          political_groups.each do |political_group|
+            assert has_link?(political_group.name)
+          end
+        end
+      end
+    end
+
+    def test_events_summary
+      with_current_site(site) do
+        visit @path
+
+        within ".events-summary" do
+          assert has_content?("Upcoming events")
+          assert has_link?("Past events")
+
+          upcoming_events.each do |event|
+            assert has_selector?(".person_event-item", text: event.title)
+            assert has_link?(event.title)
+          end
+        end
+      end
+    end
+
+    def test_calendar_component
+      with_current_site(site) do
+        visit gobierto_people_events_path(start_date: upcoming_event.starts_at)
+
+        within ".calendar-component" do
+          assert has_link?(upcoming_event.starts_at.day)
+        end
+      end
+    end
+
+    def test_agenda_switcher
+      with_current_site(site) do
+        visit @path
+
+        within ".agenda-switcher" do
+          people.each do |person|
+            assert has_link?(person.name)
+          end
+        end
+      end
+    end
+
+    def test_subscription_block
+      with_current_site(site) do
+        visit @path
+
+        within ".subscribable-box" do
+          assert has_button?("Subscribe")
+        end
       end
     end
   end

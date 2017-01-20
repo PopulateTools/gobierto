796a315c1afeb9ac157f0b285dae16de2c55ce2f
David Anguita <david@davidanguita.name>
2017-01-20 17:47:37 +0100

    Replace any “View item” link placeholders regarding Person resources

---
+++ test/integration/gobierto_admin/gobierto_people/people_index_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+module GobiertoAdmin
+  module GobiertoPeople
+    class PeopleIndexTest < ActionDispatch::IntegrationTest
+      def setup
+        super
+        @path = admin_people_people_path
+      end
+
+      def admin
+        @admin ||= gobierto_admin_admins(:nick)
+      end
+
+      def site
+        @site ||= sites(:madrid)
+      end
+
+      def people
+        @people ||= ::GobiertoPeople::Person.all
+      end
+
+      def test_people_index
+        with_signed_in_admin(admin) do
+          with_current_site(site) do
+            visit @path
+
+            within "table.people-list tbody" do
+              assert has_selector?("tr", count: people.size)
+
+              people.each do |person|
+                assert has_selector?("tr#person-item-#{person.id}")
+
+                within "tr#person-item-#{person.id}" do
+                  if person.active?
+                    assert has_link?("View person")
+                  else
+                    assert has_selector?(".view_item", text: "View person")
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/integration/gobierto_admin/gobierto_people/person_posts_index_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+module GobiertoAdmin
+  module GobiertoPeople
+    class PersonPostsIndexTest < ActionDispatch::IntegrationTest
+      def setup
+        super
+        @path = admin_people_person_posts_path(person)
+      end
+
+      def admin
+        @admin ||= gobierto_admin_admins(:nick)
+      end
+
+      def site
+        @site ||= sites(:madrid)
+      end
+
+      def person
+        @person ||= gobierto_people_people(:richard)
+      end
+
+      def test_person_posts_index
+        with_signed_in_admin(admin) do
+          with_current_site(site) do
+            visit @path
+
+            within "table.person-posts-list tbody" do
+              assert has_selector?("tr", count: person.posts.count)
+
+              person.posts.each do |person_post|
+                assert has_selector?("tr#person-post-item-#{person_post.id}")
+
+                within "tr#person-post-item-#{person_post.id}" do
+                  if person_post.active?
+                    assert has_link?("View post")
+                  else
+                    assert has_selector?(".view_item", text: "View post")
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end

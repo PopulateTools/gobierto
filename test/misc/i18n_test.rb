9900009383cf1a242060095cff2a123ea4e362e0
Fernando Blat <ferblape@gmail.com>
2017-01-23 13:26:42 +0100

    Add I18n tests

---
+++ test/misc/i18n_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+require "i18n/tasks"
+
+module Misc
+  class I18nTest < ActiveSupport::TestCase
+    def i18n
+      @i18n ||= I18n::Tasks::BaseTask.new
+    end
+
+    def missing_keys
+      i18n.missing_keys
+    end
+
+    def unused_keys
+      i18n.unused_keys
+    end
+
+    def test_no_missing_keys
+      assert_empty missing_keys, "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
+    end
+
+    def test_no_unused_keys
+      assert_empty unused_keys, "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+    end
+  end
+end

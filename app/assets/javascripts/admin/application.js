dad170f3e3ceda4358b37ce1a98a94b77dfa4974
Fernando Blat <ferblape@gmail.com>
2017-01-31 11:01:58 +0100

    Create events with a default beginning date and time

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -8,12 +8,36 @@
 
 $(document).on('turbolinks:load', function() {
 
-  // Datepicker
-  $('.air-datepicker').datepicker({
-    autoClose: true,
-    onSelect: function onSelect(_, _, instance) {
-      $(instance.el).trigger("datepicker-change");
-    }
-  });
+  if($('.air-datepicker').length){
+    var start = new Date();
+    start.setDate(start.getDate() + 1);
+    start.setHours(start.getHours() + 1);
+    start.setMinutes(0);
+
+    var $fromDatePicker = $('.air-datepicker:eq(0)');
+    var $toDatePicker = $('.air-datepicker:eq(1)');
+
+    // Datepicker start time
+    $fromDatePicker.datepicker({
+      autoClose: true,
+      startDate: start,
+      onSelect: function onSelect(_, selectedDate, instance) {
+        $(instance.el).trigger("datepicker-change");
+        selectedDate.setHours(selectedDate.getHours() + 1);
+        $toDatePicker.data('datepicker').selectDate(selectedDate);
+      }
+    });
+    // Datepicker end time
+    $toDatePicker.datepicker({
+      autoClose: true,
+      startDate: start,
+      onSelect: function onSelect(_, _, instance) {
+        $(instance.el).trigger("datepicker-change");
+      }
+    });
+
+    var date = $fromDatePicker.data('datepicker').date
+    $fromDatePicker.data('datepicker').selectDate(date);
+  }
 
 });

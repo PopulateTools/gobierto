3675ba164d40d6eec4bad23fbb59dd2c309dce07
Alvaro Ortiz <alvaro@furilo.com>
2017-02-03 17:10:08 +0100

    Styles for statements

--- app/assets/javascripts/gobierto_people/application.js
+++ app/assets/javascripts/gobierto_people/application.js
@@ -1,2 +1,11 @@
 //= require ./app/init
 //= require_tree ./app
+
+$(document).on('turbolinks:load', function() {
+
+  $('.open-other-statements').click(function(e) {
+    e.preventDefault();
+    $('.other-statements').toggle();
+  }); 
+
+});

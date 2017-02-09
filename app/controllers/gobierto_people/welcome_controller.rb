1cb54494890d12dae73775bab878d5e72e573922
Fernando Blat <ferblape@gmail.com>
2017-02-09 19:10:17 +0100

    Default filter is government

--- app/controllers/gobierto_people/welcome_controller.rb
+++ app/controllers/gobierto_people/welcome_controller.rb
@@ -3,7 +3,7 @@ module GobiertoPeople
     include PoliticalGroupsHelper
 
     def index
-      @people = current_site.people.active.sorted.last(10)
+      @people = current_site.people.active.politician.government.last(10)
       @events = current_site.person_events.upcoming.sorted.first(10)
       @posts  = current_site.person_posts.active.sorted.last(10)
       @political_groups = get_political_groups

f1752a166a22443b2a2d3d9c65342e513005b1ec
Fernando Blat <ferblape@gmail.com>
2017-02-13 18:23:29 +0100

    Filter person in statements and posts

--- app/controllers/gobierto_people/person_posts_controller.rb
+++ app/controllers/gobierto_people/person_posts_controller.rb
@@ -1,6 +1,7 @@
 module GobiertoPeople
   class PersonPostsController < GobiertoPeople::ApplicationController
     def index
+      @people = current_site.people.active
       @posts = current_site.person_posts.active.sorted
     end
   end
--- app/controllers/gobierto_people/person_statements_controller.rb
+++ app/controllers/gobierto_people/person_statements_controller.rb
@@ -1,6 +1,7 @@
 module GobiertoPeople
   class PersonStatementsController < GobiertoPeople::ApplicationController
     def index
+      @people = current_site.people.active
       @statements = current_site.person_statements.active.sorted
     end
   end

3936c2194a0c4a9b6e342b056b70d24a55084148
Fernando Blat <ferblape@gmail.com>
2017-02-15 18:40:08 +0100

    Implement subscription class hierarchy

--- app/models/user/subscription.rb
+++ app/models/user/subscription.rb
@@ -25,4 +25,29 @@ class User::Subscription < ApplicationRecord
   def subscribable
     generic? ? subscribable_type.constantize : super
   end
+
+  def self.find_users_for(subject_type, subject_id, site_id)
+    conditions = [{subscribable_type: subject_type, subscribable_id: subject_id, site_id: site_id}]
+
+    if subject_id.present?
+      conditions.push({subscribable_type: subject_type, subscribable_id: nil, site_id: site_id})
+    end
+
+    if subject_type.include?("::")
+      module_name = subject_type.split("::").first
+      conditions.push({subscribable_type: module_name, subscribable_id: nil, site_id: site_id})
+
+      main_class = subject_type.split("::").last
+      classes = main_class.underscore.split('_')
+      if classes.length > 1
+        conditions.push({subscribable_type: "#{module_name}::#{classes.first.classify}", site_id: site_id})
+      end
+    end
+
+    scoped = self.where(conditions.pop)
+    while conditions.any?
+      scoped = scoped.or(self.where(conditions.pop))
+    end
+    scoped.pluck(:user_id).uniq
+  end
 end

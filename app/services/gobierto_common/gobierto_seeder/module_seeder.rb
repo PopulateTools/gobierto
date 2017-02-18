c0d959ba6f58ef7d4dcb8e561c773b5a0f24228d
Alberto Miedes Garcés <albertomg994@gmail.com>
2017-02-18 17:37:12 +0100

    Update deprecated methods
    Related Ruby docs:
    
    * https://ruby-doc.org/core-2.2.0/Dir.html#method-c-exists-3F
    * https://ruby-doc.org/core-2.2.0/File.html#method-c-exists-3F

--- app/services/gobierto_common/gobierto_seeder/module_seeder.rb
+++ app/services/gobierto_common/gobierto_seeder/module_seeder.rb
@@ -4,7 +4,7 @@ module GobiertoCommon
       def self.seed(module_name, site)
         module_path = module_name.underscore
         module_seeds_path = Rails.root.join("db/seeds/modules/#{module_path}")
-        return unless Dir.exists?(module_seeds_path)
+        return unless Dir.exist?(module_seeds_path)
 
         return unless site.configuration.modules.include?(module_name)
 

8c141748b58d2cc7a6a9673f2e78d989cfc1ce32
Amaia Castro <amaia@amaiac.net>
2017-03-03 16:57:11 +0100

    Seeds for Dival person statements content blocks structure

--- db/seeds/sites/gobierto_dival/gobierto_people/seeds.rb
+++ db/seeds/sites/gobierto_dival/gobierto_people/seeds.rb
@@ -281,6 +281,33 @@ module GobiertoSeeds
           label: { "ca" => "Notes", "es" => "Notas" }
         })
       end
+
+      ## Dival person statements blocks
+      seeds_filename = File.join(File.dirname(__FILE__), "person_statements_seeds.yml")
+      block_data = YAML.load File.read(seeds_filename)
+
+      block_data['blocks'].each do |content_block|
+        block = GobiertoCommon::ContentBlock.find_by(site_id: site.id, internal_id: content_block['internal_id'])
+        if block.nil?
+          block = GobiertoCommon::ContentBlock.create!({
+            internal_id: content_block['internal_id'],
+            site: site,
+            content_model_name: content_block['content_model_name'],
+            title: content_block['title']
+          })
+        end
+
+        content_block['fields'].each do |field|
+          block_field = GobiertoCommon::ContentBlockField.find_by content_block: block, name: field['name']
+          if block_field.nil?
+            GobiertoCommon::ContentBlockField.create!({content_block: block, name: field['name'],
+              field_type: ::GobiertoCommon::ContentBlockField.field_types[field['field_type']],
+              label: field['label']
+            })
+          end
+        end
+      end
+
     end
   end
 end

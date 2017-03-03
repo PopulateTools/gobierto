c1946ed37c5fa3ae939db321c6388b928ccb4e6c
Fernando Blat <ferblape@gmail.com>
2017-03-03 11:45:49 +0100

    Set session to the current domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store,
-  key: "_updated_gobierto_session",
-  domain: Rails.env.test? ? :all : ENV.fetch("DOMAIN") { ".gobierto.dev" },
+  key: "_gobierto_session",
+  domain: :all,
   tld_length: ENV.fetch("TLD_LENGTH") { "2" }.to_i

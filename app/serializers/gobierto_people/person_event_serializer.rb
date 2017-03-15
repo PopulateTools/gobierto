8aa9aafc5e9b281774790c955bc9cd02e976ef46
Amaia Castro <amaia@amaiac.net>
2017-03-15 18:48:02 +0100

    Add locations and attendees to events serialization

--- app/serializers/gobierto_people/person_event_serializer.rb
+++ app/serializers/gobierto_people/person_event_serializer.rb
@@ -1,6 +1,9 @@
 class GobiertoPeople::PersonEventSerializer < ActiveModel::Serializer
   attributes :id, :person_id, :person_name, :title, :description, :starts_at, :ends_at, :attachment_url, :created_at, :updated_at
 
+  has_many :locations
+  has_many :attendees
+
   def person_name
     object.person.try(:name)
   end

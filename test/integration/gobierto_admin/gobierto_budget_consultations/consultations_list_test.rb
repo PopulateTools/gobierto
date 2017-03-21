8d801325457c82cef4680579ca72947ead66cb68
Fernando Blat <ferblape@gmail.com>
2017-03-21 13:04:10 +0100

    Show report tab only if the consultation is closed

---
+++ test/integration/gobierto_admin/gobierto_budget_consultations/consultations_list_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+
+module GobiertoAdmin
+  module GobiertoBudgetConsultations
+    class ConsultationsListTest < ActionDispatch::IntegrationTest
+      def setup
+        super
+        @path = admin_budget_consultations_path(consultation)
+      end
+
+      def consultation
+        @consultation ||= gobierto_budget_consultations_consultations(:madrid_open)
+      end
+
+      def closed_consultation
+        @closed_consultation ||= gobierto_budget_consultations_consultations(:madrid_past)
+      end
+
+      def admin
+        @admin ||= consultation.admin
+      end
+
+      def site
+        @site ||= consultation.site
+      end
+
+      def test_consultations_list
+        with_signed_in_admin(admin) do
+          with_current_site(site) do
+            visit @path
+
+            click_link consultation.title
+            assert has_selector?("form.edit_consultation")
+          end
+        end
+      end
+
+      def test_visit_closed_consultation
+        with_signed_in_admin(admin) do
+          with_current_site(site) do
+            visit @path
+
+            click_link closed_consultation.title
+            assert has_content?("Stats")
+          end
+        end
+      end
+    end
+  end
+end

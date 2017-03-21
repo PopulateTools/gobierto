1a1b96266a231a7d3bd5e496580df9cdbcd54718
Fernando Blat <ferblape@gmail.com>
2017-03-21 12:19:34 +0100

    Fix test

--- test/integration/gobierto_exports/exports_page_test.rb
+++ test/integration/gobierto_exports/exports_page_test.rb
@@ -1,21 +1,23 @@
 require "test_helper"
 
-class HomePageTest < ActionDispatch::IntegrationTest
-  def setup
-    super
-    @path = gobierto_exports_root_path
-  end
+module GobiertoExports
+  class HomePageTest < ActionDispatch::IntegrationTest
+    def setup
+      super
+      @path = gobierto_exports_root_path
+    end
 
-  def site
-    @site ||= sites(:madrid)
-  end
+    def site
+      @site ||= sites(:madrid)
+    end
 
-  def test_index
-    with_current_site(site) do
-      visit @path
+    def test_index
+      with_current_site(site) do
+        visit @path
 
-      assert has_selector?("h1", text: "Download data")
-      assert has_selector?("h2", text: "Officials and Agendas")
+        assert has_selector?("h1", text: "Download data")
+        assert has_selector?("h2", text: "Officials and Agendas")
+      end
     end
   end
 end

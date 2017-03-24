4055fdc0d1dd76a67b0f3ac5c5a48369e46e6c25
Martín González <m@martingonzalez.net>
2017-03-24 12:49:59 +0100

    Implement roles and a bit of semantic markup

--- app/assets/javascripts/module-site_header.js
+++ app/assets/javascripts/module-site_header.js
@@ -1,13 +1,18 @@
 $(document).on('turbolinks:load', function() {
 
-	$('.menu_control, .close').click(function(e) {
+	$('.nav_control, .close').click(function(e) {
 		e.preventDefault();
-		if( $('menu.complete').css('display') == 'none' ) {
-			$('menu.complete').velocity("transition.slideDownIn");
+		if( $('.menu.complete').css('display') == 'none' ) {
+      $('.nav').attr('aria-hidden', 'true');
+      $('.menu.complete').attr({
+        'aria-hidden': 'false',
+        'aria-expanded': 'true'
+      });
+			$('.menu.complete').velocity("transition.slideDownIn");
 			if(($(window).width() < 740)) {
-				$('menu.complete').velocity("scroll", { offset: -30});
+				$('.menu.complete').velocity("scroll", { offset: -30});
 			}
-			$('menu.complete .close').velocity({
+			$('.menu.complete .close').velocity({
 				display: 'block', 
 				top: '-2em'
 				
@@ -16,17 +21,22 @@ $(document).on('turbolinks:load', function() {
 			});
 		}
 		else {
-			$('menu.complete .close').velocity({
+      $('.nav').attr('aria-hidden', 'false');
+      $('.menu.complete').attr({
+        'aria-hidden': 'true',
+        'aria-expanded': 'false'
+      });
+			$('.menu.complete .close').velocity({
 				top: '2em'
 			});
-			$('menu.complete').velocity("transition.slideUpOut", { delay: 150 });
+			$('.menu.complete').velocity("transition.slideUpOut", { delay: 150 });
 		}	
 	});
 
-	$('menu.nav .search_box input').focus(function(e) {
+	$('.nav .search_box input').focus(function(e) {
 		$(this).velocity({width: '250px'});
 	});
-	$('menu.nav .search_box input').blur(function(e) {
+	$('.nav .search_box input').blur(function(e) {
 		$(this).velocity({width: '150px'});
 	});
 

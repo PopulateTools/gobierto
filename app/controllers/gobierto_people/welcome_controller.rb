4e2a9e5bc15170aa062bf095720a21f7009d90aa
Alberto Miedes Garcés <albertomg994@gmail.com>
2017-04-04 16:37:09 +0200

    Fix events retrieval for GobiertoPeople welcome controller

--- app/controllers/gobierto_people/welcome_controller.rb
+++ app/controllers/gobierto_people/welcome_controller.rb
@@ -4,13 +4,24 @@ module GobiertoPeople
 
     def index
       @people = current_site.people.active.politician.government.last(10)
-      @events = current_site.person_events.upcoming.sorted.first(10)
       @posts  = current_site.person_posts.active.sorted.last(10)
       @political_groups = get_political_groups
       @home_text = load_home_text
+      set_events
     end
 
     private
+
+    def set_events
+      @events = current_site.person_events.by_person_party(Person.categories[:government]).sorted
+
+      if @events.upcoming.empty?
+        @no_upcoming_events = true
+        @events = @events.past.first(10)
+      else
+        @events = @events.upcoming.first(10)
+      end
+    end
 
     def load_home_text
       current_site.gobierto_people_settings.find_by(key: "home_text_#{I18n.locale}").try(:value)

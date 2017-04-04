df65ebd4a3bcaaf8f52a8f349b32bb9f4d21ccf5
Alberto Miedes Garcés <albertomg994@gmail.com>
2017-04-04 13:35:35 +0200

    Move route calculation logic into helper

--- app/helpers/gobierto_people/locations_helper.rb
+++ app/helpers/gobierto_people/locations_helper.rb
@@ -3,5 +3,16 @@ module GobiertoPeople
     def external_location_service_url(location)
       "https://www.google.com/maps/place/#{location.lat},#{location.lng}"
     end
+
+    def gobierto_people_past_events_for_people_group_url(people_group)
+      case people_group
+      when 'government'
+        gobierto_people_government_party_past_events_path
+      when 'opposition'
+        gobierto_people_opposition_party_past_events_path
+      when 'executive'
+        gobierto_people_executive_category_past_events_path
+      end
+    end
   end
 end

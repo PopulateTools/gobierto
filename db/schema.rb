e9ed994263453a16f8ea4572367cdf244012e522
Alberto Miedes Garcés <albertomg994@gmail.com>
2017-04-10 11:31:40 +0200

    Create default submodule settings for existing sites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322144939) do
+ActiveRecord::Schema.define(version: 20170410082426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

62e5734e475f66b2c07da8cf230de62a1f02c6ab
Fernando Blat <ferblape@gmail.com>
2017-04-20 09:43:03 +0200

    Introduced I18n database backend translations

---
+++ config/initializers/locale.rb
@@ -0,0 +1,13 @@
+require 'i18n/backend/active_record'
+
+Translation  = I18n::Backend::ActiveRecord::Translation
+
+if Translation.table_exists?
+  I18n.backend = I18n::Backend::ActiveRecord.new
+
+  I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
+  I18n::Backend::Simple.send(:include, I18n::Backend::Memoize)
+  I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
+
+  I18n.backend = I18n::Backend::Chain.new(I18n.backend, I18n::Backend::Simple.new)
+end

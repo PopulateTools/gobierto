1e918c813627a196a4415968e9bb2a5568580cae
Fernando Blat <ferblape@gmail.com>
2017-05-22 18:32:31 +0200

    Allow the person to select calendars to integrate

--- app/models/gobierto_people/person_google_calendar_configuration.rb
+++ app/models/gobierto_people/person_google_calendar_configuration.rb
@@ -1,11 +1,11 @@
 module GobiertoPeople
   class PersonGoogleCalendarConfiguration < PersonCalendarConfiguration
-    def google_calendar_credentials=(value)
-      data['google_calendar_credentials'] = value
-    end
-
     def google_calendar_credentials
       data['google_calendar_credentials']
+    end
+
+    def google_calendar_credentials=(value)
+      data['google_calendar_credentials'] = value
     end
 
     def google_calendar_id
@@ -14,6 +14,14 @@ module GobiertoPeople
 
     def google_calendar_id=(value)
       data['google_calendar_id'] = value
+    end
+
+    def calendars=(value)
+      data['calendars'] = value
+    end
+
+    def calendars
+      data['calendars']
     end
   end
 end

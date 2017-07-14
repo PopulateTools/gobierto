d76be73856d4d6c8300c9cd4fa6eef3c83470e88
Fernando Blat <ferblape@gmail.com>
2017-07-14 09:11:41 +0200

    Translated and integrated budget lines

--- app/assets/javascripts/gobierto_budgets/app/init.js
+++
@@ -1,7 +0,0 @@
-this.GobiertoBudgets = {
-  init: function() { console.log('GobiertoBudgets init');}
-};
-
-$(document).on("turbolinks:load", function(){
-  return GobiertoBudgets.init();
-});
---
+++ test/integration/gobierto_budgets/guide_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class GobiertoBudgets::GuideTest < ActionDispatch::IntegrationTest
+  def setup
+    super
+    @path = gobierto_budgets_budgets_guide_path
+  end
+
+  def site
+    @site ||= sites(:madrid)
+  end
+
+  def last_year
+    2016
+  end
+
+  def test_greeting
+    with_current_site(site) do
+      visit @path
+
+      assert has_content?("How a municipal budget works")
+      assert has_content?("In 2016 were entered...")
+      assert has_content?("4483.5M€")
+    end
+  end
+end

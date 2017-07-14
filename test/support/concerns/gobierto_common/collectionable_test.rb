dc8c76e4407dd7f52e373c23b2b0b65102b05df2
Adrian Pradilla <adrianpradilla@gmail.com>
2017-07-14 10:27:55 +0200

    Delete test

--- test/support/concerns/gobierto_common/collectionable_test.rb
+++
@@ -1,8 +0,0 @@
-module GobiertoCommon::CollectionableTest
-  def test_remove_cleans_collection_items
-    perform_enqueued_jobs do
-      collectionable_object.destroy
-    end
-    assert GobiertoCommon::CollectionItem.where(item: collectionable_object).empty?
-  end
-end

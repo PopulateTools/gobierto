4122ead99919ac18759c8f1e120c4ef3c8dd0c8b
Fernando Blat <ferblape@gmail.com>
2017-08-02 12:33:37 +0200

    Fix migrations to match production database

--- db/migrate/20170729084620_remove_person_id_from_gobierto_calendars_events.rb
+++ db/migrate/20170729084620_remove_person_id_from_gobierto_calendars_events.rb
@@ -1,13 +1,27 @@
 class RemovePersonIdFromGobiertoCalendarsEvents < ActiveRecord::Migration[5.1]
   def up
+    add_column :gobierto_calendars_events, :collection_id, :integer
+
+    GobiertoCalendars::Event.reset_column_information
+
+    GobiertoPeople::Person.all.each do |p|
+      p.send :create_events_collection
+    end.size
+
+    GobiertoCalendars::Event.all.each do |e|
+      if p = GobiertoPeople::Person.find_by(id: e.person_id)
+        e.collection = p.events_collection
+        e.save!
+      end
+    end
+
     remove_column :gobierto_calendars_events, :person_id
-    #remove_index :gobierto_calendars_events, name: "index_gobierto_calendars_events_on_person_id"
-    #remove_index :gobierto_calendars_events, name: "index_gobierto_calendars_events_on_person_id_and_external_id"
+
+    add_foreign_key :gobierto_calendars_events, :collections, on_delete: :cascade
   end
 
   def down
+    remove_column :gobierto_calendars_events, :collection_id
     add_column :gobierto_calendars_events, :person_id, :integer, null: false
-    #add_index :gobierto_calendars_events, :person_id, name: "index_gobierto_calendars_events_on_person_id"
-    #add_index :gobierto_calendars_events, [:person_id, :external_id], unique: true, name: "index_gobierto_calendars_events_on_person_id_and_external_id"
   end
 end
--- db/migrate/20170729140429_add_collection_reference_to_events.rb
+++
@@ -1,6 +0,0 @@
-class AddCollectionReferenceToEvents < ActiveRecord::Migration[5.1]
-  def change
-    add_column :gobierto_calendars_events, :collection_id, :integer
-    add_foreign_key :gobierto_calendars_events, :collections, on_delete: :cascade
-  end
-end

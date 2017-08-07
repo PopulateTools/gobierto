51ddf944040c4565ccae3208e08fdd8ee613ee55
Alberto Miedes Garcés <albertomg994@gmail.com>
2017-08-07 12:37:58 +0200

    Filter events by date when clicking on calendar links

--- app/controllers/gobierto_participation/events_controller.rb
+++ app/controllers/gobierto_participation/events_controller.rb
@@ -2,19 +2,28 @@ module GobiertoParticipation
   class EventsController < GobiertoParticipation::ApplicationController
 
     def index
-      @container = find_container(params[:container_type], params[:container_id])
-      @events = @container.events.upcoming.page params[:page]
-      @calendar_events = @container.events
+      set_container
+      set_events
     end
 
     def show
-      @container = find_container(params[:container_type], params[:container_id])
+      set_container
 
       @event = find_event
       @calendar_events = @container.events
     end
 
     private
+
+    def set_events
+      if params[:date]
+        @events = find_events_by_date params[:date]
+      else
+        @events = @container.events.upcoming.sorted.page params[:page]
+      end
+
+      @calendar_events = @container.events
+    end
 
     def find_event
       GobiertoCalendars::Event.find_by!(slug: params[:id])
@@ -24,6 +33,20 @@ module GobiertoParticipation
       return unless container_type && container_id
       container_klass = GobiertoCommon::ActsAsCollectionContainer.container_klass_for[container_type]
       container_klass ? container_klass.find(container_id) : nil
+    end
+
+    def set_container
+      if params[:container_type] && params[:container_id]
+        container_klass = GobiertoCommon::ActsAsCollectionContainer.container_klass_for[params[:container_type]]
+        @container = container_klass ? container_klass.find(params[:container_id]) : nil
+      end
+    end
+
+    def find_events_by_date(date)
+      @filtering_date = Date.parse(date)
+      events = @container.events.by_date(@filtering_date)
+      events = (@filtering_date >= Time.now ? events.sorted : events.sorted_backwards)
+      events.page params[:page]
     end
 
   end

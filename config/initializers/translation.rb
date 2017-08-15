42dfd643859c6cf4edba8c580cdd64fe5a79fe53
Fernando Blat <ferblape@gmail.com>
2017-08-15 10:02:38 +0200

    Adapt i18n tasks configuration

--- config/initializers/translation.rb
+++ config/initializers/translation.rb
@@ -1,8 +1,16 @@
+# frozen_string_literal: true
+
 TranslationIO.configure do |config|
-  config.api_key        = ENV['TRANSLATION_IO_API_KEY']
-  config.source_locale  = 'en'
-  config.target_locales = ['ca', 'es']
+  config.api_key = ENV["TRANSLATION_IO_API_KEY"]
+  config.source_locale = "en"
+  config.target_locales = %w(ca es)
   config.disable_gettext = true
+  config.ignored_key_prefixes = [
+    "i18n_tasks.",
+    "number.",
+    "spree.",
+    "cookies_eu."
+  ]
 
   # Find other useful usage information here:
   # https://github.com/aurels/translation-gem/blob/master/README.md

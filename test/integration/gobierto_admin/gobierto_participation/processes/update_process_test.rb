227eb82f33c5e8e119baa6d90564a7348b8a7e24
Alberto Miedes Garcés <albertomg994@gmail.com>
2017-09-20 15:39:29 +0200

    Improve UX when creating processes & process_stages

--- test/integration/gobierto_admin/gobierto_participation/processes/update_process_test.rb
+++ test/integration/gobierto_admin/gobierto_participation/processes/update_process_test.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 module GobiertoAdmin
   module GobiertoParticipation
     class CreateProcessTest < ActionDispatch::IntegrationTest
+
       def admin
         @admin ||= gobierto_admin_admins(:nick)
       end
@@ -26,18 +27,18 @@ module GobiertoAdmin
           with_current_site(site) do
             visit edit_admin_participation_process_path(process)
 
-            within "form.edit_process" do
-              fill_in "process_title_translations_en", with: "Edited process title"
-              select "Women", from: "process_issue_id"
+            within 'form.edit_process' do
+              fill_in 'process_title_translations_en', with: 'Edited process title'
+              select 'Women', from: 'process_issue_id'
 
-              click_button "Update"
+              click_button 'Update'
             end
 
-            assert has_message? "Process was successfully updated"
+            assert has_message? 'Process was successfully updated'
 
             visit admin_participation_processes_path
 
-            assert has_content? "Edited process title"
+            assert has_content? 'Edited process title'
 
             process.reload
 
@@ -52,18 +53,18 @@ module GobiertoAdmin
           with_current_site(site) do
             visit edit_admin_participation_process_path(group)
 
-            within "form.edit_process" do
-              fill_in "process_title_translations_en", with: "Edited group title"
-              select "Women", from: "process_issue_id"
+            within 'form.edit_process' do
+              fill_in 'process_title_translations_en', with: 'Edited group title'
+              select 'Women', from: 'process_issue_id'
 
-              click_button "Update"
+              click_button 'Update'
             end
 
-            assert has_message? "Process was successfully updated"
+            assert has_message? 'Process was successfully updated'
 
             visit admin_participation_processes_path
 
-            assert has_content? "Edited group title"
+            assert has_content? 'Edited group title'
 
             group.reload
 
@@ -88,10 +89,10 @@ module GobiertoAdmin
                 fill_in 'process_stages_attributes_2_ends',   with: '2017-01-14'
               end
 
-              click_button "Update"
+              click_button 'Update'
             end
 
-            assert has_message? "Process was successfully updated"
+            assert has_message? 'Process was successfully updated'
 
             visit admin_participation_processes_path
 
@@ -105,16 +106,16 @@ module GobiertoAdmin
           with_current_site(site) do
             visit edit_admin_participation_process_path(process)
 
-            within "form.edit_process" do
-              find("#process_stages_attributes_0_active").set(false) # disable Information stage
-              click_button "Update"
+            within 'form.edit_process' do
+              find('#process_stages_attributes_0_active').set(false) # disable Information stage
+              click_button 'Update'
             end
 
-            assert has_message? "Process was successfully updated"
+            assert has_message? 'Process was successfully updated'
 
             visit admin_participation_processes_path
 
-            assert array_match %w(meetings ideas results), process.stages.pluck(:stage_type)
+            assert array_match %w(meetings ideas results), process.stages.active.pluck(:stage_type)
           end
         end
       end
@@ -124,43 +125,44 @@ module GobiertoAdmin
           with_current_site(site) do
             visit edit_admin_participation_process_path(process)
 
-            within "form.edit_process" do
+            within 'form.edit_process' do
               # Update Information stage
-              within "#edit_stage_0" do
-                fill_in "process_stages_attributes_0_title_translations_en", with: "Edited information stage title"
-                fill_in "process_stages_attributes_0_starts", with: "2017-07-15"
-                fill_in "process_stages_attributes_0_ends", with: "2017-07-16"
+              within '#edit_stage_0' do
+                fill_in 'process_stages_attributes_0_title_translations_en', with: 'Edited information stage title'
+                fill_in 'process_stages_attributes_0_starts', with: '2017-07-15'
+                fill_in 'process_stages_attributes_0_ends', with: '2017-07-16'
               end
 
               # Update Meetings stage
-              within "#edit_stage_1" do
-                fill_in "process_stages_attributes_1_title_translations_en", with: "Edited meetings stage title"
-                fill_in "process_stages_attributes_1_starts", with: "2017-08-17"
-                fill_in "process_stages_attributes_1_ends", with: "2017-08-18"
+              within '#edit_stage_1' do
+                fill_in 'process_stages_attributes_1_title_translations_en', with: 'Edited meetings stage title'
+                fill_in 'process_stages_attributes_1_starts', with: '2017-08-17'
+                fill_in 'process_stages_attributes_1_ends', with: '2017-08-18'
               end
 
-              click_button "Update"
+              click_button 'Update'
             end
 
-            assert has_message? "Process was successfully updated"
+            assert has_message? 'Process was successfully updated'
 
             visit admin_participation_processes_path
 
-            information_stage = process.stages.find_by(stage_type: "information")
-            meetings_stage = process.stages.find_by(stage_type: "meetings")
+            information_stage = process.stages.find_by(stage_type: 'information')
+            meetings_stage = process.stages.find_by(stage_type: 'meetings')
 
-            assert array_match %w(information meetings ideas results), process.stages.pluck(:stage_type)
+            assert array_match %w(information meetings ideas results), process.stages.active.pluck(:stage_type)
 
-            assert_equal "Edited information stage title", information_stage.title
-            assert_equal Date.parse("2017-07-15"), information_stage.starts
-            assert_equal Date.parse("2017-07-16"), information_stage.ends
+            assert_equal 'Edited information stage title', information_stage.title
+            assert_equal Date.parse('2017-07-15'), information_stage.starts
+            assert_equal Date.parse('2017-07-16'), information_stage.ends
 
-            assert_equal "Edited meetings stage title", meetings_stage.title
-            assert_equal Date.parse("2017-08-17"), meetings_stage.starts
-            assert_equal Date.parse("2017-08-18"), meetings_stage.ends
+            assert_equal 'Edited meetings stage title', meetings_stage.title
+            assert_equal Date.parse('2017-08-17'), meetings_stage.starts
+            assert_equal Date.parse('2017-08-18'), meetings_stage.ends
           end
         end
       end
+
     end
   end
 end

d73dc6fd05608bb4054c6a354f44f45c3e386173
Adrian Pradilla <adrianpradilla@gmail.com>
2017-09-29 14:40:48 +0200

    Review i18 participation keys

--- app/controllers/gobierto_participation/welcome_controller.rb
+++ app/controllers/gobierto_participation/welcome_controller.rb
@@ -5,19 +5,19 @@ module GobiertoParticipation
     def index
       @processes = current_site.processes.process.active
       @issues = current_site.issues.alphabetically_sorted
-      @participation_events = find_participation_events
-      @participation_news = find_participation_news
-      @participation_activities = find_participation_activities
+      @events = find_participation_events
+      @news = find_participation_news
+      @activities = find_participation_activities
     end
 
     private
 
     def find_participation_events
-      ::GobiertoCalendars::Event.events_in_collections_and_container_type(current_site, "GobiertoParticipation").sorted.upcoming.published
+      ::GobiertoCalendars::Event.events_in_collections_and_container_type(current_site, "GobiertoParticipation").sorted.upcoming.published.limit(4)
     end
 
     def find_participation_news
-      ::GobiertoCms::Page.pages_in_collections_and_container_type(current_site, "GobiertoParticipation").sorted.active
+      ::GobiertoCms::Page.pages_in_collections_and_container_type(current_site, "GobiertoParticipation").sorted.active.limit(5)
     end
 
     def find_participation_activities

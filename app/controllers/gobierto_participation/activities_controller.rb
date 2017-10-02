0ee94e1e7519b6b417b73f3a7aeffbdbe098e2f4
Adrian Pradilla <adrianpradilla@gmail.com>
2017-10-02 10:33:13 +0200

    Fix filter by theme in activities

--- app/controllers/gobierto_participation/activities_controller.rb
+++ app/controllers/gobierto_participation/activities_controller.rb
@@ -7,13 +7,23 @@ module GobiertoParticipation
 
       @issue = find_issue if params[:issue_id]
 
-      @activities = find_participation_activities
+      @activities = if @issue
+                      find_participation_activities_issue(@issue)
+                    else
+                      find_participation_activities
+                    end
     end
 
     private
 
     def find_issue
       current_site.issues.find_by_slug!(params[:issue_id])
+    end
+
+    def find_participation_activities_issue(issue)
+      ActivityCollectionDecorator.new(Activity.in_site(current_site)
+                                              .in_container(issue)
+                                              .sorted.includes(:subject, :author, :recipient).page(params[:page]))
     end
 
     def find_participation_activities

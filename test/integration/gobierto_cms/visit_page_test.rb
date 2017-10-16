3e4da180f162bb0794477efe88d0838bf5efef34
Adrian Pradilla <adrianpradilla@gmail.com>
2017-10-16 16:52:44 +0200

    Relocate attachments and tests

--- test/integration/gobierto_cms/visit_page_test.rb
+++ test/integration/gobierto_cms/visit_page_test.rb
@@ -14,7 +14,32 @@ module GobiertoCms
     end
 
     def cms_page
-      @cms_page ||= gobierto_cms_pages(:consultation_faq)
+      @cms_page ||= gobierto_cms_pages(:about_site)
+    end
+
+    def attachments
+      @attachments ||= cms_page.attachments
+    end
+
+    def test_breadcrumb_items
+      with_current_site(site) do
+        visit @path
+
+        within ".global_breadcrumb" do
+          assert has_link? cms_page.collection.title
+        end
+      end
+    end
+
+    def test_breadcrumb_page
+      with_current_site(site) do
+        visit @path
+
+        within ".breadcrumb" do
+          assert has_link? cms_page.collection.title
+          assert has_link? cms_page.title
+        end
+      end
     end
 
     def test_visit_page
@@ -23,6 +48,12 @@ module GobiertoCms
 
         assert has_selector?("h1", text: cms_page.title)
         assert has_content?(cms_page.body)
+
+        within ".page_attachments" do
+          attachments.each do |attachment|
+            assert has_link? attachment.name
+          end
+        end
       end
     end
   end

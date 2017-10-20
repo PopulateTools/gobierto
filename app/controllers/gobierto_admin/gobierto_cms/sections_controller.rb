dc131f72b0cd2bf240adc68d720bf4d60529c1d6
Adrian Pradilla <adrianpradilla@gmail.com>
2017-10-21 00:32:20 +0200

    WIP filter pages in section

--- app/controllers/gobierto_admin/gobierto_cms/sections_controller.rb
+++ app/controllers/gobierto_admin/gobierto_cms/sections_controller.rb
@@ -11,7 +11,15 @@ module GobiertoAdmin
 
       def show
         @section = find_section
-        @pages = ::GobiertoCms::Page.pages_in_collections(current_site)
+        @pages = ::GobiertoCms::Page.pages_in_collections(current_site).active
+      end
+
+      def pages
+        @pages = ::GobiertoCms::Page.search(params[:query])
+
+        respond_to do |format|
+          format.js {render layout: false}
+        end
       end
 
       def new

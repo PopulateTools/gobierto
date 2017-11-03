78b1f465a3019518e3b6d393dab8d73669adda63
Adrian Pradilla <adrianpradilla@gmail.com>
2017-11-03 11:40:24 +0100

    Simplify root path and add doc to developing module

--- app/controllers/meta_welcome_controller.rb
+++ app/controllers/meta_welcome_controller.rb
@@ -1,14 +1,9 @@
+# frozen_string_literal: true
+
 class MetaWelcomeController < ApplicationController
   def index
     render_404 and return if current_site.nil?
 
-    if current_site.configuration.home_page == "GobiertoPeople"
-      redirect_to gobierto_people_root_path and return
-    elsif current_site.configuration.home_page == "GobiertoBudgets"
-      redirect_to gobierto_budgets_site_path and return
-    elsif current_site.configuration.home_page == "GobiertoParticipation" ||
-          current_site.configuration.home_page == "GobiertoIndicators"
-      redirect_to eval("#{current_site.configuration.home_page.underscore}_root_path") and return
-    end
+    redirect_to eval("#{current_site.configuration.home_page.underscore}_root_path") and return
   end
 end

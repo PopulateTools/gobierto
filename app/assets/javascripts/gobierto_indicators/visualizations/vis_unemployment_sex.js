46ffc69c83e83b97ded948bd1f894b9f846854cf
Fernando Blat <ferblape@gmail.com>
2017-11-10 12:34:47 +0100

    Fix ticks

--- app/assets/javascripts/gobierto_indicators/visualizations/vis_unemployment_sex.js
+++ app/assets/javascripts/gobierto_indicators/visualizations/vis_unemployment_sex.js
@@ -125,7 +125,7 @@ var VisUnemploymentSex = Class.extend({
     this.line = d3.line()
       .x(function(d) { return this.xScale(d.date); }.bind(this))
       .y(function(d) { return this.yScale(d.pct); }.bind(this));
-      
+
       var lines = this.svg.append('g')
         .attr('class', 'lines')
         .selectAll('path')
@@ -138,19 +138,19 @@ var VisUnemploymentSex = Class.extend({
       linesGroup.append('path')
         .attr('d', function(d) { d.line = this; return this.line(d.values); }.bind(this))
         .attr('stroke', function(d) { return this.color(d.key); }.bind(this));
-        
+
       linesGroup.append('circle')
        .attr('cx', function(d) { return this.xScale(d.values.map(function(d) { return d.date; }).slice(-1)[0]); }.bind(this))
        .attr('cy', function(d) { return this.yScale(d.values.map(function(d) { return d.pct; }).slice(-1)[0]); }.bind(this))
        .attr('r', 5)
        .attr('fill', function(d) { return this.color(d.key); }.bind(this));
-       
+
       var linesText = d3.select(this.container).append('div')
         .attr('class', 'lines-labels')
         .selectAll('p')
         .data(this.nest, function(d) { return d.key; })
         .enter();
-        
+
       linesText.append('div')
         .style('right', '20px')
         .style('top', function(d) { return this.yScale(d.values.map(function(d) { return d.pct; }).slice(-1)[0]) + 'px'; }.bind(this))
@@ -217,7 +217,7 @@ var VisUnemploymentSex = Class.extend({
     this.xAxis
       .tickPadding(5)
       .tickSize(0, 0)
-      .ticks(2)
+      .ticks(4)
       .scale(this.xScale);
 
     this.svg.select('.x.axis').call(this.xAxis);
@@ -226,8 +226,8 @@ var VisUnemploymentSex = Class.extend({
     this.yAxis
       .tickSize(-this.width)
       .scale(this.yScale)
-      .ticks(2, '%');
-    
+      .ticks(3, '%');
+
     this.svg.select('.y.axis').call(this.yAxis);
 
     // Remove the zero
@@ -249,12 +249,12 @@ var VisUnemploymentSex = Class.extend({
   },
   _resize: function() {
     this.isMobile = window.innerWidth <= 768;
-    
+
     this.width = this._width() - this.margin.left - this.margin.right;
     this.height = this._height() - this.margin.top - this.margin.bottom;
 
     this.updateRender();
-    
+
     d3.select(this.container + ' svg')
       .attr('width', this.width + this.margin.left + this.margin.right)
       .attr('height', this.height + this.margin.top + this.margin.bottom);
@@ -264,11 +264,11 @@ var VisUnemploymentSex = Class.extend({
 
     this.svg.selectAll('.lines path')
       .attr('d', function(d) { d.line = this; return this.line(d.values); }.bind(this));
-      
+
     this.svg.selectAll('.lines circle')
       .attr('cx', function(d) { return this.xScale(d.values.map(function(d) { return d.date; }).slice(-1)[0]); }.bind(this))
       .attr('cy', function(d) { return this.yScale(d.values.map(function(d) { return d.pct; }).slice(-1)[0]); }.bind(this));
-      
+
     d3.selectAll(this.container + ' .lines-labels div')
       .style('top', function(d) { return this.yScale(d.values.map(function(d) { return d.pct; }).slice(-1)[0]) + 'px'; }.bind(this));
 

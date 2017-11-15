9e331c9438f1b8915670565cf2ea71bd51205cf3
Adrian Pradilla <adrianpradilla@gmail.com>
2017-11-15 11:08:36 +0100

    Add section breadcrumb, page show test and fix position for section_items

---
+++ test/integration/gobierto_cms/page_show_test.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module GobiertoCms
+  class PageShowTest < ActionDispatch::IntegrationTest
+    def page_path
+      @page_path ||= gobierto_cms_path(
+        id: cms_page.slug,
+        slug_section: section.slug
+      )
+    end
+
+    def admin
+      @admin ||= gobierto_admin_admins(:nick)
+    end
+
+    def site
+      @site ||= sites(:madrid)
+    end
+
+    def section
+      @section ||= gobierto_cms_sections(:participation)
+    end
+
+    def cms_page
+      @cms_page ||= gobierto_cms_pages(:about_participation)
+    end
+
+    def test_pages_show
+      with_javascript do
+        with_current_site(site) do
+          visit page_path
+
+          within ".global_breadcrumb" do
+            assert has_link? section.title
+          end
+
+          within ".breadcrumb" do
+            assert has_link? section.title
+          end
+
+          within ".side_navigation" do
+            assert has_link? cms_page.title
+          end
+
+          within "article" do
+            assert has_selector?("h1", text: cms_page.title)
+            assert has_content?(cms_page.body)
+          end
+        end
+      end
+    end
+  end
+end

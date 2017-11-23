f5cd29ab7a59eb5714eed0118f7f8e463f2cda86
Fernando Blat <ferblape@gmail.com>
2017-11-23 16:56:58 +0100

    Render page in the root path without redirection

--- app/controllers/meta_welcome_controller.rb
+++ app/controllers/meta_welcome_controller.rb
@@ -1,14 +1,33 @@
 # frozen_string_literal: true
 
 class MetaWelcomeController < ApplicationController
+  include User::SessionHelper
+
   def index
     render_404 and return if current_site.nil?
 
-    unless current_site.configuration.home_page == "GobiertoCms"
-      redirect_to eval("#{current_site.configuration.home_page.underscore}_root_path") and return
+    if current_site.configuration.home_page != "GobiertoCms"
+      redirect_to eval("#{current_site.configuration.home_page.underscore}_root_path")
     else
+      # This code allows the homepage to render a page without redirecting to the page url
       item = GlobalID::Locator.locate(current_site.configuration.home_page_item_id)
-      redirect_to item.to_url and return
+      render_404 and return if item.nil?
+
+      if item.is_a?(GobiertoCms::Section)
+        @section = item
+        @page = GobiertoCms::Page.first_page_in_section(item)
+      else
+        @page = item
+      end
+
+      if @section ||= @page.section
+        @section_item = ::GobiertoCms::SectionItem.find_by!(item: @page, section: @section)
+      else
+        @collection = @page.collection
+        @pages = ::GobiertoCms::Page.where(id: @collection.pages_in_collection).active
+      end
+
+      render "gobierto_cms/pages/show"
     end
   end
 end

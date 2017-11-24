ca0fd089c83cec423f60872bcfc4bd19b0925d71
Hugoren Martinako <aumpfbahn@gmail.com>
2017-11-24 14:41:33 +0100

    Mobile version - 1st approach

--- app/assets/javascripts/module-site_header.js
+++ app/assets/javascripts/module-site_header.js
@@ -2,35 +2,46 @@
 $(document).on('turbolinks:load', function() {
   $('.nav_control, .close').click(function(e) {
     e.preventDefault();
-    if( $('.menu.complete').css('display') == 'none' ) {
-      $('.nav').attr('aria-hidden', 'true');
-      $('.menu.complete').attr({
-        'aria-hidden': 'false',
-        'aria-expanded': 'true'
-      });
-      $('.menu.complete').velocity("transition.slideDownIn");
-      if(($(window).width() < 740)) {
-        $('.menu.complete').velocity("scroll", { offset: -30});
-      }
-      $('.menu.complete .close').velocity({
-        display: 'block',
-        top: '-2em'
 
-      }, {
-        delay: 150
-      });
+    let nav = $('.main-nav');
+    let subnav = $('.sub-nav');
+
+    if( nav.css('display') == 'none' ) {
+      nav.velocity("transition.slideDownIn");
+      subnav.velocity("transition.slideDownIn", { delay: 150 });
+    } else {
+      nav.velocity("transition.slideUpOut");
+      subnav.velocity("transition.slideUpOut");
     }
-    else {
-      $('.nav').attr('aria-hidden', 'false');
-      $('.menu.complete').attr({
-        'aria-hidden': 'true',
-        'aria-expanded': 'false'
-      });
-      $('.menu.complete .close').velocity({
-        top: '2em'
-      });
-      $('.menu.complete').velocity("transition.slideUpOut", { delay: 150 });
-    }
+    // if( $('.menu.complete').css('display') == 'none' ) {
+    //   $('.nav').attr('aria-hidden', 'true');
+    //   $('.menu.complete').attr({
+    //     'aria-hidden': 'false',
+    //     'aria-expanded': 'true'
+    //   });
+    //   $('.menu.complete').velocity("transition.slideDownIn");
+    //   if(($(window).width() < 740)) {
+    //     $('.menu.complete').velocity("scroll", { offset: -30});
+    //   }
+    //   $('.menu.complete .close').velocity({
+    //     display: 'block',
+    //     top: '-2em'
+    //
+    //   }, {
+    //     delay: 150
+    //   });
+    // }
+    // else {
+    //   $('.nav').attr('aria-hidden', 'false');
+    //   $('.menu.complete').attr({
+    //     'aria-hidden': 'true',
+    //     'aria-expanded': 'false'
+    //   });
+    //   $('.menu.complete .close').velocity({
+    //     top: '2em'
+    //   });
+    //   $('.menu.complete').velocity("transition.slideUpOut", { delay: 150 });
+    // }
   });
 
   $('.site_header_logo .search_icon').click(function(e) {

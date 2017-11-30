b46716e3abae0f3b5f82a8f996d353fce762c94e
Fernando Blat <ferblape@gmail.com>
2017-11-30 17:21:22 +0100

    Fix JS back to ES5

--- app/assets/javascripts/module-site_header.js
+++ app/assets/javascripts/module-site_header.js
@@ -3,8 +3,8 @@ $(document).on('turbolinks:load', function() {
   $('.nav_control, .close').click(function(e) {
     e.preventDefault();
 
-    let nav = $('.main-nav');
-    let subnav = $('.sub-nav');
+    var nav = $('.main-nav');
+    var subnav = $('.sub-nav');
 
     if( nav.css('display') == 'none' ) {
       nav.velocity("transition.slideDownIn");
@@ -13,35 +13,6 @@ $(document).on('turbolinks:load', function() {
       nav.velocity("transition.slideUpOut");
       subnav.velocity("transition.slideUpOut");
     }
-    // if( $('.menu.complete').css('display') == 'none' ) {
-    //   $('.nav').attr('aria-hidden', 'true');
-    //   $('.menu.complete').attr({
-    //     'aria-hidden': 'false',
-    //     'aria-expanded': 'true'
-    //   });
-    //   $('.menu.complete').velocity("transition.slideDownIn");
-    //   if(($(window).width() < 740)) {
-    //     $('.menu.complete').velocity("scroll", { offset: -30});
-    //   }
-    //   $('.menu.complete .close').velocity({
-    //     display: 'block',
-    //     top: '-2em'
-    //
-    //   }, {
-    //     delay: 150
-    //   });
-    // }
-    // else {
-    //   $('.nav').attr('aria-hidden', 'false');
-    //   $('.menu.complete').attr({
-    //     'aria-hidden': 'true',
-    //     'aria-expanded': 'false'
-    //   });
-    //   $('.menu.complete .close').velocity({
-    //     top: '2em'
-    //   });
-    //   $('.menu.complete').velocity("transition.slideUpOut", { delay: 150 });
-    // }
   });
 
   $('.site_header_logo .search_icon').click(function(e) {

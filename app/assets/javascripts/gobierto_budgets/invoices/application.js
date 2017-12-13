180e967079eeb7dfd5991e98581a28c2460e9a79
Hugoren Martinako <aumpfbahn@gmail.com>
2017-12-13 15:22:18 +0100

    d3.v3 Integration mixed

---
+++ app/assets/javascripts/gobierto_budgets/invoices/application.js
@@ -0,0 +1,5 @@
+//= require d3.v3
+//= require crossfilter
+//= require dc
+//= require jsgrid.min
+//= require ./invoices_controller
---
+++ app/assets/javascripts/gobierto_budgets/invoices/invoices_controller.js
@@ -0,0 +1,213 @@
+this.GobiertoBudgets.InvoicesController = (function() {
+
+  function InvoicesController() {}
+
+  InvoicesController.prototype.show = function(options){
+
+    var data = d3.csv.parse(d3.select('pre#data').text().trim());
+    // d3.csv('budget_invoices-providers.mock.csv', function (data) {
+
+    // Boxes calculations
+    function median(values){
+    	values.sort(function(a,b){
+      	return a-b;
+      });
+      var half = Math.floor(values.length / 2);
+
+      if (values.length % 2)
+      	return values[half];
+      else
+      	return (values[half - 1] + values[half]) / 2.0;
+    }
+
+    document.getElementById("numberOfInvoices").innerText = data.length.toLocaleString();
+    document.getElementById("meanBudget").innerText = _.mean(_.map(data, 'amount').map(Number)).toLocaleString(navigator.language, { style: 'currency', currency: 'EUR' });
+    document.getElementById("medianBudget").innerText = median(_.map(data, 'amount').map(Number)).toLocaleString(navigator.language, { style: 'currency', currency: 'EUR' });
+
+    var dateFormat = d3.time.format('%Y/%m/%d');
+    var _r = {
+      domain: [501, 1001, 5001, 10001, 15001],
+      range: ["1€ - 500€", "501€ - 1000€", "1001€ - 5000€", "5001€ - 10000€", "10001€ - 15000€", "+15000€"]
+    };
+    var rangeFormat = d3.scale.threshold().domain(_r.domain).range(_r.range);
+    // pre-calculate for better performance
+    data.forEach(function (d) {
+      d.dd = dateFormat.parse(d.date);
+      d.month = d3.time.month(d.dd);
+      d.range = rangeFormat(+d.amount);
+      d.payed = (d.payed == 'true');
+      d.amount = Number(d.amount);
+    });
+
+    // See the [crossfilter API](https://github.com/square/crossfilter/wiki/API-Reference) for reference.
+    var ndx = crossfilter(data);
+
+    /*
+      BARS CHART - BY DATE
+    */
+
+    // Declaration
+    var bars = dc.barChart("#bars", "group");
+
+    // Dimensions
+    var months = ndx.dimension(function(d) {
+        return d.month
+      }),
+      budgetMonthly = months.group().reduceSum(function(d) {
+        return +d.amount;
+      });
+
+    bars
+      .dimension(months)
+      .group(budgetMonthly)
+      .x(d3.scale.ordinal())
+      .xUnits(dc.units.ordinal)
+      .round(d3.time.month.round)
+      .elasticX(true)
+      .elasticY(true)
+      .alwaysUseRounding(true)
+      .renderHorizontalGridLines(true)
+      .barPadding(0.5);
+
+    // Customize
+    bars.xAxis().tickFormat(d3.time.format('%b')); //TODO: Set locales
+    bars.yAxis().ticks(5);
+    bars.yAxis().tickFormat(
+      function(v) {
+        return v.toLocaleString() + '€';
+      });
+    bars.margins().left = 60;
+
+    // Render
+    bars.render();
+
+    /*
+      ROW CHART - MAIN PROVIDERS
+    */
+
+    // Declaration
+    var hbars1 = dc.rowChart("#hbars1", "group");
+
+    // Dimensions
+    var providers = ndx.dimension(function(d) {
+        return d.name
+      }),
+      providerByAmount = providers.group().reduceSum(function(d) {
+        return +d.amount;
+      });
+
+    // Styling
+    var _count = 10,
+      _gap = 5, // DC default
+      _barHeight = 20;
+
+    hbars1
+      .height(hbars1.margins().top + hbars1.margins().bottom + (_count * _barHeight) + ((_count + 1) * _gap)) // NOTE: Margins top/bottom + bars + gaps (space between)
+      .fixedBarHeight(_barHeight)
+      .cap(_count).othersGrouper(null) // NOTE: Show only a couple of results, and hide Others groups
+      .x(d3.scale.ordinal())
+      .dimension(providers)
+      .group(providerByAmount)
+      .labelOffsetX(-95)
+      .elasticX(true);
+
+    // Customize
+    hbars1.xAxis().tickFormat(function (v) {return v + '€';});
+    hbars1.xAxis().ticks(5);
+    hbars1.margins().left = 100;
+
+    // Render
+    hbars1.render();
+
+    /*
+      ROW CHART - BY AMOUNT
+    */
+
+    // Declaration
+    var hbars2 = dc.rowChart("#hbars2", "group");
+
+    // Dimensions
+    var amounts = ndx.dimension(function(d) {
+        return d.range
+      }),
+      amountByInvoices = amounts.group().reduceCount();
+
+    // Styling
+    var _count = amountByInvoices.size(),
+      _gap = 5, // DC default
+      _barHeight = 20;
+
+    hbars2
+      .height(hbars2.margins().top + hbars2.margins().bottom + (_count * _barHeight) + ((_count + 1) * _gap)) // NOTE: Margins top/bottom + bars + gaps (space between)
+      .fixedBarHeight(_barHeight)
+      .x(d3.scale.threshold())
+      .dimension(amounts)
+      .group(amountByInvoices)
+      .ordering(function(d) { return _r.range.indexOf(d.key) })
+      .labelOffsetX(-100)
+      .elasticX(true);
+
+    // Customize
+    hbars2.xAxis().ticks(5);
+    hbars2.margins().left = 100;
+
+    // Render
+    hbars2.render();
+
+    /*
+      TABLE FILTER - FULL PROVIDERS
+    */
+
+    $("#providers-table").jsGrid({
+      width: "100%",
+      height: "auto",
+      filtering: true,
+      sorting: true,
+      autoload: true,
+      // paging: true,
+      data: data,
+      fields: [{
+          name: "nif",
+          type: "text",
+          autosearch: true,
+          align: "center",
+          width: 30
+        },
+        {
+          name: "name",
+          type: "text",
+          autosearch: true,
+          width: 80
+        },
+        {
+          name: "date",
+          type: "text",
+          align: "center",
+          width: 30
+        },
+        {
+          name: "payed",
+          type: "checkbox",
+          align: "center",
+          width: 20
+        },
+        {
+          name: "amount",
+          type: "number",
+          width: 30
+        },
+        {
+          name: "concept",
+          type: "text",
+          autosearch: true
+        }
+      ]
+    });
+
+    // });
+  };
+
+  return InvoicesController;
+})();
+
+this.GobiertoBudgets.invoices_controller = new GobiertoBudgets.InvoicesController;

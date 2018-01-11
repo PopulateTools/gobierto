68a68f03b3055895249b62a4ef57c26bdd184804
Eduardo Martínez <eduardomech@gmail.com>
2018-01-11 21:41:59 +0100

    #1312: Change and extend sessions_controller test

--- test/controllers/user/sessions_controller_test.rb
+++ test/controllers/user/sessions_controller_test.rb
@@ -7,6 +7,10 @@ class User::SessionsControllerTest < GobiertoControllerTest
     @confirmed_user ||= users(:dennis)
   end
 
+  def other_site
+    @other_site ||= sites(:santander)
+  end
+
   def referrer_url
     "http://example.com/home"
   end
@@ -19,20 +23,36 @@ class User::SessionsControllerTest < GobiertoControllerTest
   end
 
   def test_create
-    post(
-      user_sessions_url,
-      params: { user_session: valid_session_params }
-    )
-    assert_redirected_to root_path
+    with_current_site(confirmed_user.site) do
+      post(
+        user_sessions_url,
+        params: { user_session: valid_session_params }
+      )
+      assert_redirected_to root_path
+    end
+  end
+
+  def test_create_in_other_site
+    with_current_site(other_site) do
+      post(
+        user_sessions_url,
+        params: { user_session: valid_session_params }
+      )
+      assert_response :success
+      assert_equal "The data you entered doesn't seem to be valid. Please try again.",
+        flash[:alert]
+    end
   end
 
   def test_create_with_referrer
-    post(
-      user_sessions_url,
-      params: {
-        user_session: valid_session_params.merge(referrer_url: referrer_url)
-      }
-    )
-    assert_redirected_to referrer_url
+    with_current_site(confirmed_user.site) do
+      post(
+        user_sessions_url,
+        params: {
+          user_session: valid_session_params.merge(referrer_url: referrer_url)
+        }
+      )
+      assert_redirected_to referrer_url
+    end
   end
 end

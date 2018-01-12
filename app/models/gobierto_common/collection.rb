3fbca4b8d01da2df854a74a2aa3701a3eb7b9ea1
Alberto Miedes Garcés <albertomg994@gmail.com>
2018-01-12 18:35:00 +0100

    Scope uniqueness validation of collection on site

--- app/models/gobierto_common/collection.rb
+++ app/models/gobierto_common/collection.rb
@@ -14,7 +14,7 @@ module GobiertoCommon
 
     validates :site, :title, :item_type, presence: true
     validates :slug, uniqueness: { scope: :site }
-    validates :container_id, uniqueness: { scope: [:container_type, :item_type] }
+    validates :container_id, uniqueness: { scope: [:site, :container_type, :item_type] }
 
     attr_reader :container
 

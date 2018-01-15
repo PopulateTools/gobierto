ac41e799144688a00b63459205e01d234e0a04b5
Adrian Pradilla <adrianpradilla@gmail.com>
2018-01-15 11:07:16 +0100

    Fix stage page form with select2 are duplicated when clicking back button on browser in rails 5

--- app/assets/javascripts/admin/app/controllers/process_stage_pages_controller.js
+++ app/assets/javascripts/admin/app/controllers/process_stage_pages_controller.js
@@ -7,6 +7,7 @@ this.GobiertoAdmin.ProcessStagePagesController = (function() {
   };
 
   function _selectPage() {
+    Turbolinks.clearCache();
     $(document).ready(function() {
       $("select#process_stage_page_page_id").select2({
         width: '100%'

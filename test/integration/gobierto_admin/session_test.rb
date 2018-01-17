f292d8f19b328dbe5bae41e9e6633dbfda3150f9
Eduardo Martínez <eduardomech@gmail.com>
2018-01-17 18:24:21 +0100

    #1312: Add some tests to check admin sessions depending on host

--- test/integration/gobierto_admin/session_test.rb
+++ test/integration/gobierto_admin/session_test.rb
@@ -9,6 +9,18 @@ module GobiertoAdmin
       @sign_in_path = new_admin_sessions_path
     end
 
+    def site
+      sites(:madrid)
+    end
+
+    def other_site
+      sites(:santander)
+    end
+
+    def unmanaged_site
+      sites(:huesca)
+    end
+
     def admin
       @admin ||= gobierto_admin_admins(:tony)
     end
@@ -54,6 +66,72 @@ module GobiertoAdmin
 
     def test_sign_in_when_already_signed_in
       with_signed_in_admin(admin) do
+        visit @sign_in_path
+
+        assert has_message?("You are already signed in")
+      end
+    end
+
+    def test_sign_in_and_visit_other_site
+      with_current_site_with_host(site) do
+        sign_in_admin(admin)
+        assert_equal site.name, find('div#current-site-name').text
+      end
+
+      with_current_site_with_host(other_site) do
+        visit @sign_in_path
+
+        assert has_content?("Log in")
+      end
+
+      with_current_site_with_host(site) do
+        visit @sign_in_path
+
+        assert has_message?("You are already signed in")
+      end
+    end
+
+    def test_sign_in_with_domain
+      with_site_host(site) do
+        sign_in_admin(admin)
+        assert_equal site.name, find('div#current-site-name').text
+        click_link "admin-sign-out"
+      end
+
+      with_site_host(other_site) do
+        sign_in_admin(admin)
+        assert_equal other_site.name, find('div#current-site-name').text
+        click_link "admin-sign-out"
+      end
+
+      with_site_host(unmanaged_site) do
+        sign_in_admin(admin)
+        assert_includes admin.sites.map(&:name), find('div#current-site-name').text
+        refute_equal unmanaged_site.name, find('div#current-site-name').text
+        click_link "admin-sign-out"
+      end
+
+      sign_in_admin(admin)
+      assert_includes admin.sites.map(&:name), find('div#current-site-name').text
+      click_link "admin-sign-out"
+    end
+
+    def test_sign_out
+      with_current_site_with_host(site) do
+        sign_in_admin(admin)
+      end
+
+      with_current_site_with_host(other_site) do
+        sign_in_admin(admin)
+      end
+
+      with_current_site_with_host(site) do
+        visit admin_root_path
+        click_link "admin-sign-out"
+        assert has_content?("Log in")
+      end
+
+      with_current_site_with_host(other_site) do
         visit @sign_in_path
 
         assert has_message?("You are already signed in")

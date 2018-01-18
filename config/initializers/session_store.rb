b30de3008c6a4d0cbaf5a2b9096b0327d73bd619
Eduardo Martínez <eduardomech@gmail.com>
2018-01-18 13:17:54 +0100

    #1312: Recover session_store domain: :all configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,4 +4,5 @@
 
 Rails.application.config.session_store :cookie_store,
                                        key: "_gobierto_session",
+                                       domain: :all,
                                        tld_length: ENV.fetch("TLD_LENGTH") { "2" }.to_i

31b0f81f881f188e2728b58c9cbd22b0fccbd797
Eduardo Martínez <eduardomech@gmail.com>
2018-01-23 16:01:30 +0100

    #213: Remove export action from budgets controller

--- app/controllers/gobierto_budgets/budgets_controller.rb
+++ app/controllers/gobierto_budgets/budgets_controller.rb
@@ -1,6 +1,6 @@
 class GobiertoBudgets::BudgetsController < GobiertoBudgets::ApplicationController
   before_action :load_place
-  before_action :load_year, except: [:guide, :export]
+  before_action :load_year, except: [:guide]
 
   def index
     @kind = GobiertoBudgets::BudgetLine::INCOME
@@ -21,43 +21,6 @@ class GobiertoBudgets::BudgetsController < GobiertoBudgets::ApplicationControlle
 
     @any_custom_income_budget_lines  = GobiertoBudgets::BudgetLine.any_data?(site: current_site, year: @year, kind: GobiertoBudgets::BudgetLine::INCOME, area: GobiertoBudgets::CustomArea)
     @any_custom_expense_budget_lines = GobiertoBudgets::BudgetLine.any_data?(site: current_site, year: @year, kind: GobiertoBudgets::BudgetLine::EXPENSE, area: GobiertoBudgets::CustomArea)
-  end
-
-  def export
-    year = params[:year].to_i
-
-    presenter = GobiertoBudgets::BudgetLineExportPresenter
-    indexes = presenter::INDEX_KEYS
-
-    place_budget_lines = []
-    GobiertoBudgets::BudgetLine.all_kinds. each do |kind|
-      GobiertoBudgets::BudgetArea.all_areas.each do |area|
-        indexes.each do |index, attribute|
-          if area.available_kinds.include?(kind)
-            index_budget_lines = GobiertoBudgets::BudgetLine.all(where: { year: year,
-                                                                          site: current_site,
-                                                                          place: @place,
-                                                                          area_name: area.area_name,
-                                                                          kind: kind,
-                                                                          index: index },
-                                                                 include: [:index, :updated_at],
-                                                                 presenter: presenter)
-            index_budget_lines.each do |line|
-              if idx = place_budget_lines.index { |global_line| global_line.id == line.id }
-                place_budget_lines[idx].merge!(line)
-              else
-                place_budget_lines << line
-              end
-            end
-          end
-        end
-      end
-    end
-
-    respond_to do |format|
-      format.json { render json: place_budget_lines }
-      format.csv { render csv: GobiertoExports::CSVRenderer.new(place_budget_lines).to_csv, filename: "budget_lines_#{year}" }
-    end
   end
 
   def guide

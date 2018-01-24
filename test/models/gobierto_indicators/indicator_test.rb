b3cb601565706f5a7c4f9de3500f95b0d24c372b
Adrian Pradilla <adrianpradilla@gmail.com>
2018-01-24 13:25:20 +0100

    Typo with indicator fixture

--- test/models/gobierto_indicators/indicator_test.rb
+++ test/models/gobierto_indicators/indicator_test.rb
@@ -5,7 +5,7 @@ require "test_helper"
 module GobiertoIndicator
   class IndicatorTest < ActiveSupport::TestCase
     def indicator
-      @indicator ||= gobierto_indicators_indicators(:ita)
+      @indicator ||= gobierto_indicators_indicators(:ita14)
     end
 
     def test_valid

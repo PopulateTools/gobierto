3899bef44f0020280eda2befac10759ebb0ebb48
Eduardo Martínez <eduardomech@gmail.com>
2018-02-01 09:46:58 +0100

    Add parent association between categories

--- app/models/gobierto_plans/category.rb
+++ app/models/gobierto_plans/category.rb
@@ -7,6 +7,9 @@ module GobiertoPlans
     belongs_to :plan
     has_and_belongs_to_many :nodes, dependent: :destroy, class_name: "GobiertoPlans::Node"
 
+    belongs_to :parent_category, class_name: "GobiertoPlans::Category", foreign_key: :parent_id
+    has_many :categories, foreign_key: :parent_id
+
     translates :name
   end
 end

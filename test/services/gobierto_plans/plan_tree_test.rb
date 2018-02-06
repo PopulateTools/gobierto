23edbc88f09d566b14f12c383364f337bb3d5bcf
Adrian Pradilla <adrianpradilla@gmail.com>
2018-02-06 12:15:31 +0100

    Add plan tests: descendats, uid, children_progress and plan_tree call output

--- test/services/gobierto_plans/plan_tree_test.rb
+++ test/services/gobierto_plans/plan_tree_test.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "support/populate_data_helpers"
 
 class GobiertoPlans::PlanTreeTest < ActiveSupport::TestCase
+  include PopulateDataHelpers
+
   def site
     @site ||= sites(:madrid)
   end
@@ -11,7 +14,15 @@ class GobiertoPlans::PlanTreeTest < ActiveSupport::TestCase
     @plan ||= gobierto_plans_plans(:strategic_plan)
   end
 
+  def plan_service
+    @plan_service ||= begin
+      GobiertoPlans::PlanTree.new(plan)
+    end
+  end
+
   def test_call
-    assert GobiertoPlans::PlanTree.new(plan).call
+    with_stubbed_populate_data_plan do
+      assert_equal [populate_data_plan], plan_service.call.to_json
+    end
   end
 end

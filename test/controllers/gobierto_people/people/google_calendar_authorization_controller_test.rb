dd87ff56926c027974eda3b0aa16d66a9963925a
Fernando Blat <ferblape@gmail.com>
2018-02-09 16:50:52 +0100

    Scope by current_site

--- test/controllers/gobierto_people/people/google_calendar_authorization_controller_test.rb
+++ test/controllers/gobierto_people/people/google_calendar_authorization_controller_test.rb
@@ -12,25 +12,29 @@ class GobiertoPeople::People::GoogleCalendar::AuthorizationControllerTest < Acti
   end
 
   def test_new_request_without_code
-    get :new, params: { token: person.google_calendar_token }
-    assert_equal person.id, session[:google_calendar_person_id]
-    assert_response :redirect
+    with_current_site(site) do
+      get :new, params: { token: person.google_calendar_token }
+      assert_equal person.id, session[:google_calendar_person_id]
+      assert_response :redirect
+    end
   end
 
   def test_new_request_with_code
-    client_secrets = mock
-    auth_client = mock
+    with_current_site(site) do
+      client_secrets = mock
+      auth_client = mock
 
-    client_secrets.stubs(to_authorization: auth_client)
-    auth_client.stubs(update!: true, fetch_access_token!: true, to_json: "json")
-    auth_client.stubs(:code=, true)
-    auth_client.stubs(:client_secret=, true)
-    Google::APIClient::ClientSecrets.stubs(:load).returns(client_secrets)
+      client_secrets.stubs(to_authorization: auth_client)
+      auth_client.stubs(update!: true, fetch_access_token!: true, to_json: "json")
+      auth_client.stubs(:code=, true)
+      auth_client.stubs(:client_secret=, true)
+      Google::APIClient::ClientSecrets.stubs(:load).returns(client_secrets)
 
-    get :new, params: { code: "foo" }, session: { google_calendar_person_id: person.id }
-    assert_response :redirect
+      get :new, params: { code: "foo" }, session: { google_calendar_person_id: person.id }
+      assert_response :redirect
 
-    configuration = GobiertoCalendars::GoogleCalendarConfiguration.find_by(collection: person.calendar)
-    assert configuration.data["google_calendar_credentials"].present?
+      configuration = GobiertoCalendars::GoogleCalendarConfiguration.find_by(collection: person.calendar)
+      assert configuration.data["google_calendar_credentials"].present?
+    end
   end
 end

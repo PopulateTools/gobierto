9db2208808d38c157ea5c2edd1290291e4c7fc1d
Fernando Blat <ferblape@gmail.com>
2018-02-09 15:54:16 +0100

    Load plans data in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,11 @@ fixtures_to_load = [
   "gobierto_participation/areas",
   "gobierto_common/collection_items",
   "gobierto_common/scopes",
-  "gobierto_admin/permissions"
+  "gobierto_admin/permissions",
+  "gobierto_plans/plan_types",
+  "gobierto_plans/plans",
+  "gobierto_plans/categories",
+  "gobierto_plans/nodes"
 ]
 
 ENV["FIXTURES"] = fixtures_to_load.join(",")

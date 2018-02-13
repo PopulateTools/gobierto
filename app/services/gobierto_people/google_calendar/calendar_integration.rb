fa95a0f212d488eaee874121a3299e3f97b2e8d5
Fernando Blat <ferblape@gmail.com>
2018-02-13 15:34:59 +0100

    Refactor class

--- app/services/gobierto_people/google_calendar/calendar_integration.rb
+++ app/services/gobierto_people/google_calendar/calendar_integration.rb
@@ -6,8 +6,8 @@ require 'fileutils'
 module GobiertoPeople
   module GoogleCalendar
     class CalendarIntegration
-      CLIENT_SECRETS_PATH = Rails.root.join('config', 'google_calendar_integration_client_secret.json')
-      USERNAME = 'default'
+      CLIENT_SECRETS_PATH = Rails.root.join("config", "google_calendar_integration_client_secret.json")
+      USERNAME = "default"
       SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
 
       def self.sync_person_events(person)
@@ -16,19 +16,8 @@ module GobiertoPeople
 
       def sync!
         set_google_calendar_id!
-
-        received_events_ids = []
-        service.list_calendar_lists(max_results: 100).items.each do |calendar|
-          if configuration.calendars.present? && configuration.calendars.include?(calendar.id)
-            received_events_ids.concat sync_calendar_events(calendar)
-          end
-        end
-
-        person.events.upcoming.synchronized.each do |event|
-          unless received_events_ids.include?(event.external_id)
-            event.pending!
-          end
-        end
+        import_events!
+        delete_unreceived_events!
       end
 
       def calendars
@@ -44,9 +33,10 @@ module GobiertoPeople
         @service = Google::Apis::CalendarV3::CalendarService.new
         @service.client_options.application_name = person.site.name
         @service.authorization = authorize(person)
+        @received_event_ids = []
       end
 
-      attr_reader :person, :configuration, :service
+      attr_reader :person, :configuration, :service, :received_event_ids
 
       def set_google_calendar_id!
         if @configuration.google_calendar_id.nil?
@@ -56,24 +46,18 @@ module GobiertoPeople
       end
 
       def sync_calendar_events(calendar)
-        received_events_ids = []
-
         response = service.list_events(calendar.id, always_include_email: true, time_min: GobiertoCalendars.sync_range_start.iso8601)
         response.items.each do |event|
-          received_events_ids.push event.id
           next if is_private?(event)
 
           if is_recurring?(event)
             service.list_event_instances(calendar.id, event.id).items.each_with_index do |event, i|
-              received_events_ids.push event.id
               sync_event(event, i)
             end
           else
             sync_event(event)
           end
         end
-
-        received_events_ids
       end
 
       def is_private?(event)
@@ -130,7 +114,10 @@ module GobiertoPeople
         if filter_result.action == GobiertoCalendars::FilteringRuleApplier::REMOVE
           GobiertoPeople::PersonEventForm.new(event_params).destroy
         else
-          unless GobiertoPeople::PersonEventForm.new(event_params).save
+          gobierto_event = GobiertoPeople::PersonEventForm.new(event_params)
+          if gobierto_event.save
+            received_event_ids.push(event.id)
+          else
             Rails.logger.info "[Google Calendar Integration] Invalid event: #{event_params}"
           end
         end
@@ -151,6 +138,20 @@ module GobiertoPeople
         if time_attribute
           time_attribute.date_time || DateTime.parse(time_attribute.date)
         end
+      end
+
+      def import_events!
+        service.list_calendar_lists(max_results: 100).items.each do |calendar|
+          if configuration.calendars.present? && configuration.calendars.include?(calendar.id)
+            sync_calendar_events(calendar)
+          end
+        end
+      end
+
+      def delete_unreceived_events!
+        person.events.upcoming.synchronized.
+          where.not(external_id: received_event_ids).
+          each(&:pending!)
       end
     end
   end

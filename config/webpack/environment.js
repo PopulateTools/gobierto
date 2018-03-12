18b356c20644110b6fb4a33ce289ac083d31b134
Hugoren Martinako <aumpfbahn@gmail.com>
2018-03-12 12:32:28 +0100

    admin jquery environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+  })
+)
 
 module.exports = environment

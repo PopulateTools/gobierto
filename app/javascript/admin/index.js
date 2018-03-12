93ff4882f0d28868c545effc95b2c5ef4c94c50d
Hugoren Martinako <aumpfbahn@gmail.com>
2018-03-12 14:38:40 +0100

    dependencies

--- app/javascript/admin/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('./modules/application.js')
-const search = require('./modules/module-admin-search.js')
-const admin = require('./modules/module-admin.js')
-
-module.exports = { app, admin, search }

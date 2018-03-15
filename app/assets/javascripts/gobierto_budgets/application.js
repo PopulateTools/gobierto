f6a6cf91044ecc47db4e43ea2eeeb1de37e2bce5
Adrian Pradilla <adrianpradilla@gmail.com>
2018-03-15 13:09:57 +0100

    Review filter by year in gobierto budgets

--- app/assets/javascripts/gobierto_budgets/application.js
+++ app/assets/javascripts/gobierto_budgets/application.js
@@ -9,46 +9,46 @@
 
 $(document).on('turbolinks:load', function() {
 
-  var grossSavingCard = new GrossSavingCard('.gross_saving_card', window.populateData.municipalityId);
+  var grossSavingCard = new GrossSavingCard('.gross_saving_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   grossSavingCard.render();
 
-  var netSavingCard = new NetSavingCard('.net_saving_card', window.populateData.municipalityId);
+  var netSavingCard = new NetSavingCard('.net_saving_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   netSavingCard.render();
 
-  var taxAutonomyCard = new TaxAutonomyCard('.tax_autonomy_card', window.populateData.municipalityId);
+  var taxAutonomyCard = new TaxAutonomyCard('.tax_autonomy_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   taxAutonomyCard.render();
 
-  var selfFinancingCapacityCard = new SelfFinancingCapacityCard('.self_financing_capacity_card', window.populateData.municipalityId);
+  var selfFinancingCapacityCard = new SelfFinancingCapacityCard('.self_financing_capacity_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   selfFinancingCapacityCard.render();
 
-  var financialChargeCard = new FinancialChargeCard('.financial_charge_card', window.populateData.municipalityId);
+  var financialChargeCard = new FinancialChargeCard('.financial_charge_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   financialChargeCard.render();
 
-  var liabilityCostCard = new LiabilityCostCard('.liability_cost_card', window.populateData.municipalityId);
+  var liabilityCostCard = new LiabilityCostCard('.liability_cost_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   liabilityCostCard.render();
 
-  var investmentFinancingCard = new InvestmentFinancingCard('.investment_financing_card', window.populateData.municipalityId);
+  var investmentFinancingCard = new InvestmentFinancingCard('.investment_financing_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   investmentFinancingCard.render();
 
-  var grossSavingsRateCard = new GrossSavingsRateCard('.gross_savings_rate_card', window.populateData.municipalityId);
+  var grossSavingsRateCard = new GrossSavingsRateCard('.gross_savings_rate_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   grossSavingsRateCard.render();
 
-  var netSavingsRateCard = new NetSavingsRateCard('.net_savings_rate_card', window.populateData.municipalityId);
+  var netSavingsRateCard = new NetSavingsRateCard('.net_savings_rate_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   netSavingsRateCard.render();
 
-  var debtLevelCard = new DebtLevelCard('.debt_level_card', window.populateData.municipalityId);
+  var debtLevelCard = new DebtLevelCard('.debt_level_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   debtLevelCard.render();
 
-  var perCapitaInvestmentCard = new PerCapitaInvestmentCard('.per_capita_investment_card', window.populateData.municipalityId);
+  var perCapitaInvestmentCard = new PerCapitaInvestmentCard('.per_capita_investment_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   perCapitaInvestmentCard.render();
 
-  var perCapitaTaxBurdenCard = new PerCapitaTaxBurdenCard('.per_capita_tax_burden_card', window.populateData.municipalityId);
+  var perCapitaTaxBurdenCard = new PerCapitaTaxBurdenCard('.per_capita_tax_burden_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   perCapitaTaxBurdenCard.render();
 
-  var financialRiskCard = new FinancialRiskCard('.financial_risk_card', window.populateData.municipalityId);
+  var financialRiskCard = new FinancialRiskCard('.financial_risk_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   financialRiskCard.render();
 
-  var expenditureRigidityCard = new ExpenditureRigidityCard('.expenditure_rigidity_card', window.populateData.municipalityId);
+  var expenditureRigidityCard = new ExpenditureRigidityCard('.expenditure_rigidity_card', window.populateData.municipalityId, window.populateDataYear.currentYear);
   expenditureRigidityCard.render();
 
   if(isDesktop()) {
--- app/controllers/gobierto_budgets/indicators_controller.rb
+++ app/controllers/gobierto_budgets/indicators_controller.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 class GobiertoBudgets::IndicatorsController < GobiertoBudgets::ApplicationController
+  before_action :load_year
+
   def index
   end
 
   private
 
+  def load_year
+    if params[:year].nil?
+      redirect_to gobierto_budgets_indicators_path(GobiertoBudgets::SearchEngineConfiguration::Year.last)
+    else
+      @year = params[:year].to_i
+    end
+  end
 end

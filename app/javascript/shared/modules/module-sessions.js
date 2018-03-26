43b8bd299578ebaa0677bb6e29d5920855879a52
Hugoren Martinako <aumpfbahn@gmail.com>
2018-03-26 09:26:44 +0200

    missing modules

---
+++ app/javascript/shared/modules/module-sessions.js
@@ -0,0 +1,15 @@
+$(document).on('turbolinks:load', function() {
+
+	$('.show_login_box').click(function(e) {
+		e.preventDefault();
+		$('.password_recovery_box').addClass("hidden");
+		$('.login_box').removeClass('hidden').velocity("transition.slideLeftIn");
+	});
+
+	$('.show_password_recovery_box').click(function(e) {
+		e.preventDefault();
+		$('.login_box').addClass("hidden");
+		$('.password_recovery_box').removeClass('hidden').velocity("transition.slideLeftIn");
+	});
+
+});

c511d4ca9a14a6988c91fe8d1c5b7b43c7da1522
Fernando Blat <ferblape@gmail.com>
2018-04-04 12:47:44 +0200

    New way to declare I18n fallbacks

--- config/initializers/translations.rb
+++ config/initializers/translations.rb
@@ -34,7 +34,5 @@ if Translation.table_exists?
   I18n.backend = I18n::Backend::Chain.new(I18n.backend, I18n::Backend::Simple.new)
 end
 
-I18n.fallbacks.map(ca: :es)
-I18n.fallbacks.map(es: :ca)
-I18n.fallbacks.map(en: :es)
+I18n.fallbacks = {ca: [:es, :en], es: [:ca, :en], en: [:es, :ca]}
 

0487d765ecb6decaf99d2968eb0fb7fb0029b550
Alberto Miedes <albertomg994@gmail.com>
2018-04-13 14:21:13 +0200

    Strip HTML comments from imported events

---
+++ test/decorators/gobierto_calendars/event_decorator_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module GobiertoCalendars
+  class EventDecoratorTest < ActiveSupport::TestCase
+
+    def event
+      @event ||= gobierto_calendars_events(:richard_published)
+    end
+
+    def test_formatted_html_description
+      description = '<script>alert(1);</script><!-- C1 -->Content<!-- C2 -->More content<!-- C3 -->'
+      event.update(description: description)
+      decorator = EventDecorator.new(event)
+      expected_output = '<p>alert(1);ContentMore content</p>'
+
+      assert_equal expected_output, decorator.formatted_html_description
+    end
+
+  end
+end

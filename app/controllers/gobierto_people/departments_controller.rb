8cc79d431140159e13acb5c0f0c359a8e1ee381b
Eduardo Martínez <eduardomech@gmail.com>
2018-06-14 11:50:12 +0200

    Reduce departments and interest_groups views and controllers

--- app/controllers/gobierto_people/departments_controller.rb
+++ app/controllers/gobierto_people/departments_controller.rb
@@ -2,29 +2,20 @@
 
 module GobiertoPeople
   class DepartmentsController < GobiertoPeople::ApplicationController
+    DEFAULT_LIMIT = 10
     before_action :check_active_submodules
 
     def index
-      @departments = @site.departments
+      @departments_count = @site.departments.count
+      @departments = @site.departments.joins(:events).group(:id).order("count(#{ events_table }.id) DESC").limit(DEFAULT_LIMIT)
       @total_events = events_with_department.count
       @total_people = events_with_department.select(:collection_id).distinct.count
-      @events_by_department = transform_id_keys(events_with_department.group(:department_id).order(count: :desc).count, departments_model)
-      @people_by_department = transform_id_keys(events_with_department.select(:collection_id).distinct.group(:department_id).order("count_collection_id desc").count, departments_model)
-
-      monthly_columns = [:department_id, "extract(year from starts_at)", "extract(month from starts_at)"]
-      @monthly_events_grouped_by_department = transform_id_keys(events_with_department.group(*monthly_columns).count.group_by { |key, _| key[0] }, departments_model)
-      @monthly_events_grouped_by_department.transform_values! { |value| base_punchcard_dates.merge(value.to_h.transform_keys { |key| Date.new(key[1], key[2]) }) }
     end
 
     def show
       @department = @site.departments.find(params[:id])
       @total_events = @department.events.count
       @total_people = @department.events.select(:collection_id).distinct.count
-      @events_by_people = transform_id_keys(@department.events.person_events.group(:container_id).order(count: :desc).count, people_model)
-
-      monthly_columns = [:container_id, "extract(year from starts_at)", "extract(month from starts_at)"]
-      @monthly_events_grouped_by_person = transform_id_keys(@department.events.person_events.group(*monthly_columns).count.group_by { |key, _| key[0] }, people_model)
-      @monthly_events_grouped_by_person.transform_values! { |value| base_punchcard_dates.merge(value.to_h.transform_keys { |key| Date.new(key[1], key[2]) }) }
     end
 
     def check_active_submodules
@@ -33,23 +24,8 @@ module GobiertoPeople
 
     protected
 
-    def base_punchcard_dates
-      @base_punchcard_dates ||= begin
-                                  first_date = events_with_department.order(starts_at: :asc).first.starts_at
-                                  last_date = events_with_department.order(starts_at: :asc).last.starts_at
-                                  combinations = [*(first_date.year..last_date.year)].product([*(1..12)])
-                                  combinations.slice!(0, first_date.month - 1)
-                                  combinations.slice!(combinations.length - 12 + last_date.month, 12 - last_date.month)
-                                  combinations.map { |combination| [Date.new(*combination), 0] }.to_h
-                                end
-    end
-
-    def transform_id_keys(hash, model)
-      hash.transform_keys! { |id| model.find(id) }
-    end
-
     def events_with_department
-      @site.events.where.not(department_id: nil).where("starts_at >= ?", 10.years.ago)
+      @site.events.where.not(department_id: nil)
     end
 
     def events_model
@@ -58,18 +34,6 @@ module GobiertoPeople
 
     def events_table
       events_model.table_name
-    end
-
-    def departments_model
-      GobiertoPeople::Department
-    end
-
-    def departments_table
-      departments_model.table_name
-    end
-
-    def people_model
-      GobiertoPeople::Person
     end
   end
 end
--- app/controllers/gobierto_people/interest_groups_controller.rb
+++ app/controllers/gobierto_people/interest_groups_controller.rb
@@ -2,21 +2,19 @@
 
 module GobiertoPeople
   class InterestGroupsController < GobiertoPeople::ApplicationController
+    DEFAULT_LIMIT = 10
     before_action :check_active_submodules
 
     def index
       @interest_groups_count = @site.interest_groups.count
-      @interest_groups = @site.interest_groups.limit(default_limit)
+      @interest_groups = @site.interest_groups.joins(:events).group(:id).order("count(#{ events_table }.id) DESC").limit(DEFAULT_LIMIT)
       @total_events = events_with_interest_group.count
-      @events_by_interest_group = transform_id_keys(events_with_interest_group.group(:interest_group_id).order(count: :desc).limit(default_limit).count, interest_groups_model)
     end
 
     def show
-      @interest_group = @site.interest_groups.find(params[:id])
+      @interest_group = @site.interest_groups.find_by_slug!(params[:id])
       @total_events = @interest_group.events.count
       @total_people = @interest_group.events.select(:collection_id).distinct.count
-      @events_by_department = transform_id_keys(@interest_group.events.person_events.group(:department_id).order(count: :desc).count, department_model)
-      @events_by_people = transform_id_keys(@interest_group.events.person_events.group(:container_id).order(count: :desc).count, people_model)
     end
 
     def check_active_submodules
@@ -25,16 +23,8 @@ module GobiertoPeople
 
     protected
 
-    def transform_id_keys(hash, model)
-      hash.transform_keys! { |id| model.find(id) }
-    end
-
     def events_with_interest_group
-      @site.events.where.not(interest_group_id: nil).where("starts_at >= ?", 10.years.ago)
-    end
-
-    def default_limit
-      10
+      @site.events.where.not(interest_group_id: nil)
     end
 
     def events_model
@@ -43,22 +33,6 @@ module GobiertoPeople
 
     def events_table
       events_model.table_name
-    end
-
-    def interest_groups_model
-      GobiertoPeople::InterestGroup
-    end
-
-    def interest_groups_table
-      interest_groups_model.table_name
-    end
-
-    def people_model
-      GobiertoPeople::Person
-    end
-
-    def department_model
-      GobiertoPeople::Department
     end
   end
 end

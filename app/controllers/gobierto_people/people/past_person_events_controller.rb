c0f72f174b2e8b570451bd041afc9a5bde7b7379
Alberto Miedes <albertomg994@gmail.com>
2018-06-20 12:09:23 +0200

    Permit non-pagination and avoid N+1 on person_events for custom engines

--- app/controllers/gobierto_people/people/past_person_events_controller.rb
+++ app/controllers/gobierto_people/people/past_person_events_controller.rb
@@ -8,6 +8,9 @@ module GobiertoPeople
         if params[:date]
           @filtering_date = Date.parse(params[:date])
           @events = @person.events.by_date(@filtering_date).sorted.page params[:page]
+        elsif params[:page] == "false"
+          # permit non-pagination and avoid N+1 queries for custom engines
+          @events = @person.events.past.sorted.includes(:interest_group)
         else
           @events = @person.events.past.sorted.page params[:page]
         end

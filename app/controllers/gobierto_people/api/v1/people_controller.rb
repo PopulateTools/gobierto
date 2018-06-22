96f4947a205fd480ac66a39b657de37e42f8111c
Fernando Blat <ferblape@gmail.com>
2018-06-22 16:17:10 +0200

    Add URLs to history api

--- app/controllers/gobierto_people/api/v1/people_controller.rb
+++ app/controllers/gobierto_people/api/v1/people_controller.rb
@@ -23,14 +23,25 @@ module GobiertoPeople
 
             records.each do |record|
               if (index = result_indexes[record.name])
-                result[index][:value] << { key: Time.zone.parse(record.year_month), value: record.custom_events_count }
+                result[index][:value] << {
+                  key: Time.zone.parse(record.year_month),
+                  value: record.custom_events_count,
+                  url: gobierto_people_person_past_events_path(record.slug, {page: false}.merge(date_range(record.year_month)))
+                }
               else
                 result_indexes[record.name] = result.size
                 result << {
                   key: record.name,
                   value: [
-                    { key: Time.zone.parse(record.year_month), value: record.custom_events_count }
-                  ]
+                    {
+                      key: Time.zone.parse(record.year_month),
+                      value: record.custom_events_count,
+                      url: gobierto_people_person_past_events_path(record.slug, {page: false}.merge(date_range(record.year_month)))
+                    }
+                  ],
+                  properties: {
+                    url: gobierto_people_person_past_events_path(record.slug, page: false)
+                  }
                 }
               end
             end
@@ -64,6 +75,13 @@ module GobiertoPeople
             :from_date,
             :to_date
           ).to_h
+        end
+
+        def date_range(year_month)
+          {
+            start_date: Time.zone.parse(year_month).to_date.to_s(:db),
+            end_date: (Time.zone.parse(year_month).to_date + 1.month).to_s(:db)
+          }
         end
 
       end

630373a23ef42ee52030523a9b3ffc5b9cde0e93
Eduardo Martínez <eduardomech@gmail.com>
2018-07-26 00:03:04 +0200

    Change controllers and helpers to take political_group of people from vocabulary

--- app/controllers/concerns/gobierto_people/political_groups_helper.rb
+++ app/controllers/concerns/gobierto_people/political_groups_helper.rb
@@ -6,7 +6,8 @@ module GobiertoPeople
 
     def get_political_groups
       person_table = Person.table_name
-      PoliticalGroup.includes(:people).where.not(person_table => { political_group_id: nil }).where(person_table => { site_id: current_site.id, visibility_level: 1 }).all
+      join_clause = "JOIN #{person_table} ON #{person_table}.political_group_id = terms.id"
+      current_site.political_groups.distinct.joins(join_clause).where.not(person_table => { political_group_id: nil }).where(person_table => { site_id: current_site.id, visibility_level: 1 }).all
     end
   end
 end

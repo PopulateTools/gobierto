99d59751e9f2d27e2e887d04d3c63ee037643c46
Eduardo Martínez <eduardomech@gmail.com>
2018-08-02 14:33:03 +0200

    Update migration to work with vocabularies validations

--- db/migrate/20180801142224_add_name_translations_and_slug_to_vocabularies.rb
+++ db/migrate/20180801142224_add_name_translations_and_slug_to_vocabularies.rb
@@ -5,7 +5,7 @@ class AddNameTranslationsAndSlugToVocabularies < ActiveRecord::Migration[5.2]
     add_column :vocabularies, :name_translations, :jsonb
     add_column :vocabularies, :slug, :string
     GobiertoCommon::Vocabulary.all.each do |vocabulary|
-      vocabulary.update(name_translations: localized_name_attr(vocabulary), slug: slug(vocabulary))
+      vocabulary.update_columns(name_translations: localized_name_attr(vocabulary), slug: slug(vocabulary))
     end
     remove_column :vocabularies, :name
   end
@@ -13,18 +13,18 @@ class AddNameTranslationsAndSlugToVocabularies < ActiveRecord::Migration[5.2]
   def down
     add_column :vocabularies, :name, :string
     GobiertoCommon::Vocabulary.all.each do |vocabulary|
-      vocabulary.update(name: delocalized_name_attr(vocabulary))
+      vocabulary.update_columns(name: delocalized_name_attr(vocabulary))
     end
     remove_column :vocabularies, :slug
     remove_column :vocabularies, :name_translations
   end
 
   def localized_name_attr(vocabulary)
-    { vocabulary.site.configuration.default_locale => vocabulary.name }
+    { vocabulary.site.configuration.default_locale => vocabulary.attributes["name"] }
   end
 
   def slug(vocabulary)
-    slug_core = vocabulary.name.tr(" ", "_").parameterize
+    slug_core = vocabulary.attributes["name"].tr(" ", "_").parameterize
     return slug_core unless vocabulary.site.vocabularies.where(slug: slug_core).exists?
     "#{ slug_core }-#{ vocabulary.site.vocabularies.where(slug: slug_core).count + 1 }"
   end

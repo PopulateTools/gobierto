5ee0862adfffbb0097906fbd3b960c4559182aa7
Hugoren Martinako <aumpfbahn@gmail.com>
2018-08-06 10:55:42 +0200

    turn code to d3v4
    update dependencies

--- app/javascript/shared/index.js
+++ app/javascript/shared/index.js
@@ -22,7 +22,7 @@ import Cleave from 'cleave.js'
 import Vue from 'vue'
 // https://www.giacomodebidda.com/how-to-import-d3-plugins-with-webpack/
 import * as d3v4Base from 'd3v4'
-import * as d3v3Base from 'd3' // required to run dc.js
+// import * as d3v3Base from 'd3' // required to run dc.js
 import d3Legend from 'd3-svg-legend'
 import { wordwrap, parseAttributes, f, ascendingKey, descendingKey, conventions, drawAxis, attachTooltip, loadData, nestBy, round, clamp, polygonClip } from 'd3-jetpack' // NOTE: some methods returned conflict with d3v4, so must select
 import * as flight from 'flightjs'
@@ -36,13 +36,13 @@ Turbolinks.start()
 Vue.config.productionTip = false
 
 const d3 = Object.assign(d3v4Base, d3Legend, { wordwrap, parseAttributes, f, ascendingKey, descendingKey, conventions, drawAxis, attachTooltip, loadData, nestBy, round, clamp, polygonClip });
-const d3v3 = Object.assign(d3v3Base, d3Legend, { wordwrap, parseAttributes, f, ascendingKey, descendingKey, conventions, drawAxis, attachTooltip, loadData, nestBy, round, clamp, polygonClip });
+// const d3v3 = Object.assign(d3v3Base, d3Legend, { wordwrap, parseAttributes, f, ascendingKey, descendingKey, conventions, drawAxis, attachTooltip, loadData, nestBy, round, clamp, polygonClip });
 
 export {
   AUTOCOMPLETE_DEFAULTS,
   Class,
   d3,
-  d3v3,
+  // d3v3,
   d3locale,
   accounting,
   moment,

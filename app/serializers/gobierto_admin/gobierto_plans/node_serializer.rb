27a52149fa09c8eb41080feb45680b5a61f33fb3
Eduardo Martínez <eduardomech@gmail.com>
2018-08-14 00:34:22 +0200

    Add serializers for plan nodes and categories

---
+++ app/serializers/gobierto_admin/gobierto_plans/node_serializer.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module GobiertoAdmin
+  module GobiertoPlans
+    class NodeSerializer < ActiveModel::Serializer
+      attributes(
+        :id,
+        :categories_hierarchy,
+        :name_translations,
+        :progress,
+        :status_translations,
+        :starts_at,
+        :ends_at,
+        :options,
+        :created_at,
+        :updated_at
+      )
+
+      def category
+        @category ||= base_categories_relation.where(cnt: { node_id: object.id }).first
+      end
+
+      def categories_hierarchy
+        @categories_hierarchy ||= begin
+                                  [category].tap do |hierarchy|
+                                    while (parent = hierarchy.first.parent_term).present?
+                                      hierarchy.unshift(parent)
+                                    end
+                                  end.pluck(:id)
+                                end
+      end
+
+      private
+
+      def base_categories_relation
+        (instance_options[:plan] || object).categories.joins(Arel.sql("JOIN #{ categories_nodes_table } AS cnt ON cnt.category_id = #{ categories_table }.id"))
+      end
+
+      def categories_nodes_table
+        ::GobiertoPlans::CategoriesNode.table_name
+      end
+
+      def categories_table
+        ::GobiertoCommon::Term.table_name
+      end
+    end
+  end
+end

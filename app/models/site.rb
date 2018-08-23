c55bf53c1f8ed79dbab435ebeeaa05da6267be5f
Fernando Blat <ferblape@gmail.com>
2018-08-23 16:44:25 +0200

    Fix master tests

--- app/models/site.rb
+++ app/models/site.rb
@@ -75,7 +75,7 @@ class Site < ApplicationRecord
   serialize :configuration_data
 
   before_save :store_configuration
-  before_create :initialize_admins
+  after_create :initialize_admins
   after_save :run_seeder
 
   validates :title, presence: true
@@ -197,7 +197,12 @@ class Site < ApplicationRecord
   end
 
   def initialize_admins
-    self.admins = Array(GobiertoAdmin::Admin.preset)
+    preset_admin = GobiertoAdmin::Admin.preset
+    if preset_admin.new_record?
+      self.admins.create(preset_admin.attributes)
+    else
+      self.admins << preset_admin
+    end
   end
 
   def run_seeder

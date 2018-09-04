276a86473484840c2b37741fa14c219a322f4ec0
Hugoren Martinako <aumpfbahn@gmail.com>
2018-09-04 14:56:52 +0200

    woof!

--- app/javascript/plans/modules/plan_types_controller.js
+++ app/javascript/plans/modules/plan_types_controller.js
@@ -275,53 +275,7 @@ window.GobiertoPlans.PlanTypesController = (function() {
             return result
           }
         }
-      });
-
-      // module pattern
-      const UUID = (function() {
-        // Maps for number <-> hex string conversion
-        var _byteToHex = [];
-        var _hexToByte = {};
-        for (var i = 0; i < 256; i++) {
-          _byteToHex[i] = (i + 0x100).toString(16).substr(1);
-          _hexToByte[_byteToHex[i]] = i;
-        }
-
-        return {
-          // **`parse()` - Parse a UUID into it's component bytes**
-          parse(s, buf, offset) {
-            const i = (buf && offset) || 0;
-            let ii = 0;
-
-            buf = buf || [];
-            s.toLowerCase().replace(/[0-9a-f]{2}/g, function(oct) {
-              if (ii < 16) { // Don't overflow!
-                buf[i + ii++] = _hexToByte[oct];
-              }
-            });
-
-            // Zero out remaining bytes if string was short
-            while (ii < 16) {
-              buf[i + ii++] = 0;
-            }
-
-            return buf;
-          },
-          // **`unparse()` - Convert UUID byte array (ala parse()) into a string**
-          unparse(buf, offset) {
-            let i = offset || 0;
-            const bth = _byteToHex;
-            return bth[buf[i++]] + bth[buf[i++]] +
-              bth[buf[i++]] + bth[buf[i++]] + '-' +
-              bth[buf[i++]] + bth[buf[i++]] + '-' +
-              bth[buf[i++]] + bth[buf[i++]] + '-' +
-              bth[buf[i++]] + bth[buf[i++]] + '-' +
-              bth[buf[i++]] + bth[buf[i++]] +
-              bth[buf[i++]] + bth[buf[i++]] +
-              bth[buf[i++]] + bth[buf[i++]];
-          }
-        }
-      })()
+      })
 
       function findNodeByProp(id, currentNode, prop = 'id') {
         var i,

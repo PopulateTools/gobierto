a21269077f2d1825f4cd77255c7174f0b6d6a97a
Hugoren Martinako <aumpfbahn@gmail.com>
2018-09-11 13:37:49 +0200

    plans dependencies OK
    - tests passing

--- app/javascript/plans/index.js
+++ app/javascript/plans/index.js
@@ -1,13 +1,17 @@
 // external dependencies
 import $ from 'jquery'
+import 'jquery-ujs'
 import * as I18n from 'i18n-js'
+import 'babel-polyfill' // NOTE: REQUIRED to use ES6 syntax (https://github.com/rails/webpacker/issues/523)
 import algoliasearch from 'algoliasearch'
+import Turbolinks from 'turbolinks'
 
 global.$ = global.jQuery = $; // NOTE: jQuery exposed to global (window for node environment) due to script directly in the view
 global.I18n = I18n
 global.algoliasearch = algoliasearch
+Turbolinks.start()
 
+// local dependencies
 import './modules/init.js'
-// First module
 import './modules/application.js'
 import './modules/plan_types_controller.js'

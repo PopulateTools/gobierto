a1178412ad8f932101b7bc3835e39f2342da5e1a
Hugoren Martinako <aumpfbahn@gmail.com>
2018-09-13 14:39:29 +0200

    missing dep

--- app/javascript/commons/index.js
+++ app/javascript/commons/index.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import 'jquery-ujs'
 import * as I18n from 'i18n-js'
 import algoliasearch from 'algoliasearch'
 import Turbolinks from 'turbolinks'
@@ -9,7 +10,7 @@ global.I18n = I18n
 global.algoliasearch = algoliasearch
 Turbolinks.start()
 
-// TODO: Esto podría ser dividido en pequeños módulos para inyectar solo las cosas necesarias 
+// TODO: Esto podría ser dividido en pequeños módulos para inyectar solo las cosas necesarias
 import 'shared'
 
 // OLD VERSIONS

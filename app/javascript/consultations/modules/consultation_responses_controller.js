3949a33a8020515f1da406937a942d01bc4ff7d2
Hugoren Martinako <aumpfbahn@gmail.com>
2018-09-24 11:33:48 +0200

    missing lib

--- app/javascript/consultations/modules/consultation_responses_controller.js
+++ app/javascript/consultations/modules/consultation_responses_controller.js
@@ -1,5 +1,6 @@
 import { isMobile } from 'shared'
 import Vue from 'vue'
+import 'sticky-kit/dist/sticky-kit.js'
 
 window.GobiertoBudgetConsultations.ConsultationResponsesController = (function() {
   function ConsultationResponsesController() {}

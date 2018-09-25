54524870a804ebe8c259f36b3c06b269916736a2
Alberto Miedes <albertomg994@gmail.com>
2018-09-25 15:23:14 +0200

    Fix issues with GobiertoCms::Page datepicker

--- app/forms/gobierto_admin/gobierto_cms/page_form.rb
+++ app/forms/gobierto_admin/gobierto_cms/page_form.rb
@@ -53,10 +53,6 @@ module GobiertoAdmin
         @visibility_level ||= "draft"
       end
 
-      def published_on
-        @published_on ||= Time.zone.now
-      end
-
       private
 
       def build_page

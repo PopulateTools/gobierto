24633e32d00392d70f22540c36328d43c60aecd8
Hugoren Martinako <aumpfbahn@gmail.com>
2018-10-04 15:04:00 +0200

    init module

---
+++ app/javascript/charters/modules/charters_controller.js
@@ -0,0 +1,12 @@
+window.GobiertoCharters.ChartersController = (function() {
+
+  function ChartersController() {}
+
+  ChartersController.prototype.index = function(){
+
+  };
+
+  return ChartersController;
+})();
+
+window.GobiertoCharters.charters_controller = new GobiertoCharters.ChartersController;

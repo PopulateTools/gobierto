33168c43c85bf15173233a303bbc063e78b52cf6
Fernando Blat <ferblape@gmail.com>
2018-10-09 11:11:22 +0200

    Disable 2017

--- app/controllers/gobierto_budgets/indicators_controller.rb
+++ app/controllers/gobierto_budgets/indicators_controller.rb
@@ -21,7 +21,7 @@ class GobiertoBudgets::IndicatorsController < GobiertoBudgets::ApplicationContro
   end
 
   def available_years
-    @available_years ||= GobiertoBudgets::SearchEngineConfiguration::Year.all.delete_if { |x| x == current_year }
+    @available_years ||= GobiertoBudgets::SearchEngineConfiguration::Year.all.delete_if { |x| x >= current_year - 1 }
   end
 
   def current_year

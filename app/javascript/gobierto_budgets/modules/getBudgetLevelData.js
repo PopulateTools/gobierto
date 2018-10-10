fd33d812a80b8d61eb1d9b2e58cba52cc7af8dbc
Hugoren Martinako <aumpfbahn@gmail.com>
2018-10-10 13:25:11 +0200

    fix slider bad import

--- app/javascript/gobierto_budgets/modules/getBudgetLevelData.js
+++ app/javascript/gobierto_budgets/modules/getBudgetLevelData.js
@@ -4,20 +4,22 @@ import { Card } from './card.js'
 export class getBudgetLevelData extends Card {
   constructor() {
     super()
-    
+
     this.data = null;
     this.dataUrl = $('body').data('bubbles-data');
   }
 
   getData(callback) {
-    d3.json(this.dataUrl, function(error, data) {
-      if (error) throw error;
+    if (this.dataUrl) {      
+      d3.json(this.dataUrl, function(error, data) {
+        if (error) throw error;
 
-      this.data = data;
+        this.data = data;
 
-      window.budgetLevels = this.data;
+        window.budgetLevels = this.data;
 
-      if (callback) callback();
-    }.bind(this));
+        if (callback) callback();
+      }.bind(this));
+    }
   }
 }

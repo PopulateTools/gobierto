1ee0b0c3c5be3f242cb2e9a09e52cc792a6ff093
Eduardo Martínez <eduardomech@gmail.com>
2018-10-22 20:35:41 +0200

    Find custom_field_by_uid scoped to item class name

--- app/helpers/gobierto_common/custom_fields_helper.rb
+++ app/helpers/gobierto_common/custom_fields_helper.rb
@@ -3,7 +3,7 @@
 module GobiertoCommon
   module CustomFieldsHelper
     def custom_field_by_uid(item, uid)
-      GobiertoCommon::CustomFieldRecord.find_by(item: item, custom_field: GobiertoCommon::CustomField.find_by(uid: uid))&.value
+      GobiertoCommon::CustomFieldRecord.find_by(item: item, custom_field: GobiertoCommon::CustomField.find_by(uid: uid, class_name: item.class.name))&.value
     end
   end
 end

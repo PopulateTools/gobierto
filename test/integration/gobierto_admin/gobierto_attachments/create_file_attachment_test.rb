8048901b5753780831f56dfd5dd562a671d2db1b
Fernando Blat <ferblape@gmail.com>
2018-11-12 14:48:50 +0100

    Fix link in flash message

--- test/integration/gobierto_admin/gobierto_attachments/create_file_attachment_test.rb
+++ test/integration/gobierto_admin/gobierto_attachments/create_file_attachment_test.rb
@@ -23,79 +23,76 @@ module GobiertoAdmin
       end
 
       def test_create_file_attachments_errors
-        with_javascript do
-          with_signed_in_admin(admin) do
-            with_current_site(site) do
-              visit @path
-              click_link "Files"
-              assert has_selector?("h1", text: "Sport city")
+        with_signed_in_admin(admin) do
+          with_current_site(site) do
+            visit @path
+            click_link "Files"
+            assert has_selector?("h1", text: "Sport city")
 
-              click_link "New"
-              assert has_selector?("h1", text: "Sport city")
-              click_button "Create"
-              assert has_alert?("File can't be blank")
-            end
+            click_link "New"
+            assert has_selector?("h1", text: "Sport city")
+            click_button "Create"
+            assert has_alert?("File can't be blank")
           end
         end
       end
 
       def test_create_file_attachments_with_name
-        with_javascript do
-          with_signed_in_admin(admin) do
-            with_current_site(site) do
-              visit @path
+        with_signed_in_admin(admin) do
+          with_current_site(site) do
+            visit @path
 
-              click_link "Files"
+            click_link "Files"
 
-              assert has_selector?("h1", text: "Sport city")
+            assert has_selector?("h1", text: "Sport city")
 
-              click_link "New"
-              assert has_selector?("h1", text: "Sport city")
+            click_link "New"
+            assert has_selector?("h1", text: "Sport city")
 
-              fill_in "file_attachment_name", with: "My file_attachment"
-              fill_in "file_attachment_description", with: "My file_attachment description"
-              attach_file("file_attachment_file", "test/fixtures/files/gobierto_attachments/attachment/pdf-collection-update-attachment.pdf")
+            fill_in "file_attachment_name", with: "My file_attachment"
+            fill_in "file_attachment_description", with: "My file_attachment description"
+            attach_file("file_attachment_file", "test/fixtures/files/gobierto_attachments/attachment/pdf-collection-update-attachment.pdf")
 
-              with_stubbed_s3_file_upload do
-                click_button "Create"
-              end
-
-              assert has_message?("Attachment created successfully.")
-              file_attachment = ::GobiertoAttachments::Attachment.find_by(name: "My file_attachment",
-                                                                          description: "My file_attachment description")
-              activity = Activity.last
-              assert_equal file_attachment, activity.subject
-              assert_equal admin, activity.author
-              assert_equal site.id, activity.site_id
-              assert_equal "gobierto_attachments.attachment_created", activity.action
+            with_stubbed_s3_file_upload do
+              click_button "Create"
             end
+
+            assert has_message?("Attachment created successfully.")
+            file_attachment = ::GobiertoAttachments::Attachment.find_by(name: "My file_attachment",
+                                                                        description: "My file_attachment description")
+            activity = Activity.last
+            assert_equal file_attachment, activity.subject
+            assert_equal admin, activity.author
+            assert_equal site.id, activity.site_id
+            assert_equal "gobierto_attachments.attachment_created", activity.action
+
+            click_link "View the document"
+
+            assert has_content?("Documents for Sport city")
+            assert has_content?("My file_attachment")
           end
         end
       end
 
       def test_create_file_attachments_without_name
-        with_javascript do
-          with_signed_in_admin(admin) do
-            with_current_site(site) do
-              visit @path
+        with_signed_in_admin(admin) do
+          with_current_site(site) do
+            visit @path
 
-              click_link "Files"
-              assert has_selector?("h1", text: "Sport city")
+            click_link "Files"
+            assert has_selector?("h1", text: "Sport city")
 
-              click_link "New"
-              assert has_selector?("h1", text: "Sport city")
+            click_link "New"
+            assert has_selector?("h1", text: "Sport city")
 
-              fill_in "file_attachment_description", with: "My file_attachment description"
-              attach_file("file_attachment_file", "test/fixtures/files/gobierto_attachments/attachment/pdf-collection-update-attachment.pdf")
+            fill_in "file_attachment_description", with: "My file_attachment description"
+            attach_file("file_attachment_file", "test/fixtures/files/gobierto_attachments/attachment/pdf-collection-update-attachment.pdf")
 
-              with_stubbed_s3_file_upload do
-                click_button "Create"
-              end
-
-              assert has_message?("Attachment created successfully.")
-              file_attachment = ::GobiertoAttachments::Attachment.find_by(name: "pdf-collection-update-attachment.pdf",
-                                                                          description: "My file_attachment description")
+            with_stubbed_s3_file_upload do
+              click_button "Create"
             end
+
+            assert has_message?("Attachment created successfully.")
           end
         end
       end

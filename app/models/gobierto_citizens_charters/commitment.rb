ac101f71c6e02e060ff251f26d0408ecee87c1a1
Eduardo Martínez <eduardomech@gmail.com>
2018-11-13 17:11:26 +0100

    Configure algolia for charters and commitments

--- app/models/gobierto_citizens_charters/commitment.rb
+++ app/models/gobierto_citizens_charters/commitment.rb
@@ -7,8 +7,15 @@ module GobiertoCitizensCharters
     acts_as_paranoid column: :archived_at
 
     include ActsAsParanoidAliases
-    include GobiertoCommon::UrlBuildable
     include GobiertoCommon::Sluggable
+    include GobiertoCommon::Searchable
+
+    algoliasearch_gobierto do
+      attribute :site_id, :updated_at, :title_en, :title_es, :title_ca, :searchable_description
+      searchableAttributes %w(title_en title_es title_ca searchable_description)
+      attributesForFaceting [:site_id]
+      add_attribute :resource_path, :class_name
+    end
 
     belongs_to :charter, -> { with_archived }
     has_many :editions, dependent: :destroy
@@ -18,6 +25,7 @@ module GobiertoCitizensCharters
     validates :slug, uniqueness: { scope: :charter_id }
     translates :title
     translates :description
+    delegate :site_id, to: :charter
 
     after_restore :set_slug
 
@@ -35,6 +43,14 @@ module GobiertoCitizensCharters
 
     def belongs_to_archived?
       charter.archived?
+    end
+
+    def searchable_description
+      searchable_translated_attribute(description_translations)
+    end
+
+    def resource_path
+      url_helpers.gobierto_citizens_charters_charter_url(slug: charter.slug, host: charter.service.site.domain)
     end
   end
 end

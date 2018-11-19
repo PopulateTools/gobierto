4679a17d63408d2908a7cd83d454a723aa991d71
Hugoren Martinako <aumpfbahn@gmail.com>
2018-11-19 11:33:41 +0100

    drop element after hovered element

--- app/javascript/gobierto_admin/modules/sections_controller.js
+++ app/javascript/gobierto_admin/modules/sections_controller.js
@@ -16,87 +16,92 @@ window.GobiertoAdmin.SectionsController = (function() {
   }
 
   function _tree() {
-    $(function() {
-      var $tree = $('#tree1');
-      $.getJSON(
-        _endpoint(),
-        function(data) {
-          $('#tree1').tree({
-            data: data['section_items'],
-            autoOpen: true,
-            dragAndDrop: true,
-            onDragStop: handle_drag_stop,
-            onCreateLi: function(node, $li) {
-              // Append a link to the jqtree-element div.
-              // The link has an url '#node-[id]' and a data property 'node-id'.
+    let nodeMouseOver = undefined;
 
-              if (node.visibility_level === "draft") {
-                var newName = $li.find("span").text() + " ("+ I18n.t("gobierto_admin.shared.draft") +")"
-                $li.find("span").text(newName);
-                $li.find(".jqtree-title").addClass("draft");
-              }
+    var $tree = $('#tree1');
+    $.getJSON(
+      _endpoint(),
+      function(data) {
+        $('#tree1').tree({
+          data: data['section_items'],
+          autoOpen: true,
+          dragAndDrop: true,
+          onDragStop: handle_drag_stop,
+          onCreateLi: function(node, $li) {
+            // Append a link to the jqtree-element div.
+            // The link has an url '#node-[id]' and a data property 'node-id'.
 
-              $li.find('.jqtree-element').append(
-                '<a remote=true href="#node-' + node.id + '" class="delete"><i class="fa fa-trash-o tipsit" data-node-id="' +
-                node.id + '" title="' + I18n.t('gobierto_admin.gobierto_cms.sections.show.delete_element') + '"></i></a>'
-              );
+            if (node.visibility_level === "draft") {
+              var newName = $li.find("span").text() + " ("+ I18n.t("gobierto_admin.shared.draft") +")"
+              $li.find("span").text(newName);
+              $li.find(".jqtree-title").addClass("draft");
             }
-          });
-        }
-      );
 
-      function handle_drag_stop(node) {
-        $.ajax({
-          url: _endpoint(node.id),
-          type: 'PUT',
-          dataType: 'json',
-          data: {
-            tree: $('#tree1').tree('toJson')
-          },
-          success: function() {},
-          error: function() {}
+            $li.find('.jqtree-element').append(
+              '<a remote=true href="#node-' + node.id + '" class="delete"><i class="fa fa-trash-o tipsit" data-node-id="' +
+              node.id + '" title="' + I18n.t('gobierto_admin.gobierto_cms.sections.show.delete_element') + '"></i></a>'
+            );
+          }
         });
       }
+    );
 
-      // Handle a click on the edit link
-      $tree.on(
-        'click', '.delete',
-        function(e) {
-          // Get the id from the 'node-id' data property
-          var node_id = $(e.target).data('node-id');
+    function handle_drag_stop(node) {        
+      $.ajax({
+        url: _endpoint(node.id),
+        type: 'PUT',
+        dataType: 'json',
+        data: {
+          tree: $('#tree1').tree('toJson')
+        },
+        success: function() {},
+        error: function() {}
+      });
+    }
 
-          // Get the node from the tree
-          var node = $tree.tree('getNodeById', node_id);
+    // Handle a click on the edit link
+    $tree.on(
+      'click', '.delete',
+      function(e) {
+        // Get the id from the 'node-id' data property
+        var node_id = $(e.target).data('node-id');
 
-          if (node) {
-            // Display the node name
-            $tree.tree('removeNode', node);
-          }
+        // Get the node from the tree
+        var node = $tree.tree('getNodeById', node_id);
 
-          $.ajax({
-            type: "POST",
-            url: _endpoint(node_id),
-            dataType: "json",
-            data: {
-              "_method": "delete"
-            },
-            complete: function() {
-
-            }
-          });
-
-          return false;
+        if (node) {
+          // Display the node name
+          $tree.tree('removeNode', node);
         }
-      );
-    });
 
+        $.ajax({
+          type: "POST",
+          url: _endpoint(node_id),
+          dataType: "json",
+          data: {
+            "_method": "delete"
+          },
+          complete: function() {
+
+          }
+        });
+
+        return false;
+      }
+    );
+
+    $tree.on('mouseover', function(e) {
+      nodeMouseOver = $tree.tree('getNodeByHtmlElement', e.target);        
+    });
+    
     // Define the elements that can be dragged
     $(".draggable").draggable({
       revert: true
     });
 
     $("#tree1").droppable({
-      drop: function(event, ui) {
+      drop: function(event, ui) {       
+        
         $.ajax({
           url: _endpoint(),
           type: 'POST',
@@ -104,12 +109,15 @@ window.GobiertoAdmin.SectionsController = (function() {
           data: {
             page_id: ui.draggable.attr("data-id")
           },
-          success: function(data) {
+          success: function(data) {            
+            var node = $tree.tree('getNodeById', nodeMouseOver.id);
+
             $('#tree1').tree(
-              'appendNode', {
+              'addNodeAfter', {
                 name: ui.draggable.attr("data-title"),
                 id: data.id
-              }
+              },
+              node
             );
           },
           error: function() {}

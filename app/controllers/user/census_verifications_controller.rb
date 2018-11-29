a7c387eae0c4806088d81fa58d9b4af58a3621a0
Eduardo Martínez <eduardomech@gmail.com>
2018-11-29 17:25:09 +0100

    Allow census verification controller to use a verification form defined by auth strategy

--- app/controllers/user/census_verifications_controller.rb
+++ app/controllers/user/census_verifications_controller.rb
@@ -3,7 +3,7 @@ class User::CensusVerificationsController < User::BaseController
   before_action(only: [:new, :create]) { require_not_verified_user_in(current_site) }
 
   def new
-    @user_verification_form = User::CensusVerificationForm.new(
+    @user_verification_form = census_verification_form_class.new(
       site_id: current_site.id, referrer_url: request.referrer,
       date_of_birth_year: current_user.date_of_birth.year,
       date_of_birth_month: current_user.date_of_birth.month,
@@ -16,7 +16,7 @@ class User::CensusVerificationsController < User::BaseController
   end
 
   def create
-    @user_verification_form = User::CensusVerificationForm.new(
+    @user_verification_form = census_verification_form_class.new(
       user_verification_params.except(*ignored_user_verification_params).merge(
         date_of_birth_year: user_verification_params["date_of_birth(1i)"],
         date_of_birth_month: user_verification_params["date_of_birth(2i)"],
@@ -46,5 +46,17 @@ class User::CensusVerificationsController < User::BaseController
 
   def ignored_user_verification_params
     ["date_of_birth(1i)", "date_of_birth(2i)", "date_of_birth(3i)"]
+  end
+
+  def census_verification_form_class
+    @census_verification_form_class ||= custom_census_verification_form_class.presence || User::CensusVerificationForm
+  end
+
+  def custom_census_verification_form_class
+    return unless auth_modules_present?
+
+    if (name = current_site.configuration.auth_modules_data.map(&:census_verification_form).compact.first).present?
+      User.const_get(name)
+    end
   end
 end

f50a3b2bebfbec05a3057442dff21027186d0952
Fernando Blat <ferblape@gmail.com>
2019-01-23 08:41:53 +0100

    Sort trips by date

--- app/controllers/gobierto_people/people/trips_controller.rb
+++ app/controllers/gobierto_people/people/trips_controller.rb
@@ -7,7 +7,7 @@ module GobiertoPeople
       before_action :check_active_submodules
 
       def index
-        @person_trips = CollectionDecorator.new(@person.trips.between_dates(filter_start_date, filter_end_date), decorator: TripDecorator)
+        @person_trips = CollectionDecorator.new(@person.trips.between_dates(filter_start_date, filter_end_date).sorted, decorator: TripDecorator)
       end
 
       def show

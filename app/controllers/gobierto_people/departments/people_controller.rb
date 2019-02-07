d388077ad85c70978a7d2300db04e23fc6d8068a
Eduardo Martínez <eduardomech@gmail.com>
2019-02-07 11:21:41 +0100

    Change people sidebar filter to show departments with any kind of people activities

--- app/controllers/gobierto_people/departments/people_controller.rb
+++ app/controllers/gobierto_people/departments/people_controller.rb
@@ -18,17 +18,36 @@ module GobiertoPeople
 
         @sidebar_departments = current_site.departments
 
-        if current_site.date_filter_configured?
-          @sidebar_departments = QueryWithEvents.new(
-            source: @sidebar_departments,
-            start_date: filter_start_date,
-            end_date: filter_end_date
-          )
-        end
+        filter_sidebar_departments if current_site.date_filter_configured?
 
         render "gobierto_people/people/index"
       end
 
+      private
+
+      def filter_sidebar_departments
+        @sidebar_departments = @sidebar_departments.select do |department|
+          events = department.events.published
+          gifts = department.gifts
+          trips = department.trips
+          invitations = department.invitations
+
+          if filter_start_date.present?
+            events = events.where("starts_at >= ?", filter_start_date)
+            gifts = gifts.where("date >= ?", filter_start_date)
+            trips = trips.where("start_date >= ?", filter_start_date)
+            invitations = invitations.where("start_date >= ?", filter_start_date)
+          end
+
+          if filter_end_date.present?
+            events = events.where("ends_at <= ?", filter_end_date)
+            gifts = gifts.where("date <= ?", filter_end_date)
+            trips = trips.where("end_date <= ?", filter_end_date)
+            invitations = invitations.where("end_date <= ?", filter_end_date)
+          end
+          events.exists? || gifts.exists? || trips.exists? || invitations.exists?
+        end
+      end
     end
   end
 end

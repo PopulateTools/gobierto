778ab38382d46e680950b346c00870709e912c91
Eduardo Martínez <eduardomech@gmail.com>
2019-03-19 17:15:28 +0100

    Add javascript to show/hide groups depending on sites or authorization level

--- app/javascript/gobierto_admin/modules/admins_controller.js
+++ app/javascript/gobierto_admin/modules/admins_controller.js
@@ -3,8 +3,31 @@ window.GobiertoAdmin.AdminsController = (function() {
     function AdminsController() {}
 
     AdminsController.prototype.form = function() {
+      _addToggleSiteBehaviors();
       _addChangeAuthorizationLevelBehaviors();
     };
+
+    function _addToggleSiteBehaviors() {
+      var $siteCheckboxes = $("[data-behavior='toggle_site']");
+
+      $siteCheckboxes.click(function() {
+        var siteId = $(this).attr('data-site-id');
+        var $siteAdminGroups = $("[data-class='site_admin_group'][data-site-id='" + siteId + "']");
+
+        if (this.checked) {
+          $siteAdminGroups.show('slow');
+        } else {
+          $siteAdminGroups.hide('slow');
+
+          var siteCheckboxes = $("[data-behavior='toggle_site']");
+          var sitePermissions = $.map(siteCheckboxes, function (val) {
+            if (val.checked) {
+              return val.checked;
+            }
+          });
+        }
+      });
+    }
 
     function _addChangeAuthorizationLevelBehaviors() {
       var $regular  = $("[data-behavior='authorization-level-regular']");
@@ -13,14 +36,17 @@ window.GobiertoAdmin.AdminsController = (function() {
 
       $regular.click(function() {
         $('#sites_permissions').show('fast');
+        $('#admin_groups').show('fast');
       });
 
       $manager.click(function() {
         $('#sites_permissions').hide('fast');
+        $('#admin_groups').hide('fast');
       });
 
       $disabled.click(function() {
         $('#sites_permissions').hide('fast');
+        $('#admin_groups').hide('fast');
       });
     }
 

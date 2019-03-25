a3d0d082a872aa9b30e317ee23e4d773ac18808a
Alberto Miedes <albertomg994@gmail.com>
2019-03-25 09:50:32 +0100

    Report JS errors to Rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -4,10 +4,14 @@ require "rollbar/rails"
 
 Rollbar.configure do |config|
   config.access_token = Rails.application.secrets.rollbar_access_token
-
   config.enabled = false if Rails.env.development? || Rails.env.test?
-
   config.exception_level_filters.merge!("ActionController::InvalidCrossOriginRequest" => "ignore",
                                         "ActionController::RoutingError" => "ignore",
                                         "ActionController::UnknownFormat" => "ignore")
+  config.js_enabled = true
+  config.js_options = {
+    accessToken: Rails.application.secrets.rollbar_post_client_item,
+    captureUncaught: true,
+    payload: { environment: Rails.env }
+  }
 end

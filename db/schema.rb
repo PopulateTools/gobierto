ae1f434fbfcaefb41e7f74a5ebcafc0c1a26a014
Eduardo Martínez <eduardomech@gmail.com>
2019-03-27 19:10:19 +0100

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -875,6 +875,7 @@ ActiveRecord::Schema.define(version: 2019_04_15_093350) do
     t.jsonb "options"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "visibility_level", default: 0, null: false
     t.index ["name_translations"], name: "index_gplan_nodes_on_name_translations", using: :gin
   end
 

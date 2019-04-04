b3d3bf56be5e324dcc8bf12e3324213c7be51ed1
Eduardo Martínez <eduardomech@gmail.com>
2019-04-04 16:44:58 +0200

    Add moderable concern to GobiertoPlans::Node

--- app/models/gobierto_plans/node.rb
+++ app/models/gobierto_plans/node.rb
@@ -4,6 +4,8 @@ require_dependency "gobierto_plans"
 
 module GobiertoPlans
   class Node < ApplicationRecord
+    include GobiertoCommon::Moderable
+
     has_and_belongs_to_many :categories, class_name: "GobiertoCommon::Term", association_foreign_key: :category_id, join_table: :gplan_categories_nodes
     has_paper_trail ignore: [:visibility_level]
 
@@ -12,6 +14,18 @@ module GobiertoPlans
     scope :with_category, ->(category) { where(gplan_categories_nodes: { category_id: GobiertoCommon::Term.find(category).last_descendants }) }
     scope :with_progress, ->(progress) { where("progress > ? AND progress <= ?", *progress.split("-")) }
     scope :with_interval, ->(interval) { where("starts_at >= ? AND ends_at <= ?", *interval.split(",").map { |date| Date.parse(date) }) }
+
+    extra_moderation_permissions_lookup_attributes do |_|
+      [{
+        namespace: "site_module",
+        resource_name: "gobierto_plans",
+        resource_id: nil
+      }]
+    end
+
+    default_moderation_stage do |node|
+      node.published? ? :approved : :not_sent
+    end
 
     translates :name, :status
 

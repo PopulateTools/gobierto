1598744f0b045cffaf24019744952aea3a0ae55a
Eduardo Martínez <eduardomech@gmail.com>
2019-04-12 10:53:50 +0200

    Create categories controller and views inherited from terms

---
+++ app/controllers/gobierto_admin/gobierto_plans/categories_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module GobiertoAdmin
+  module GobiertoPlans
+    class CategoriesController < GobiertoAdmin::GobiertoCommon::OrderedTermsController
+      skip_before_action :check_permissions!
+
+      before_action -> { module_allowed_action!(current_admin, current_admin_module, :manage) }
+
+      helper_method :current_admin_actions
+
+      def index
+        find_vocabulary
+        @terms = TreeDecorator.new(tree(@vocabulary.terms), decorator: BaseTermDecorator)
+      end
+
+      private
+
+      def index_path
+        admin_plans_plan_categories_path(@plan)
+      end
+
+      def find_term
+        find_vocabulary
+        @term = @vocabulary.terms.find(params[:id])
+      end
+
+      def find_vocabulary
+        @plan = current_site.plans.find params[:plan_id]
+        @vocabulary = @plan.categories_vocabulary
+      end
+
+      def raise_action_not_allowed
+        find_vocabulary
+        redirection_path = current_admin_actions.include?(:index) ? admin_plans_plan_projects_path(@plan) : admin_plans_plan_categories_path(@plan)
+        redirect_to(
+          redirection_path,
+          alert: t("gobierto_admin.module_helper.not_enabled")
+        )
+      end
+
+      def current_admin_actions
+        @current_admin_actions ||= GobiertoAdmin::GobiertoPlans::ProjectPolicy.new(
+          current_admin: current_admin,
+          current_site: current_site
+        ).allowed_actions
+      end
+    end
+  end
+end

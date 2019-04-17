903ac957835d3c43caa5c0866235071be80db570
Fernando Blat <ferblape@gmail.com>
2019-04-17 10:01:57 +0200

    Optimize #update_parents_and_positions method
    Also, remove it from sortable, it’s coupled to Term class

--- app/models/concerns/gobierto_common/sortable.rb
+++ app/models/concerns/gobierto_common/sortable.rb
@@ -11,33 +11,6 @@ module GobiertoCommon
         update(position_assignment.keys, position_assignment.values)
       end
 
-      # positions_from_params arg: hash of arrays
-      # The element with id 0 defines the order of the parent nodes
-      # Example:
-      #   {
-      #     0: [parent_id_1, parent_id_2],
-      #     parent_id_1: [children_id_1, children_id_2],
-      #     parent_id_2: [children_id_3, children_id_4]
-      #   }
-      def self.update_parents_and_positions(positions_from_params)
-        child_assignment = {}
-        parent_assignment = {}
-        positions_from_params.each do |parent_id, children_ids|
-          if parent_id == "0"
-            children_ids.each_with_index do |child_id, position|
-              parent_assignment[child_id] = { position: position, term_id: nil, level: 0 }
-            end
-          else
-            children_ids.each_with_index do |child_id, position|
-              child_assignment[child_id] = { position: position, term_id: parent_id }
-            end
-          end
-        end
-
-        update(parent_assignment.keys, parent_assignment.values)
-        update(child_assignment.keys, child_assignment.values)
-      end
-
       def self.reset_position!
         ActiveRecord::Base.transaction do
           unscope(:order).order(created_at: :asc).each_with_index do |object, index|
--- app/models/gobierto_common/term.rb
+++ app/models/gobierto_common/term.rb
@@ -55,6 +55,29 @@ module GobiertoCommon
       terms.map(&:last_descendants).flatten
     end
 
+    # positions_from_params arg: hash of arrays
+    # The element with id 0 defines the order of the parent nodes
+    # Example:
+    #   {
+    #     0: [parent_id_1, parent_id_2],
+    #     parent_id_1: [children_id_1, children_id_2],
+    #     parent_id_2: [children_id_3, children_id_4]
+    #   }
+    def self.update_parents_and_positions(positions_from_params)
+      positions_from_params.each do |parent_id, children_ids|
+        if parent_id == "0"
+          children_ids.each_with_index do |child_id, position|
+            where(id: child_id).update_all({ position: position, term_id: nil, level: 0 })
+          end
+        else
+          children_ids.each_with_index do |child_id, position|
+            parent_level = find(parent_id).level
+            where(id: child_id).update_all({ position: position, term_id: parent_id, level: parent_level + 1 })
+          end
+        end
+      end
+    end
+
     private
 
     def calculate_level

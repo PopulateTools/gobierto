de1f346ad05cefb0edfa01162b8c6be2ad00bb88
Eduardo Martínez <eduardomech@gmail.com>
2019-04-23 18:59:19 +0200

    Add tests for GobiertoPlans::Plan#to_s

--- test/models/gobierto_plans/plan_test.rb
+++ test/models/gobierto_plans/plan_test.rb
@@ -11,5 +11,25 @@ module GobiertoPlans
     def test_valid
       assert plan.valid?
     end
+
+    def test_to_s
+      plan_string = plan.to_s
+
+      assert plan_string.include? "3 axes"
+      assert plan_string.include? "1 line of action"
+      assert plan_string.include? "2 actions"
+      assert plan_string.include? "2 projects/actions"
+    end
+
+    def test_plan_without_configuration_to_s
+      plan.update_attribute(:configuration_data, nil)
+
+      plan_string = plan.to_s
+
+      assert plan_string.include? "3 items of level 1"
+      assert plan_string.include? "1 item of level 2"
+      assert plan_string.include? "2 items of level 3"
+      assert plan_string.include? "2 projects"
+    end
   end
 end

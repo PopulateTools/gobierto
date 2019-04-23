5fa0f8531d7d4ac92cb82e8bfeacdd19633dd666
Eduardo Martínez <eduardomech@gmail.com>
2019-04-23 17:27:18 +0200

    Use case insensitive search in name filter of projects

--- app/models/gobierto_plans/node.rb
+++ app/models/gobierto_plans/node.rb
@@ -10,7 +10,7 @@ module GobiertoPlans
     has_and_belongs_to_many :categories, class_name: "GobiertoCommon::Term", association_foreign_key: :category_id, join_table: :gplan_categories_nodes
     has_paper_trail ignore: [:visibility_level]
 
-    scope :with_name, ->(name) { where("gplan_nodes.name_translations ->> 'en' LIKE :name OR gplan_nodes.name_translations ->> 'es' LIKE :name OR gplan_nodes.name_translations ->> 'ca' LIKE :name", name: "%#{name}%") }
+    scope :with_name, ->(name) { where("gplan_nodes.name_translations ->> 'en' ILIKE :name OR gplan_nodes.name_translations ->> 'es' ILIKE :name OR gplan_nodes.name_translations ->> 'ca' ILIKE :name", name: "%#{name}%") }
     scope :with_status, ->(status) { with_status_translation(status) }
     scope :with_category, ->(category) { where(gplan_categories_nodes: { category_id: GobiertoCommon::Term.find(category).last_descendants }) }
     scope :with_progress, ->(progress) { where("progress > ? AND progress <= ?", *progress.split("-")) }

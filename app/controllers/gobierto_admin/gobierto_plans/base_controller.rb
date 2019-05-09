47d0cd1b30ac6e7b815ae7d0b1c828da1106da34
Eduardo Martínez <eduardomech@gmail.com>
2019-05-09 22:52:09 +0200

    Use plan tab link by default if admin can manage plans, projects tab link otherwise

--- app/controllers/gobierto_admin/gobierto_plans/base_controller.rb
+++ app/controllers/gobierto_admin/gobierto_plans/base_controller.rb
@@ -6,9 +6,13 @@ module GobiertoAdmin
       before_action { module_enabled!(current_site, "GobiertoPlans") }
       before_action { module_allowed!(current_admin, "GobiertoPlans") }
 
-      helper_method :gobierto_plans_plan_type_preview_url
+      helper_method :gobierto_plans_plan_type_preview_url, :current_admin_can_manage_plans?
 
       protected
+
+      def current_admin_can_manage_plans?
+        @can_manage_plans = current_admin.module_allowed_action?(current_admin_module, current_site, :manage)
+      end
 
       def gobierto_plans_plan_type_preview_url(plan, options = {})
         if plan.draft?

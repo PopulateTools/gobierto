fcea60a8beac60f2b0929164fa659607ea6eb0ac
Eduardo Martínez <eduardomech@gmail.com>
2019-05-18 20:20:31 +0200

    Define a default configuration_data for new plans

--- app/models/gobierto_plans.rb
+++ app/models/gobierto_plans.rb
@@ -8,4 +8,62 @@ module GobiertoPlans
   def self.doc_url
     "https://gobierto.readme.io/docs/planes"
   end
+
+  def self.default_plans_configuration_data
+    <<JSON
+    {
+      "level0": {
+        "one": {
+          "en": "axis",
+          "ca": "eix",
+          "es": "eje"
+        },
+        "other": {
+          "en": "axes",
+          "ca": "eixos",
+          "es": "ejes"
+        }
+      },
+      "level1": {
+        "one": {
+          "en": "line of activity",
+          "ca": "línia d'actuació",
+          "es": "línea de actuación"
+        },
+        "other": {
+          "en": "lines of activity",
+          "ca": "línies d'actuació",
+          "es": "líneas de actuación"
+        }
+      },
+      "level2": {
+        "one": {
+          "en": "activity",
+          "ca": "actuació",
+          "es": "actuación"
+        },
+        "other": {
+          "en": "activities",
+          "ca": "actuacions",
+          "es": "actuaciones"
+        }
+      },
+      "level3": {
+        "one": {
+          "en": "project/action",
+          "ca": "projecte/acció",
+          "es": "proyecto/acción"
+        },
+        "other": {
+          "en": "projects/actions",
+          "ca": "projectes/accions",
+          "es": "proyectos/acciones"
+        }
+      },
+      "level0_options": [],
+      "show_table_header": false,
+      "open_node": false
+    }
+JSON
+  end
 end

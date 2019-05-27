f3854b7caf956d105ab82ad880ca42dbb11a3e79
Eduardo Martínez <eduardomech@gmail.com>
2019-05-27 15:40:24 +0200

    Use unique ids for showTable elements and don't reset list on active node changes

--- app/javascript/gobierto_plans/modules/plan_types_controller.js
+++ app/javascript/gobierto_plans/modules/plan_types_controller.js
@@ -125,7 +125,6 @@ window.GobiertoPlans.PlanTypesController = (function() {
         watch: {
           activeNode: {
             handler: function(node) {
-              this.showTable = {}
               // update hash when a new node is active
               this.setPermalink()
 
@@ -218,8 +217,8 @@ window.GobiertoPlans.PlanTypesController = (function() {
             }
             return "object";
           },
-          toggle: function(i) {
-            Vue.set(this.showTable, i, !(this.showTable[i]));
+          toggle: function(node_i, i) {
+            Vue.set(this.showTable, `${node_i}-${i}`, !(this.showTable[`${node_i}-${i}`]));
           },
           getLabel: function(level, number_of_elements) {
             var l = (_.keys(this.levelKeys).length === (level + 1)) ? level : (level + 1);

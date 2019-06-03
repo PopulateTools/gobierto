cde07974787d8f8b842b3024e59e6ddfd4209810
Fernando Blat <ferblape@gmail.com>
2019-06-03 09:48:13 +0200

    Memoize nodes

--- app/models/gobierto_plans/plan.rb
+++ app/models/gobierto_plans/plan.rb
@@ -34,7 +34,7 @@ module GobiertoPlans
     end
 
     def nodes
-      Node.joins(categories: [:vocabulary]).where(terms: { vocabulary_id: categories_vocabulary&.id })
+      @nodes ||= Node.joins(categories: [:vocabulary]).where(terms: { vocabulary_id: categories_vocabulary&.id })
     end
 
     def levels

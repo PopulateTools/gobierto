481df9fe3dd44dabe89127703b814656ae1d5367
Fernando Blat <ferblape@gmail.com>
2019-06-06 09:41:16 +0200

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -879,8 +879,8 @@ ActiveRecord::Schema.define(version: 2019_06_03_074757) do
     t.datetime "updated_at", null: false
     t.integer "visibility_level", default: 0, null: false
     t.bigint "admin_id"
-    t.integer "published_version"
     t.bigint "status_id"
+    t.integer "published_version"
     t.integer "position"
     t.index ["admin_id"], name: "index_gplan_nodes_on_admin_id"
     t.index ["name_translations"], name: "index_gplan_nodes_on_name_translations", using: :gin

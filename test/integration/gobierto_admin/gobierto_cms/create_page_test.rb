ea6b3de8750aca1df73a9e24d16f86927dc83e4d
Fernando Blat <ferblape@gmail.com>
2019-06-20 10:06:06 +0200

    Use less verbose test context

--- test/integration/gobierto_admin/gobierto_cms/create_page_test.rb
+++ test/integration/gobierto_admin/gobierto_cms/create_page_test.rb
@@ -31,78 +31,71 @@ module GobiertoAdmin
       end
 
       def test_create_page_errors
-        with_javascript do
-          with_signed_in_admin(admin) do
-            with_current_site(site) do
-              visit @path
+        with(site: site, admin: admin, js: true) do
+          visit @path
 
-              within "tr#collection-item-#{collection.id}" do
-                click_link "News"
-              end
-
-              assert has_selector?("h1", text: "CMS")
-
-              click_link "New"
-              assert has_selector?("h1", text: "Sport city")
-
-              fill_in "page_title_translations_en", with: "My page"
-              click_button "Create"
-
-              assert has_alert?("Body can't be blank")
-            end
+          within "tr#collection-item-#{collection.id}" do
+            click_link "News"
           end
+
+          assert has_selector?("h1", text: "CMS")
+
+          click_link "New"
+          assert has_selector?("h1", text: "Sport city")
+
+          fill_in "page_title_translations_en", with: "My page"
+          click_button "Create"
+
+          assert has_alert?("Body can't be blank")
         end
       end
 
       def test_create_page
         Timecop.freeze(stubbed_current_time)
 
-        with_javascript do
-          with_signed_in_admin(admin) do
-            with_current_site(site) do
-              visit @path
+        with(site: site, admin: admin, js: true) do
+          visit @path
 
-              within "tr#collection-item-#{collection.id}" do
-                click_link "News"
-              end
-
-              assert has_selector?("h1", text: "CMS")
-
-              click_link "New"
-              assert has_selector?("h1", text: "Sport city")
-
-              # ensure default date is set
-              assert_equal stubbed_current_time.to_s, air_datepicker_field_value(:page_published_on)
-
-              fill_in "page_title_translations_en", with: "My page"
-              page.execute_script('document.getElementById("page_body_translations_en").value = "The content of the page"')
-              fill_in "page_slug", with: "new-page"
-              fill_in "page_published_on", with: chosen_publication_date
-
-              switch_locale "ES"
-              fill_in "page_title_translations_es", with: "Mi página"
-              page.execute_script('document.getElementById("page_body_translations_es").value = "Contenido de la página"')
-
-              click_button "Create"
-
-              assert has_message?("Page created successfully")
-              assert has_field?("page_slug", with: "new-page")
-              assert_equal chosen_publication_date.to_s, air_datepicker_field_value(:page_published_on)
-
-              assert_equal(
-                "The content of the page",
-                find("#page_body_translations_en", visible: false).value
-              )
-
-              switch_locale "ES"
-
-              assert_equal(
-                "Contenido de la página",
-                find("#page_body_translations_es", visible: false).value
-              )
-            end
+          within "tr#collection-item-#{collection.id}" do
+            click_link "News"
           end
+
+          assert has_selector?("h1", text: "CMS")
+
+          click_link "New"
+          assert has_selector?("h1", text: "Sport city")
+
+          # ensure default date is set
+          assert_equal stubbed_current_time.to_s, air_datepicker_field_value(:page_published_on)
+
+          fill_in "page_title_translations_en", with: "My page"
+          page.execute_script('document.getElementById("page_body_translations_en").value = "The content of the page"')
+          fill_in "page_slug", with: "new-page"
+          fill_in "page_published_on", with: chosen_publication_date
+
+          switch_locale "ES"
+          fill_in "page_title_translations_es", with: "Mi página"
+          page.execute_script('document.getElementById("page_body_translations_es").value = "Contenido de la página"')
+
+          click_button "Create"
+
+          assert has_message?("Page created successfully")
+          assert has_field?("page_slug", with: "new-page")
+          assert_equal chosen_publication_date.to_s, air_datepicker_field_value(:page_published_on)
+
+          assert_equal(
+            "The content of the page",
+            find("#page_body_translations_en", visible: false).value
+          )
+
+          switch_locale "ES"
+
+          assert_equal(
+            "Contenido de la página",
+            find("#page_body_translations_es", visible: false).value
+          )
         end
+
         Timecop.return
       end
 

49066ae121525b32b476f7932038f0002b99a231
Eduardo Martínez <eduardomech@gmail.com>
2019-06-25 18:51:22 +0200

    Change progress plugin javascript to replace progress with custom field value

--- vendor/gobierto_engines/custom-fields-progress-plugin/app/javascripts/custom_fields_progress_plugin/modules/progress_plugin.js
+++ vendor/gobierto_engines/custom-fields-progress-plugin/app/javascripts/custom_fields_progress_plugin/modules/progress_plugin.js
@@ -2,33 +2,29 @@ window.GobiertoAdmin.GobiertoCommonCustomFieldRecordsProgressPluginController = 
 
   function GobiertoCommonCustomFieldRecordsProgressPluginController() {}
 
-  var grid;
-
   GobiertoCommonCustomFieldRecordsProgressPluginController.prototype.form = function(opts = {}) {
     _handlePluginData(opts.uid);
   };
-
-  function _applyPluginStyles(element, plugin_name_hint) {
-    element.wrap("<div class='v_container'><div class='v_el v_el_level v_el_full_content'></div></div>");
-  }
 
   function _handlePluginData(uid) {
     let element = $(`[data-uid=${uid}]`)
     let id = element.attr('id')
     let data = JSON.parse($(`#${id}`).find("input[name$='[value]'").val())
 
-    _applyPluginStyles(element, "progress")
     _insertProgressPlugin(id, data)
   }
 
   function _insertProgressPlugin(id, data) {
     let element = $(`#${id}`)
-    let formatted_data = data
-      ? data.toLocaleString(I18n.locale, { style: 'percent', maximumFractionDigits: 1 })
-      : "-"
-
-    element.append(formatted_data);
-    // element.append($('<input>', { disabled: true, type: 'text', val: formatted_data }))
+    element.remove();
+    let progress_select = $("select[id$='_progress'")
+    if(progress_select.length) {
+      progress_select.replaceWith(
+        $('<input type="text" disabled="disabled">').val(
+          data.toLocaleString(I18n.locale, { style: 'percent', maximumFractionDigits: 1 })
+        )
+      );
+    }
   }
 
   return GobiertoCommonCustomFieldRecordsProgressPluginController;

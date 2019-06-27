3a31d2d8c268230e9941c18454ac3a2150642517
Alberto Miedes <albertomg994@gmail.com>
2019-06-27 16:01:46 +0200

    Fix API for retrieving progress and cost from HR plugin

--- vendor/gobierto_engines/custom-fields-data-grid-plugin/app/decorators/gobierto_plans/category_term_decorator_human_resources_attachment.rb
+++ vendor/gobierto_engines/custom-fields-data-grid-plugin/app/decorators/gobierto_plans/category_term_decorator_human_resources_attachment.rb
@@ -12,21 +12,17 @@ module GobiertoPlans
         "options @> ?",
         { configuration: { plugin_type: "human_resources" } }.to_json
       )
-      record_functions = field&.records&.first&.functions
+      record_functions = field&.records&.first&.functions(version: node.published_version)
 
       return super_result if record_functions.nil?
-
-      # TODO: the version argument is not yet supported
-      progress = record_functions.progress(version: node.published_version)
-      cost = record_functions.cost(version: node.published_version)
 
       super_result[:human_resources] = {
         title_translations: Hash[I18n.available_locales.map do |locale|
           [locale, I18n.t("gobierto_plans.custom_fields_plugins.human_resources.title", locale: locale)]
         end],
-        budgeted_amount: cost.round,
-        executed_amount: ((cost * progress) / 100).round,
-        executed_percentage: "#{progress.round} %"
+        budgeted_amount: record_functions.cost.round,
+        executed_amount: ((record_functions.cost * record_functions.progress) / 100).round,
+        executed_percentage: "#{record_functions.progress.round} %"
       }
 
       super_result

7c4e7c26d4fbd298f06a13e3771699010417480f
Alberto Miedes <albertomg994@gmail.com>
2019-06-27 10:38:41 +0200

    Fix edge cases and tests

--- vendor/gobierto_engines/custom-fields-data-grid-plugin/app/decorators/gobierto_plans/category_term_decorator_budgets_attachment.rb
+++ vendor/gobierto_engines/custom-fields-data-grid-plugin/app/decorators/gobierto_plans/category_term_decorator_budgets_attachment.rb
@@ -15,7 +15,10 @@ module GobiertoPlans
         detail: {
           link: url_helpers.gobierto_budgets_root_url(host: site.domain),
           text: I18n.t("gobierto_plans.custom_fields_plugins.budgets.detail_text")
-        }
+        },
+        budgeted_amount: nil,
+        executed_amount: nil,
+        executed_percentage: nil
       }
 
       field = site.custom_fields.find_by(
@@ -23,22 +26,31 @@ module GobiertoPlans
         { configuration: { plugin_type: "budgets" } }.to_json
       )
 
+      budget_lines = field&.records&.first&.payload&.dig("budget_lines")
+
       relative_total_amount = 0
       relative_executed_amount = 0
 
-      field.records.first.payload["budget_lines"].map do |bl|
+      budget_lines.map do |bl|
         next unless (weight = bl["weight"])
 
         details = GobiertoBudgets::BudgetLine.find_details(type: bl["area"], id: bl["id"])
 
         total_amount = details.forecast.updated_amount || details.forecast.original_amount
+
+        next unless total_amount && weight
+
         relative_total_amount += (total_amount * weight) / 100
-        relative_executed_amount += (details.execution.amount * weight) / 100
+        relative_executed_amount += ((details.execution.amount || 0.0) * weight) / 100
+      rescue GobiertoBudgets::BudgetLine::RecordNotFound
+        next
       end
 
-      super_result[:budgets][:budgeted_amount] = relative_total_amount.round(2)
-      super_result[:budgets][:executed_amount] = relative_executed_amount.round(2)
-      super_result[:budgets][:executed_percentage] = "#{((relative_executed_amount * 100) / relative_total_amount).round} %"
+      if relative_total_amount.positive?
+        super_result[:budgets][:budgeted_amount] = relative_total_amount.round(2)
+        super_result[:budgets][:executed_amount] = relative_executed_amount.round(2)
+        super_result[:budgets][:executed_percentage] = "#{((relative_executed_amount * 100) / relative_total_amount).round} %"
+      end
 
       super_result
     end

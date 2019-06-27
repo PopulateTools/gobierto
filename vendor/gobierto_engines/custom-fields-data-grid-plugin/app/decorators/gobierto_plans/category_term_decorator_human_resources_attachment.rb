3a1b42d905964e80f520fe6dde6c2da6a6937d58
Alberto Miedes <albertomg994@gmail.com>
2019-06-27 10:18:14 +0200

    Retrieve progress and cost from las version of human resources (WIP)

--- vendor/gobierto_engines/custom-fields-data-grid-plugin/app/decorators/gobierto_plans/category_term_decorator_human_resources_attachment.rb
+++ vendor/gobierto_engines/custom-fields-data-grid-plugin/app/decorators/gobierto_plans/category_term_decorator_human_resources_attachment.rb
@@ -12,15 +12,21 @@ module GobiertoPlans
         "options @> ?",
         { configuration: { plugin_type: "human_resources" } }.to_json
       )
-      record = field.records.first
+      record_functions = field&.records&.first&.functions
+
+      return super_result if record_functions.nil?
+
+      # TODO: the version argument is not yet supported
+      progress = record_functions.progress(version: node.published_version)
+      cost = record_functions.cost(version: node.published_version)
 
       super_result[:human_resources] = {
         title_translations: Hash[I18n.available_locales.map do |locale|
           [locale, I18n.t("gobierto_plans.custom_fields_plugins.human_resources.title", locale: locale)]
         end],
-        budgeted_amount: record.functions.cost.round,
-        executed_amount: ((record.functions.cost * record.functions.progress) / 100).round,
-        executed_percentage: "#{record.functions.progress.round} %"
+        budgeted_amount: cost.round,
+        executed_amount: ((cost * progress) / 100).round,
+        executed_percentage: "#{progress.round} %"
       }
 
       super_result

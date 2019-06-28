cfadd8063c0b8f3b06b6600e2dd5c2fce23d6fd7
Hugoren Martinako <aumpfbahn@gmail.com>
2019-06-28 17:47:50 +0200

    component

--- app/javascript/plugin__data_grid_budgets_public/modules/tablerow.component.js
+++ app/javascript/plugin__data_grid_budgets_public/modules/tablerow.component.js
@@ -1,15 +1,37 @@
 import Vue from "vue";
+import "./tablerow.component.scss";
 
 window.GobiertoPlans.TableRowController = (function() {
   function TableRowController() {}
 
   const PLUGINS = [];
-  const template = `<div>{{ key }}</div>`;
+  const template = `
+    <div class="tablerow">
+      <div class="tablerow__title">{{ title | translate }}</div>
+      <div class="tablerow__data">
+        <div class="tablerow__item">
+          <div>Inicial</div> 
+          <div class="tablerow__amount">
+            <div class="tablerow__amount--numeric">{{ budgetedAmount | money }}</div>
+            <div class="tablerow__amount--percent">100%</div>
+          </div>
+        </div>
+        <div class="tablerow__item">
+          <div>Ejecutado</div>
+          <div class="tablerow__amount">
+            <div class="tablerow__amount--numeric">{{ executedAmount | money }}</div>
+            <div class="tablerow__amount--percent">{{ executedPercent }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="tablerow__extra">
+        <a v-if="detail" :href="detail.link">{{ detail.text }}</a>
+      </div>
+    </div>
+  `;
 
   TableRowController.prototype.show = function() {
-    document.addEventListener("gobierto-plans-mount-plugin", e => {
-      handleComponent(e.detail);
-    });
+    document.addEventListener("gobierto-plans-mount-plugin", e => handleComponent(e.detail));
   };
 
   TableRowController.prototype.add = function(key, id) {
@@ -25,9 +47,21 @@ window.GobiertoPlans.TableRowController = (function() {
         name: "gobierto-planification__plugin",
         template: template,
         data: {
-          key: params.key
+          title: params.title_translations,
+          detail: params.detail,
+          budgetedAmount: params.budgeted_amount,
+          executedAmount: params.executed_amount,
+          executedPercent: params.executed_percentage,
         },
-        created() {
+        filters: {
+          translate(value) {
+            const lang = I18n.locale || "es"
+            return value[lang]
+          },
+          money(value) {
+            const lang = I18n.locale || "es"
+            return value.toLocaleString(lang, { style: 'currency', currency: 'EUR' })
+          }
         }
       })
     }

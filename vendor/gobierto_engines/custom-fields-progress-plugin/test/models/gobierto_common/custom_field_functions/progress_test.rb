d63512312878cad43665d79a50213f998137cec1
Eduardo Martínez <eduardomech@gmail.com>
2019-07-02 22:10:04 +0200

    Update progress functions test

--- vendor/gobierto_engines/custom-fields-progress-plugin/test/models/gobierto_common/custom_field_functions/progress_test.rb
+++ vendor/gobierto_engines/custom-fields-progress-plugin/test/models/gobierto_common/custom_field_functions/progress_test.rb
@@ -51,20 +51,8 @@ module GobiertoCommon::CustomFieldFunctions
             {
               human_resource: ActiveRecord::FixtureSet.identify(:human_resources_supervisor),
               cost: 35_000,
-              start_date: 1.day.from_now.to_date.to_s,
-              end_date: 2.days.from_now.to_date.to_s
-            },
-            {
-              human_resource: ActiveRecord::FixtureSet.identify(:human_resources_employee),
-              cost: 25_000,
-              start_date: 1.day.from_now.to_date.to_s,
-              end_date: 2.days.from_now.to_date.to_s
-            },
-            {
-              human_resource: ActiveRecord::FixtureSet.identify(:human_resources_employee),
-              cost: 20_000,
-              start_date: 1.day.from_now.to_date.to_s,
-              end_date: 2.days.from_now.to_date.to_s
+              start_date: 1.day.ago.to_date.to_s,
+              end_date: 3.days.from_now.to_date.to_s
             }
           ]
         },
@@ -72,21 +60,47 @@ module GobiertoCommon::CustomFieldFunctions
       )
     end
 
+    def plan
+      @plan = gobierto_plans_plans :strategic_plan
+    end
+
+    def progress_custom_field
+      @progress_custom_field ||= progress_record.custom_field
+    end
+
     def test_progress
-      assert_equal 0.0, progress_record.functions.progress
+      assert_equal 0.25, progress_record.functions.progress
     end
 
     def test_versions_progress
       assert_equal 1.0, progress_record.functions(version: 1).progress
       assert_equal 0.5, progress_record.functions(version: 2).progress
-      assert_equal 0.0, progress_record.functions(version: 3).progress
+      assert_equal 0.25, progress_record.functions(version: 3).progress
+    end
+
+    def test_progress_defined_for_instance
+      progress_record.custom_field.update_attribute(:instance, plan)
+
+      assert_equal 0.25, progress_record.functions.progress
+    end
+
+    def test_progress_refered_to_not_existing_custom_field
+      progress_custom_field.update_attribute(:options, {"configuration"=>{"plugin_type"=>"progress", "plugin_configuration"=>{"custom_field_uids"=>["wadus"]}}})
+
+      assert_nil progress_record.functions.progress
+    end
+
+    def test_progress_refered_to_custom_field_not_providing_progress_function
+      progress_custom_field.update_attribute(:options, {"configuration"=>{"plugin_type"=>"progress", "plugin_configuration"=>{"custom_field_uids"=>["goals"]}}})
+
+      assert_nil progress_record.functions.progress
     end
 
     def test_update_progress!
       progress_record.functions.update_progress!
 
       item.reload
-      assert_equal 0.0, item.progress
+      assert_equal 25.0, item.progress
     end
   end
 end

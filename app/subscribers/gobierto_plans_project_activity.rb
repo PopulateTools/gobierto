628fb15285561258f43c1c26d9be884de4fd5963
Eduardo Martínez <eduardomech@gmail.com>
2019-07-16 16:23:52 +0200

    Add projects activities pub/sub with progresses update activity

---
+++ app/subscribers/gobierto_plans_project_activity.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Subscribers
+  class GobiertoPlansProjectActivity < ::Subscribers::Base
+
+    def progresses_updated(event)
+      create_activity_from_event(event, "gobierto_plans.progresses_updated")
+    end
+
+    private
+
+    def create_activity_from_event(event, action)
+      Activity.create! subject: event.payload[:subject],
+                       author: event.payload[:author],
+                       subject_ip: event.payload[:ip],
+                       action: action,
+                       site_id: event.payload[:site_id],
+                       admin_activity: true
+    end
+  end
+end

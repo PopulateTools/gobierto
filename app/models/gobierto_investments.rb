231afab3fda5340d6bbc2507cb827d4f3122a220
Eduardo Martínez <eduardomech@gmail.com>
2019-07-23 18:06:16 +0200

    Add doc_url to gobierto investments module

--- app/models/gobierto_investments.rb
+++ app/models/gobierto_investments.rb
@@ -8,4 +8,8 @@ module GobiertoInvestments
   def self.classes_with_custom_fields
     [GobiertoInvestments::Project]
   end
+
+  def self.doc_url
+    "https://gobierto.readme.io/docs/inversiones"
+  end
 end

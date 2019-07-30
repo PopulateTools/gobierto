038a2298756fb5aa09f7ab07dee369f06fc853cc
Eduardo Martínez <eduardomech@gmail.com>
2019-07-30 20:50:54 +0200

    Add basic integration test

--- vendor/gobierto_engines/custom-fields-table-plugin/test/integration/table_plugin_test.rb
+++ vendor/gobierto_engines/custom-fields-table-plugin/test/integration/table_plugin_test.rb
@@ -30,14 +30,35 @@ class TablePluginTest < ActionDispatch::IntegrationTest
 
   def set_payload
     custom_field_record.update_attributes!(
-      payload: {}
+      payload: {
+        directory: [
+          {
+            person: "John",
+            phone_number: 666_666_666,
+            email: "john@example.org",
+            birthdate: "1980-12-28",
+            position: ActiveRecord::FixtureSet.identify(:human_resources_supervisor),
+            indicator: ActiveRecord::FixtureSet.identify(:indicators_net_savings)
+          }
+        ]
+      }
     )
   end
 
-  def test_show
-    with(site: site, js: true, admin: admin) do
-      visit edit_admin_plans_plan_project_path(plan, project)
+  def test_show_with_data
+    set_payload
 
+    with(site: site, js: true, admin: admin, window_size: :xl) do
+      visit edit_admin_plans_plan_project_path(plan, project)
+      within("#custom_field_directory") do
+        assert has_content?("John")
+        assert has_content?("666666666")
+        assert has_content?("john@example.org")
+        assert has_content?("1980-12-28")
+        assert has_content?("Supervisor")
+        assert has_content?("Net savings")
+      end
     end
   end
+
 end

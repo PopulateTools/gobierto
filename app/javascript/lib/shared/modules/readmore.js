624354361eb1c7d32d8552e837e25f401663ec71
Hugoren Martinako <aumpfbahn@gmail.com>
2019-09-02 16:11:57 +0200

    component read-more

---
+++ app/javascript/lib/shared/modules/readmore.js
@@ -0,0 +1,8 @@
+$(document).on("turbolinks:load", function() {
+  const elements = document.querySelectorAll(".js-read-more");
+
+  elements.forEach(el => {
+    el.firstElementChild.querySelector("[data-toggle]").addEventListener("click", () => el.classList.add("is-active"));
+    el.lastElementChild.querySelector("[data-toggle]").addEventListener("click", () => el.classList.remove("is-active"));
+  });
+});
---
+++ app/javascript/packs/sandbox.js
@@ -0,0 +1,2 @@
+import "lib/commons";
+import "lib/i18n";

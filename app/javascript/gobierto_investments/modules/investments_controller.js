1f350e5257ad5e43dea8e14e2c9794407c35a492
Hugoren Martinako <aumpfbahn@gmail.com>
2019-09-12 12:13:01 +0200

    handle multiple range sliders

--- app/javascript/gobierto_investments/modules/investments_controller.js
+++ app/javascript/gobierto_investments/modules/investments_controller.js
@@ -1,24 +1,63 @@
-import { rangeSlider } from "lib/globals";
+import { rangeSlider } from "lib/shared";
 
 window.GobiertoInvestments.InvestmentsController = (function() {
   class InvestmentsController {
-    constructor() {}
+    constructor() {
+      this.inRangeClass = "in-range";
+    }
+
     show() {
+      // Initialize all possible rangeSliders
       const rangeSliders = document.querySelectorAll(".js-range-slider");
+      rangeSliders.forEach(slider => this._setRangeSlider(slider));
+    }
 
-      rangeSliders.forEach(slider => {
-        const { min = 0, max = 100, default: defaultRange = [0, 100] } = slider.dataset;
+    _setRangeSlider(slider) {
+      const { min = 0, max = 1, default: defaultRange = [0, 100], rangeBarsSelector } = slider.dataset;
 
-        const fn = (event, ui) => console.log(event, ui);
+      const callback = (_, ui) => {
+        const { values = [] } = ui;
 
-        rangeSlider({
-          elem: slider,
-          min: parseFloat(min),
-          max: parseFloat(max),
-          defaultRange: JSON.parse(defaultRange),
-          rangeCallback: fn
-        });
+        slider.selectedRange = values;
+
+        if (slider.selector) {
+          this._setRangeBar(slider);
+        }
+      };
+
+      // Update values for THIS slider
+      slider.min = parseFloat(min);
+      slider.max = parseFloat(max);
+      slider.selectedRange = JSON.parse(defaultRange);
+
+      rangeSlider({
+        elem: slider,
+        min: slider.min,
+        max: slider.max,
+        defaultRange: slider.selectedRange,
+        rangeCallback: callback
       });
+
+      // Initialize range bars if present
+      if (rangeBarsSelector) {
+        slider.selector = document.querySelector(rangeBarsSelector);
+        this._setRangeBar(slider);
+      }
+    }
+
+    _setRangeBar(rangeBars) {
+      const { min, max, selectedRange, selector } = rangeBars;
+      const bars = selector.children;
+      const step = (max - min) / bars.length;
+      const [start, end] = selectedRange;
+
+      for (let index = 0; index < bars.length; index++) {
+        const bar = bars[index];
+
+        start <= step * index + step + min && end >= step * index + min
+          ? bar.classList.add(this.inRangeClass)
+          : bar.classList.remove(this.inRangeClass);
+      }
     }
   }
 

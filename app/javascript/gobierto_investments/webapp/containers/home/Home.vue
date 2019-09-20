3c4937a7f9b78059cf3f22ad651b85a2b720a5f3
Hugoren Martinako <aumpfbahn@gmail.com>
2019-09-20 14:24:41 +0200

    fix lints

--- app/javascript/gobierto_investments/webapp/containers/home/Home.vue
+++ app/javascript/gobierto_investments/webapp/containers/home/Home.vue
@@ -4,24 +4,24 @@
       <div class="pure-u-1 pure-u-lg-1-4" />
       <div class="pure-u-1 pure-u-lg-3-4">
         <Nav
-:active-tab="activeTabIndex"
-             @active-tab="activeTabIndex = $event"
-/>
+          :active-tab="activeTabIndex"
+          @active-tab="activeTabIndex = $event"
+        />
       </div>
     </div>
 
     <div class="pure-g m_b_4">
       <div class="pure-u-1 pure-u-lg-1-4">
         <Aside
-:filters="filters"
-               @set-filter="filterItems"
-/>
+          :filters="filters"
+          @set-filter="filterItems"
+        />
       </div>
       <div class="pure-u-1 pure-u-lg-3-4">
         <Main
-:active-tab="activeTabIndex"
-              :items="subsetItems"
-/>
+          :active-tab="activeTabIndex"
+          :items="subsetItems"
+        />
       </div>
     </div>
 

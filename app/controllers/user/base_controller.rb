19d053cc0f3ef64466b63510fd89984d19ad2a77
Eduardo Martínez <eduardomech@gmail.com>
2019-09-26 17:51:08 +0200

    Allow definition of read only user fields from auth modules
    In config/application.yml, at the auth modules section, for an auth
    provider a read_only_user_attributes can be defined, with an array of
    user attributes identifiers which the user won't be able to edit. The
    attributes identifiers can be both names of attributes defined in the
    user model or names of custom user fields. The values for these
    attributes are expected to be assigned by the session or census custom
    forms provided by the auth provider

--- app/controllers/user/base_controller.rb
+++ app/controllers/user/base_controller.rb
@@ -3,4 +3,14 @@ class User::BaseController < ApplicationController
   include User::VerificationHelper
 
   layout "user/layouts/application"
+
+  private
+
+  def read_only_user_attributes
+    return [] unless auth_modules_present?
+
+    @read_only_user_attributes ||= current_site.configuration.auth_modules_data.inject([]) do |attributes, auth_module|
+      attributes | (auth_module.read_only_user_attributes || [])
+    end
+  end
 end

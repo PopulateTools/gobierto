8400ff0425bd1695a74aa0f7adbf29364246ccfc
Eduardo Martínez <eduardomech@gmail.com>
2019-10-11 20:08:50 +0200

    Allow definition of session_store key using an environment variable

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,6 +3,6 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store,
-                                       key: "_gobierto_session",
+                                       key: ENV.fetch("GOBIERTO_SESSION_KEY") { "_gobierto_session" },
                                        domain: :all,
                                        tld_length: ENV.fetch("TLD_LENGTH") { "2" }.to_i

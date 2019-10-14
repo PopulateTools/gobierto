c12f74f5359b4d026533fbbc040bb00d8059cae1
Alberto Miedes <albertomg994@gmail.com>
2019-10-14 17:52:09 +0200

    Include url in PersonSerializer

--- app/serializers/gobierto_people/person_serializer.rb
+++ app/serializers/gobierto_people/person_serializer.rb
@@ -1,7 +1,17 @@
 module GobiertoPeople
   class PersonSerializer < ActiveModel::Serializer
 
-    attributes :name, :charge
+    attributes :name, :charge, :url
+
+    def url
+      "#{object.to_url}#{date_range_query}"
+    end
+
+    private
+
+    def date_range_query
+      "?#{instance_options[:date_range_query]}" if instance_options[:date_range_query].present?
+    end
 
   end
 end

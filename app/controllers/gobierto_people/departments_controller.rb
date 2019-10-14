c9c30d97436fb5b16dc748ef5b01907c6c02e911
Alberto Miedes <albertomg994@gmail.com>
2019-10-14 13:09:45 +0200

    Update information available for departments. Move old info to decorator.

--- app/controllers/gobierto_people/departments_controller.rb
+++ app/controllers/gobierto_people/departments_controller.rb
@@ -31,19 +31,13 @@ module GobiertoPeople
     end
 
     def show
-      @department = site_departments.find_by_slug!(params[:id])
-      people = QueryWithEvents.new(source: @department.people.active.with_event_attendances(current_site),
-                                   start_date: filter_start_date,
-                                   end_date: filter_end_date)
+      @department = DepartmentDecorator.new(
+        site_departments.find_by_slug!(params[:id]),
+        filter_start_date: filter_start_date,
+        filter_end_date: filter_end_date
+      )
 
-      meetings = QueryWithEvents.new(source: @department.events.published,
-                                     start_date: filter_start_date,
-                                     end_date: filter_end_date)
-      @department_stats = {
-        total_people_with_attendances: people.count,
-        total_meetings: meetings.count,
-        unique_interest_groups: meetings.select(:interest_group_id).distinct.count
-      }
+      @department_stats = @department.stats
     end
 
     def check_active_submodules

e7eb5fd92ed65a5187a19e88e8bb34269bb2ab95
Jorge Aznar <jorgeatgu@gmail.com>
2019-10-14 14:04:43 +0200

    pitch and bearing

--- app/javascript/gobierto_investments/webapp/components/MapTour.vue
+++ app/javascript/gobierto_investments/webapp/components/MapTour.vue
@@ -1,23 +1,33 @@
 <template>
   <div style="height: 100vh; ">
     <template>
-      <MglMap
-          ref="map"
-          :accessToken="accessToken"
-          :mapStyle.sync="mapStyle"
-      />
+      <MglMap ref="mglMap" :accessToken="accessToken" :mapStyle.sync="mapStyle" @load="onMapLoaded" :scrollZoom="scrollZoom" />
     </template>
     <button class="btn-tour-virtual" @click="backInvestments">
       {{ titleButton }}
     </button>
-    <button class="btn-tour-virtual" @click="flyTo">
+    <button class="btn-tour-virtual" @click="flyToMataro">
       FLYTO
     </button>
+    <ul>
+      <li v-for="item in geojsons">
+        <button @click="flyOnMap(item.coordinates)">{{item.coordinates}}</button>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
 import Mapbox from "mapbox-gl";
-import { MglMap, MglGeojsonLayer } from "vue-mapbox";
+import {
+  MglMap,
+  MglMarker,
+  MglPopup,
+  MglAttributionControl,
+  MglNavigationControl,
+  MglGeolocateControl,
+  MglFullscreenControl,
+  MglScaleControl
+} from "vue-mapbox";
 import Wkt from "wicket";
 import Vue from "vue";
 import axios from "axios";
@@ -27,8 +37,14 @@ export default {
   name: "MapTour",
   mixins: [CommonsMixin],
   components: {
-   MglMap,
-   MglGeojsonLayer
+    MglMap,
+    MglMarker,
+    MglPopup,
+    MglAttributionControl,
+    MglNavigationControl,
+    MglGeolocateControl,
+    MglFullscreenControl,
+    MglScaleControl
   },
   data() {
     return {
@@ -38,13 +54,18 @@ export default {
       style_id: "ck18y48jg11ip1cqeu3b9wpar",
       tilesize: "256",
       accessToken: "pk.eyJ1IjoiYmltdXgiLCJhIjoiY2swbmozcndlMDBjeDNuczNscTZzaXEwYyJ9.oMM71W-skMU6IN0XUZJzGQ",
-      center: [40.199867, -4.0654947],
       titleButton: 'Volver',
+      scrollZoom: false,
       items: [],
-      geojsons: []
+      geojsons: [],
+      map: null,
+      zoom: 16,
+      mapbox: null
     };
   },
   created() {
+    this.mapbox = Mapbox;
+
     this.labelSummary = I18n.t("gobierto_investments.projects.summary");
 
     axios.all([axios.get(baseUrl), axios.get(`${baseUrl}/meta?stats=true`)]).then(responses => {
@@ -62,6 +83,7 @@ export default {
       this.subsetItems = this.items;
 
       this.locations = this.subsetItems.map(d => d.locationOptions);
+
     })
   },
   watch: {
@@ -69,7 +91,15 @@ export default {
       this.setGeoJSONs(items);
     }
   },
+  computed: {
+    center() {
+      return this.geojsons.length !== 0 ? Object.values(this.geojsons).reverse() : [2.451, 41.552];
+    },
+  },
   methods: {
+    onMapLoaded(event) {
+      this.map = event.map;
+    },
     convertWKTtoGeoJSON(wktString) {
       const wkt = new Wkt.Wkt();
       wkt.read(wktString);
@@ -91,13 +121,17 @@ export default {
         const { id, location } = markerWithLocation[index];
         this.geojsons.push({ ...this.convertWKTtoGeoJSON(location), id });
       }
-      console.log(this.geojsons)
-
     },
     backInvestments() {
       this.$router.push({ name: "home" });
+    },
+    flyToMataro() {
+      this.map.flyTo({ center: [2.451, 41.552], pitch: 40, bearing: 40, zoom: this.zoom, speed: 1 });
+    },
+    flyOnMap(coordinates) {
+      const [lat, lng] = Object.values(coordinates)
+      this.map.flyTo({ center: [lat, lng], zoom: this.zoom, speed: 1 });
     }
-
   }
 };
 

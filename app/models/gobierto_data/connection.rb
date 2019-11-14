e7fb91c9387d92b20a933d9ab450f4f353a2d244
Eduardo Martínez <eduardomech@gmail.com>
2019-11-14 23:49:59 +0100

    Define db_config class method with site argument in GobiertoData::Connection

--- app/models/gobierto_data/connection.rb
+++ app/models/gobierto_data/connection.rb
@@ -9,14 +9,18 @@ module GobiertoData
     class << self
       def execute_query(site, query)
         base_connection_config = connection_config
-        return null_query unless (db_config = site&.gobierto_data_settings&.db_config).present?
+        return null_query unless (db_conf = db_config(site)).present?
 
-        establish_connection(db_config)
+        establish_connection(db_conf)
         connection.execute(query)
       rescue ActiveRecord::StatementInvalid => e
         failed_query(e.message)
       ensure
         establish_connection(base_connection_config)
+      end
+
+      def db_config(site)
+        site&.gobierto_data_settings&.db_config
       end
 
       private

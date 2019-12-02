551bbf44b5c017a13b0efbddd12f03e7f04b53bc
Hugoren Martinako <aumpfbahn@gmail.com>
2019-12-02 12:18:44 +0100

    showall component

--- app/javascript/gobierto_investments/webapp/components/Gallery.vue
+++ app/javascript/gobierto_investments/webapp/components/Gallery.vue
@@ -1,13 +1,23 @@
 <template>
-  <div
-    v-if="items.length"
-    class="investments-home-main--gallery"
-  >
-    <GalleryItem
-      v-for="item in items"
-      :key="item.id"
-      :item="item"
-    />
+  <div v-if="items.length">
+    <!-- <div
+      class="investments-home-main--gallery"
+    >
+    </div> -->
+    <transition-group
+      name="fade"
+      mode="out-in"
+      tag="div"
+      class="investments-home-main--gallery"
+    >
+      <GalleryItem
+        v-for="item in visibleItems"
+        :key="item.id"
+        :item="item"
+      />
+    </transition-group>
+
+    <ShowAll @show-all="showAll" />
   </div>
   <div v-else>
     {{ labelEmpty }}
@@ -16,11 +26,13 @@
 
 <script>
 import GalleryItem from "./GalleryItem.vue";
+import ShowAll from "./ShowAll.vue";
 
 export default {
   name: "Gallery",
   components: {
-    GalleryItem
+    GalleryItem,
+    ShowAll
   },
   props: {
     items: {
@@ -30,11 +42,19 @@ export default {
   },
   data() {
     return {
-      labelEmpty: ""
-    }
+      labelEmpty: "",
+      visibleItems: [],
+      maxItems: 24
+    };
   },
   created() {
     this.labelEmpty = I18n.t("gobierto_investments.projects.empty");
+    this.visibleItems = this.items.slice(0, this.maxItems);
   },
+  methods: {
+    showAll(isAllVisible) {
+      this.visibleItems = isAllVisible ? this.items : this.items.slice(0, this.maxItems)
+    }
+  }
 };
 </script>
--- app/javascript/gobierto_investments/webapp/components/Table.vue
+++ app/javascript/gobierto_investments/webapp/components/Table.vue
@@ -1,25 +1,30 @@
 <template>
-  <table
-    v-if="items.length"
-    class="investments-home-main--table"
-  >
-    <thead>
-      <th
-        v-for="column in columns"
-        :key="column.id"
-        class="investments-home-main--th"
+  <div v-if="items.length">
+    <table class="investments-home-main--table">
+      <thead>
+        <th
+          v-for="column in columns"
+          :key="column.id"
+          class="investments-home-main--th"
+        >
+          <div>{{ column.name }}</div>
+        </th>
+      </thead>
+      <transition-group
+        name="fade"
+        tag="tbody"
+        mode="out-in"
       >
-        <div>{{ column.name }}</div>
-      </th>
-    </thead>
-    <tbody>
-      <TableRow
-        v-for="item in items"
-        :key="item.id"
-        :item="item"
-      />
-    </tbody>
-  </table>
+        <TableRow
+          v-for="item in visibleItems"
+          :key="item.id"
+          :item="item"
+        />
+      </transition-group>
+    </table>
+
+    <ShowAll @show-all="showAll" />
+  </div>
   <div v-else>
     {{ labelEmpty }}
   </div>
@@ -27,12 +32,14 @@
 
 <script>
 import TableRow from "./TableRow.vue";
+import ShowAll from "./ShowAll.vue";
 import { CommonsMixin } from "../mixins/common.js";
 
 export default {
   name: "Table",
   components: {
-    TableRow
+    TableRow,
+    ShowAll
   },
   mixins: [CommonsMixin],
   props: {
@@ -44,14 +51,26 @@ export default {
   data() {
     return {
       columns: [],
-      labelEmpty: ""
-    }
+      labelEmpty: "",
+      visibleItems: [],
+      maxItems: 24
+    };
   },
   created() {
     this.labelEmpty = I18n.t("gobierto_investments.projects.empty");
+    this.visibleItems = this.items.slice(0, this.maxItems);
 
-    const { availableTableFields = [] } = this.items.length ? this.items[0] : {}
-    this.columns = availableTableFields
+    const { availableTableFields = [] } = this.items.length
+      ? this.items[0]
+      : {};
+    this.columns = availableTableFields;
   },
+  methods: {
+    showAll(isAllVisible) {
+      this.visibleItems = isAllVisible
+        ? this.items
+        : this.items.slice(0, this.maxItems);
+    }
+  }
 };
 </script>

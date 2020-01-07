6553b6c28e980603ca65aa347017c31a53ad54a9
Eduardo Martínez <eduardomech@gmail.com>
2020-01-07 23:22:48 +0100

    Add favorited_by_user class method to favoriteable concern

--- app/models/concerns/gobierto_data/favoriteable.rb
+++ app/models/concerns/gobierto_data/favoriteable.rb
@@ -11,5 +11,18 @@ module GobiertoData
         favorites.exists?(user_id: user.id)
       end
     end
+
+    class_methods do
+      def favorited_by_user(user, parent: nil)
+        return none unless user.present?
+
+        parent ||= user.site
+        association_name = parent.class.reflect_on_all_associations(:has_many).find { |association| association.options[:class_name] == name }&.name
+
+        return none unless association_name.present?
+
+        parent.send(association_name).joins(:favorites).where(GobiertoData::Favorite.table_name => { user_id: user&.id })
+      end
+    end
   end
 end

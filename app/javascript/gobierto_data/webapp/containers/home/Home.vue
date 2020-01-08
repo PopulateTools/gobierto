e3ff5a61da1e6fd4131ade1901f5136292214eb3
Jorge Aznar <jorgeatgu@gmail.com>
2020-01-08 18:07:09 +0100

    nav menu | scss

--- app/javascript/gobierto_data/webapp/containers/home/Home.vue
+++ app/javascript/gobierto_data/webapp/containers/home/Home.vue
@@ -1,33 +1,13 @@
 <template>
-  <div class="investments">
+  <div class="gobierto-data">
     <div class="pure-g gutters m_b_1">
-      <div class="pure-u-1 pure-u-lg-1-4" />
-      <div class="pure-u-1 pure-u-lg-3-4">
+      <div class="pure-u-1 pure-u-lg-4-4">
         <Nav
           :active-tab="activeTabIndex"
           @active-tab="activeTabIndex = $event"
         />
       </div>
     </div>
-
-    <div class="pure-g gutters m_b_4">
-      <div class="pure-u-1 pure-u-lg-1-4">
-        <Aside
-          :filters="filters"
-          @set-filter="filterItems"
-        />
-      </div>
-      <div class="pure-u-1 pure-u-lg-3-4">
-        <Main
-          v-if="items.length"
-          :active-tab="activeTabIndex"
-          :items="subsetItems"
-        />
-      </div>
-    </div>
-
-    <article>
-    </article>
   </div>
 </template>
 
@@ -42,13 +22,7 @@ export default {
   },
   data() {
     return {
-      items: [],
-      subsetItems: [],
-      dictionary: [],
-      filters: [],
-      phases: [],
-      activeTabIndex: 0,
-      labelSummary: ""
+      activeTabIndex: 0
     };
   },
   methods: {
--- app/javascript/gobierto_data/webapp/containers/home/Nav.vue
+++ app/javascript/gobierto_data/webapp/containers/home/Nav.vue
@@ -1,32 +1,61 @@
 <template>
-  <nav class="investments-home-nav">
+  <nav class="gobierto-data-home-nav">
     <ul>
       <li
         :class="{ 'is-active': activeTab === 0 }"
-        class="investments-home-nav--tab"
+        class="gobierto-data-home-nav--tab"
         @click="activateTab(0)"
       >
-        <i class="fas fa-map" />
-        <i class="far fa-map" />
-        <span>{{ labelMap }}</span>
+        <span>{{ labelDatasets }}</span>
       </li>
       <li
         :class="{ 'is-active': activeTab === 1 }"
-        class="investments-home-nav--tab"
+        class="gobierto-data-home-nav--tab"
         @click="activateTab(1)"
       >
-        <i class="fas fa-clone" />
-        <i class="far fa-clone" />
-        <span>{{ labelGallery }}</span>
+        <span>{{ labelQueries }}</span>
       </li>
       <li
         :class="{ 'is-active': activeTab === 2 }"
-        class="investments-home-nav--tab"
+        class="gobierto-data-home-nav--tab"
         @click="activateTab(2)"
       >
-        <i class="fas fa-list-alt" />
-        <i class="far fa-list-alt" />
-        <span>{{ labelList }}</span>
+        <span>{{ labelVisualizations }}</span>
+      </li>
+      <li
+        :class="{ 'is-active': activeTab === 3 }"
+        class="gobierto-data-home-nav--tab"
+        @click="activateTab(3)"
+      >
+        <span>{{ labelDashboards }}</span>
+      </li>
+      <li
+        :class="{ 'is-active': activeTab === 4 }"
+        class="gobierto-data-home-nav--tab"
+        @click="activateTab(4)"
+      >
+        <span>{{ labelAnalysis }}</span>
+      </li>
+      <li
+        :class="{ 'is-active': activeTab === 5 }"
+        class="gobierto-data-home-nav--tab"
+        @click="activateTab(5)"
+      >
+        <span>{{ labelNews }}</span>
+      </li>
+      <li
+        :class="{ 'is-active': activeTab === 6 }"
+        class="gobierto-data-home-nav--tab"
+        @click="activateTab(6)"
+      >
+        <span>{{ labelInformation }}</span>
+      </li>
+      <li
+        :class="{ 'is-active': activeTab === 7 }"
+        class="gobierto-data-home-nav--tab"
+        @click="activateTab(7)"
+      >
+        <span>{{ labelAPI }}</span>
       </li>
     </ul>
   </nav>
@@ -43,15 +72,25 @@ export default {
   },
   data() {
     return {
-      labelMap: "",
-      labelGallery: "",
-      labelList: "",
+      labelDatasets: "",
+      labelQueries: "",
+      labelVisualizations: "",
+      labelDashboards: "",
+      labelAnalysis: "",
+      labelNews: "",
+      labelInformation: "",
+      labelAPI: ""
     };
   },
   created() {
-    this.labelMap = I18n.t("gobierto_investments.projects.map")
-    this.labelGallery = I18n.t("gobierto_investments.projects.gallery")
-    this.labelList = I18n.t("gobierto_investments.projects.list")
+    this.labelDatasets = I18n.t("gobierto_data.projects.dataset")
+    this.labelQueries = I18n.t("gobierto_data.projects.queries")
+    this.labelVisualizations = I18n.t("gobierto_data.projects.visualizations")
+    this.labelDashboards = I18n.t("gobierto_data.projects.dashboards")
+    this.labelAnalysis = I18n.t("gobierto_data.projects.analysis")
+    this.labelNews = I18n.t("gobierto_data.projects.news")
+    this.labelInformation = I18n.t("gobierto_data.projects.info")
+    this.labelAPI = I18n.t("gobierto_data.projects.api")
   },
   methods: {
     activateTab(index) {
@@ -60,4 +99,3 @@ export default {
   }
 };
 </script>
-

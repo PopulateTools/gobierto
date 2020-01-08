869b23fe24ea1e1542ff928971b41cf70986be76
Jorge Aznar <jorgeatgu@gmail.com>
2020-01-08 17:24:07 +0100

    create models es en cat

--- app/javascript/gobierto_data/webapp/components/Table.vue
+++ app/javascript/gobierto_data/webapp/components/Table.vue
@@ -1,40 +1,13 @@
 <template>
-  <table
-    v-if="items.length"
-    class="investments-home-main--table"
-  >
-    <thead>
-      <th
-        v-for="column in columns"
-        :key="column.id"
-        class="investments-home-main--th"
-      >
-        <div>{{ column.name }}</div>
-      </th>
-    </thead>
-    <tbody>
-      <TableRow
-        v-for="item in items"
-        :key="item.id"
-        :item="item"
-      />
-    </tbody>
-  </table>
-  <div v-else>
-    {{ labelEmpty }}
+  <div>
+    <p>Tabla</p>
   </div>
 </template>
 
 <script>
-import TableRow from "./TableRow.vue";
-import { CommonsMixin } from "../mixins/common.js";
 
 export default {
   name: "Table",
-  components: {
-    TableRow
-  },
-  mixins: [CommonsMixin],
   props: {
     items: {
       type: Array,
@@ -43,15 +16,7 @@ export default {
   },
   data() {
     return {
-      columns: [],
-      labelEmpty: ""
     }
-  },
-  created() {
-    this.labelEmpty = I18n.t("gobierto_investments.projects.empty");
-
-    const { availableTableFields = [] } = this.items.length ? this.items[0] : {}
-    this.columns = availableTableFields
-  },
+  }
 };
 </script>

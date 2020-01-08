41a9da5f4cda5a6bb41d487d8918a27f52932824
Jorge Aznar <jorgeatgu@gmail.com>
2020-01-08 16:03:04 +0100

    replicate structure

---
+++ app/javascript/gobierto_data/mixins/common.js
@@ -0,0 +1,288 @@
+import { VueFiltersMixin } from "lib/shared";
+import axios from "axios";
+
+const CONFIGURATION = {
+  title: {
+    id: "title_translations"
+  },
+  description: {
+    id: "descripcio-projecte"
+  },
+  phases: {
+    id: "estat"
+  },
+  location: {
+    id: "wkt",
+    center: [41.536908, 2.4418503],
+    minZoom: 13,
+    maxZoom: 16
+  },
+  availableFilters: [
+    {
+      id: "range",
+      startKey: "data-inici",
+      endKey: "data-fin"
+    },
+    {
+      id: "estat",
+      multiple: true
+    },
+    {
+      id: "nom-servei-responsable",
+      multiple: true
+    },
+    {
+      id: "tipus-projecte"
+    },
+    {
+      id: "import"
+    }
+  ],
+  availableGalleryFields: [
+    {
+      id: "data-inici"
+    },
+    {
+      id: "data-final"
+    },
+    {
+      id: "import",
+      filter: "money"
+    },
+    {
+      id: "adjudicatari"
+    }
+  ],
+  availableTableFields: [
+    {
+      id: "nom-projecte"
+    },
+    {
+      id: "estat",
+      multiple: true
+    },
+    {
+      id: "import",
+      filter: "money"
+    }
+  ],
+  availableProjectFields: [
+    {
+      id: "nom-projecte"
+    },
+    {
+      id: "nom-servei-responsable",
+      multiple: true
+    },
+    {
+      id: "tipus-projecte",
+      multiple: true
+    },
+    {
+      id: "descripcio-projecte"
+    },
+    {
+      id: "notes"
+    },
+    {
+      id: "adreca"
+    },
+    {
+      id: "adjudicatari"
+    },
+    {
+      id: "estat",
+      multiple: true
+    },
+    {
+      id: "data-inici",
+      filter: "date"
+    },
+    {
+      id: "data-adjudicacio",
+      filter: "date"
+    },
+    {
+      id: "data-inici-redaccio",
+      filter: "date"
+    },
+    {
+      id: "data-fi-redaccio",
+      filter: "date"
+    },
+    {
+      id: "data-final",
+      type: "icon",
+      icon: {
+        href: "https://twitter.com",
+        name: "file"
+      }
+    },
+    {
+      id: "partida",
+      filter: "money",
+      type: "highlight"
+    },
+    {
+      id: "import",
+      filter: "money"
+    },
+    {
+      id: "import-adjudicacio",
+      filter: "money"
+    },
+    {
+      id: "import-liquidacio",
+      filter: "money"
+    },
+    {
+      id: "tasques",
+      type: "table",
+      table: {
+        columns: ["nomactuacio", "nimport"]
+      }
+    }
+  ],
+  itemSpecialConfiguration: {
+    fn: async data => {
+      const dataArr = Array.isArray(data) ? data : [data];
+      const filterArr = dataArr.filter(d => d.attributes.partida && d.attributes["any-partida"]);
+      const allIds = filterArr.map(d => `'${d.attributes.partida}'`).join(",");
+
+      // Avoid the query if there's no code_with_zone (partida)
+      if (allIds) {
+        const endpoint = `${location.origin}/api/v1/data`;
+        const query = `
+          SELECT code_with_zone as partida, paranyprs as year, sum(parimport) as budget
+          FROM mataro_budgets
+          WHERE "parimport" IS NOT NULL
+          AND "code_with_zone" IN (${allIds})
+          GROUP BY code_with_zone, paranyprs
+        `;
+
+        const { data: { data: sql } } = await axios.get(endpoint, {
+          params: {
+            sql: query.trim()
+          }
+        })
+
+        for (let index = 0; index < dataArr.length; index++) {
+          const { attributes: { partida, "any-partida": year } } = dataArr[index];
+          const { budget } = sql.find(d => d.partida === partida && d.year === year) || {};
+
+          dataArr[index].attributes.old_partida = partida
+          dataArr[index].attributes.partida = budget ? budget : null
+        }
+      }
+
+      return Array.isArray(data) ? dataArr : dataArr[0];
+    }
+  }
+};
+
+export const baseUrl = `${location.origin}/gobierto_investments/api/v1/projects`;
+
+export const CommonsMixin = {
+  mixins: [VueFiltersMixin],
+  methods: {
+    nav(item) {
+      this.$router.push({ name: "project", params: { id: item.id, item } });
+    },
+    getFilters(stats) {
+      const { availableFilters } = CONFIGURATION;
+      const filters = [];
+      for (let index = 0; index < availableFilters.length; index++) {
+        const { id: key, ...rest } = availableFilters[index];
+        const element = stats[key];
+        const { field_type: type, vocabulary_terms: options = [], name_translations: title = {} } = this.getAttributesByKey(key);
+
+        filters.push({
+          ...element,
+          ...rest,
+          title: this.translate(title),
+          options: Array.isArray(options) ? options.map(opt => ({ ...opt, title: this.translate(opt.name_translations) })) : [],
+          type: type ? type : key,
+          key
+        });
+      }
+
+      return filters;
+    },
+    getPhases(stats) {
+      const {
+        phases: { id }
+      } = CONFIGURATION;
+      const { vocabulary_terms = [] } = this.getAttributesByKey(id);
+      const { distribution = [] } = stats[id];
+      return vocabulary_terms.map(term => {
+        const { name_translations: title = {} } = term;
+        const { count = 0 } = distribution.find(el => parseFloat(JSON.parse(el.value)) === parseFloat(term.id)) || {};
+
+        return {
+          ...term,
+          title: this.translate(title),
+          count
+        };
+      });
+    },
+    getItem(element, attributes) {
+      const attr = this.getAttributesByKey(element.id);
+
+      let value = attributes[element.id];
+
+      if (element.multiple) {
+        value = this.translate(attributes[element.id][0].name_translations);
+      }
+
+      return {
+        ...attr,
+        ...element,
+        name: this.translate(attr.name_translations),
+        value: value
+      };
+    },
+    setItem(element) {
+      const { attributes = {} } = element;
+      const { title, description, phases, location, availableGalleryFields, availableTableFields, availableProjectFields } = CONFIGURATION;
+      const { id: locationId, ...restLocationOptions } = location;
+
+      return {
+        ...element,
+        title: this.translate(attributes[title.id]),
+        description: attributes[description.id] || "",
+        photo: Array.isArray(attributes.gallery) ? attributes.gallery[0] : "",
+        gallery: attributes.gallery || [],
+        location: attributes[locationId],
+        locationOptions: restLocationOptions || {},
+        phases: attributes[phases.id].map(element => ({ ...element, title: this.translate(element.name_translations) })),
+        phasesFieldName: this.translate(this.getItem(phases, attributes).name_translations),
+        availableGalleryFields: availableGalleryFields.map(element => this.getItem(element, attributes)),
+        availableTableFields: availableTableFields.map(element => this.getItem(element, attributes)),
+        availableProjectFields: availableProjectFields.map(element => this.getItem(element, attributes))
+      };
+    },
+    setData(data) {
+      return data.map(element => this.setItem(element));
+    },
+    async alterDataObjectOptional(data) {
+      const { itemSpecialConfiguration } = CONFIGURATION;
+
+      let spreadData = data;
+      if (itemSpecialConfiguration) {
+        const { fn = () => data } = itemSpecialConfiguration;
+        spreadData = await fn(data);
+      }
+
+      return Array.isArray(spreadData) ? this.setData(spreadData) : this.setItem(spreadData);
+    },
+    getAttributesByKey(prop) {
+      const { attributes = {} } =
+        this.dictionary.find(entry => {
+          const { attributes: { uid = "" } = {} } = entry;
+          return uid === prop;
+        }) || {};
+
+      return attributes;
+    }
+  }
+};
--- app/javascript/gobierto_data/webapp/containers/home/Home.vue
+++ app/javascript/gobierto_data/webapp/containers/home/Home.vue
@@ -38,7 +38,7 @@
 
 <script>
 import Nav from "./Nav.vue";
-import axios from "axios";
+
 
 export default {
   name: "Home",
@@ -56,72 +56,7 @@ export default {
       labelSummary: ""
     };
   },
-  async created() {
-    this.labelSummary = I18n.t("gobierto_investments.projects.summary");
-
-    const [
-      {
-        data: { data: items = [] }
-      },
-      {
-        data: { data: attributesDictionary = [], meta: filtersFromConfiguration }
-      }
-    ] = await axios.all([axios.get(baseUrl), axios.get(`${baseUrl}/meta?stats=true`)]);
-
-    this.dictionary = attributesDictionary;
-    this.items = this.setData(items);
-
-    if (filtersFromConfiguration) {
-      // get the phases, and append what items are in that phase
-      const phases = this.getPhases(filtersFromConfiguration);
-      this.phases = phases.map(phase => ({
-        ...phase,
-        items: this.items.filter(d => (d.phases.length ? d.phases[0].id === phase.id : false))
-      }));
-
-      // Add dictionary of phases in order to fulfill project page
-      this.items = this.items.map(item => ({ ...item, phasesDictionary: phases }));
-
-      this.filters = this.getFilters(filtersFromConfiguration) || [];
-
-      if (this.filters.length) {
-        this.activeFilters = new Map();
-        this.filters.forEach(f => {
-          // initialize active filters
-          this.activeFilters.set(f.key, undefined);
-
-          if (f.type === "vocabulary_options") {
-            // Add a counter for each option
-            f.options = f.options.map(opt => ({
-              ...opt,
-              counter: this.items.filter(i => i.attributes[f.key].map(g => g.id).includes(opt.id)).length
-            }));
-          }
-        });
-      }
-    }
-
-    // Assign this object BEFORE next function for better performance
-    this.subsetItems = this.items;
-
-    // Optional callback to update data in background, setup in CONFIGURATION object
-    this.items = await this.alterDataObjectOptional(this.items);
-    // Once items is updated, assign again the result
-    this.subsetItems = this.items;
-  },
   methods: {
-    filterItems(filter, key) {
-      this.activeFilters.set(key, filter);
-
-      let results = this.items;
-      this.activeFilters.forEach(activeFn => {
-        if (activeFn) {
-          results = results.filter(d => activeFn(d.attributes));
-        }
-      });
-
-      this.subsetItems = results;
-    }
   }
 };
 </script>

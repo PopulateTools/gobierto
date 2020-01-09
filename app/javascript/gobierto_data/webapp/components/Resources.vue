f7c19e57fcd23598dc140f6b9712f0cb568589e5
Jorge Aznar <jorgeatgu@gmail.com>
2020-01-09 18:28:46 +0100

    resources component

---
+++ app/javascript/gobierto_data/webapp/components/Resources.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="gobierto-data-summary-resources">
+    <h2 class="gobierto-data-tabs-section-title">
+      <i class="fas fa-caret-down" />
+      {{ labelResources }}
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Resources",
+  data() {
+    return {
+      labelResources: ""
+    }
+  },
+  created() {
+    this.labelResources = I18n.t("gobierto_data.projects.resources")
+  }
+}
+</script>
--- app/javascript/gobierto_data/webapp/components/datasets/DatasetsSummary.vue
+++ app/javascript/gobierto_data/webapp/components/datasets/DatasetsSummary.vue
@@ -24,30 +24,48 @@
         <span class="gobierto-data-summary-header-container-label">
           {{ labelSubject }}
         </span>
-        <a href="" class="gobierto-data-summary-header-container-text">
+        <a
+          href=""
+          class="gobierto-data-summary-header-container-text">
           Urbanismo e infraestructuras
         </a>
       </div>
     </div>
     <div class="pure-u-1-2">
-      <p class="gobierto-data-summary-header-description">Este conjuntos de datos contiene el detalle de más de 1.200 elementos para actividades de mayores de la ciudad de Madrid con su tipología y coordenadas. En este portal tambien están disponibles otros.</p>
+      <p class="gobierto-data-summary-header-description">
+        {{ datasetDescription }}
+      </p>
     </div>
+    <button class="gobierto-data-summary-btn-download-data">
+      <i class="fas fa-download" />
+      {{ labelDownloadData }}
+    </button>
+    <Resources />
   </div>
 </template>
+
 <script>
+import Resources from "./../Resources.vue";
+
 export default {
   name: "DatasetsSummary",
+  components: {
+    Resources
+  },
   data() {
     return {
       labelUpdated: '',
       labelFrequency: '',
-      labelSubject: ''
+      labelSubject: '',
+      labelDownloadData: '',
+      datasetDescription: 'Este conjuntos de datos contiene el detalle de más de 1.200 elementos para actividades de mayores de la ciudad de Madrid con su tipología y coordenadas. En este portal tambien están disponibles otros.'
     }
   },
   created() {
     this.labelUpdated = I18n.t("gobierto_data.projects.updated")
     this.labelFrequency = I18n.t("gobierto_data.projects.frequency")
     this.labelSubject = I18n.t("gobierto_data.projects.subject")
+    this.labelDownloadData = I18n.t("gobierto_data.projects.downloadData")
   },
 }
 

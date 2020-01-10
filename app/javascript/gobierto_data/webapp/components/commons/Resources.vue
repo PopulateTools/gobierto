5223b08ad8c21484bfc8c73c28be8519a01d863b
Jorge Aznar <jorgeatgu@gmail.com>
2020-01-10 10:48:10 +0100

    component resources

--- app/javascript/gobierto_data/webapp/components/commons/Resources.vue
+++ app/javascript/gobierto_data/webapp/components/commons/Resources.vue
@@ -4,19 +4,103 @@
       <i class="fas fa-caret-down" />
       {{ labelResources }}
     </h2>
+    <div class="gobierto-data-resources-list">
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        class="gobierto-data-resources-list-element"
+      >
+        <i class="fas fa-file-excel" />
+        <span class="gobierto-data-resources-list-element-name">{{ item.file.name }}</span>
+        <span class="gobierto-data-resources-list-element-size">{{ item.file.size }}</span>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: "Resources",
   data() {
     return {
-      labelResources: ""
+      labelResources: "",
+      items: [{
+          file: {
+            name: 'Actuaciones Bomberos Enero 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Febrero 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Marzo 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Abril 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Mayo 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Junio 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Julio 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Agosto 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Septiembre 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Octubre 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Noviembre 2019.xls',
+            size: '232kb'
+          }
+        },
+        {
+          file: {
+            name: 'Actuaciones Bomberos Diciembre 2019.xls',
+            size: '232kb'
+          }
+        }
+      ]
     }
   },
   created() {
     this.labelResources = I18n.t("gobierto_data.projects.resources")
   }
 }
+
 </script>

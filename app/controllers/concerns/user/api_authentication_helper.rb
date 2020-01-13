ff5d16186026e43d5f8c59dd977ebb42edbcc5bf
Eduardo Martínez <eduardomech@gmail.com>
2020-01-13 14:50:23 +0100

    Add api authentication helper concern and include in data api controllers

---
+++ app/controllers/concerns/user/api_authentication_helper.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module User::ApiAuthenticationHelper
+  extend ActiveSupport::Concern
+
+  included do
+    if respond_to?(:helper_method)
+      helper_method :current_user, :user_authenticated?
+    end
+  end
+
+  private
+
+  def current_user
+    @current_user ||= find_current_user
+  end
+
+  def user_authenticated?
+    current_user.present?
+  end
+
+  def authenticate_user!
+    raise_unauthorized unless user_authenticated?
+  end
+
+  def find_current_user
+    if token.present?
+      User.confirmed.joins(:api_tokens).find_by(site_id: current_site.id, user_api_tokens: { token: token })
+    end
+  end
+
+  def raise_unauthorized
+    render(json: { message: "Unauthorized" }, status: :unauthorized, adapter: :json_api) && return
+  end
+
+  def token
+    @token = request.headers["token"] || params["token"]
+  end
+end

551aadc777667e7edb2c1f20b9d435d1b00c4de3
Jorge Aznar <jorgeatgu@gmail.com>
2020-01-24 18:17:39 +0100

    create routes | fix layouts | fix pure

--- app/javascript/gobierto_data/webapp/layouts/LayoutDataset.vue
+++ app/javascript/gobierto_data/webapp/layouts/LayoutDataset.vue
@@ -32,7 +32,7 @@ export default {
   data() {
     return {
       activeTabIndex: 0,
-      activeTabSidebar: 0
+      activeTabSidebar: 1
     }
   }
 }
--- app/javascript/gobierto_data/webapp/pages/DataSets.vue
+++ app/javascript/gobierto_data/webapp/pages/DataSets.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="pure-u-1 pure-u-lg-3-4 gobierto-data-layout-column">
     <NavDatasets
       :active-tab="activeTabIndex"
       @active-tab="activeTabIndex = $event"

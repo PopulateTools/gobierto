a410f6c035f5547024268163354f0614693d5ffa
Jorge Aznar <jorgeatgu@gmail.com>
2020-01-30 12:54:09 +0100

    new directive to modal queries

--- app/javascript/gobierto_data/webapp/components/commons/Queries.vue
+++ app/javascript/gobierto_data/webapp/components/commons/Queries.vue
@@ -20,7 +20,7 @@
             class="gobierto-data-summary-queries-container"
             @mouseover="showCode(index)"
             @mouseleave="hideCode = true"
-            @click="sendQuery(item); changeTab()"
+            @click="runYourQuery(arrayQueries[index].attributes.sql);sendQuery(item); closeModal(); changeTab()"
           >
             <span class="gobierto-data-summary-queries-container-name"> {{ item.attributes.name }}</span>
 
@@ -89,7 +89,7 @@
             class="gobierto-data-summary-queries-container"
             @mouseover="showCode(index)"
             @mouseleave="hideCode = true"
-            @click="sendQuery(item); changeTab()"
+            @click="runYourQuery(arrayQueries[index].attributes.sql);sendQuery(item); closeModal(); changeTab()"
           >
             <span class="gobierto-data-summary-queries-container-name"> {{ item.attributes.name }}</span>
 
@@ -123,6 +123,7 @@
   </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
   name: "Queries",
   props: {
@@ -144,8 +145,8 @@ export default {
       totalQueries: this.arrayQueries.length + this.numberFavQueries,
       showSection: true,
       showYourQueries: true,
-      showYourFavQueries: false,
-      showYourTotalQueries: false
+      showYourFavQueries: true,
+      showYourTotalQueries: true
     }
   },
   created() {
@@ -155,6 +156,9 @@ export default {
     this.labelAll = I18n.t("gobierto_data.projects.all")
   },
   methods: {
+    closeModal() {
+      this.$root.$emit('closeQueriesModal');
+    },
     showCode(index) {
       this.hideCode = false
       this.sqlCode = this.arrayQueries[index].attributes.sql
@@ -170,6 +174,56 @@ export default {
     },
     changeTab() {
       this.$root.$emit('changeNavTab')
+    },
+    runYourQuery(code) {
+      this.showSpinner = true;
+      this.queryEditor = encodeURI(code)
+      this.$root.$emit('postRecentQuery', code)
+      this.$root.$emit('showMessages', false)
+      this.$root.$emit('updateCode', code)
+
+      if (this.queryEditor.includes('LIMIT')) {
+        this.queryEditor = this.queryEditor
+      } else {
+        this.$root.$emit('sendCompleteQuery', this.queryEditor)
+        this.code = `SELECT%20*%20FROM%20(${this.queryEditor})%20AS%20data_limited_results%20LIMIT%20100%20OFFSET%200`
+        this.queryEditor = this.code
+      }
+      this.urlPath = location.origin
+      this.endPoint = '/api/v1/data/data';
+      this.url = `${this.urlPath}${this.endPoint}?sql=${this.queryEditor}`
+
+      axios
+        .get(this.url)
+        .then(response => {
+          this.data = []
+          this.keysData = []
+          this.rawData = response.data
+          this.meta = this.rawData.meta
+          this.data = this.rawData.data
+
+          this.queryDurationRecors = [this.meta.rows, this.meta.duration]
+
+          this.keysData = Object.keys(this.data[0])
+
+          this.$root.$emit('recordsDuration', this.queryDurationRecors)
+          this.$root.$emit('sendData', this.keysData, this.data)
+          this.$root.$emit('showMessages', true)
+          this.$root.$emit('sendQueryCode', this.queryCode)
+
+        })
+        .catch(error => {
+          const messageError = error.response.data.errors[0].sql
+          this.$root.$emit('apiError', messageError)
+
+          this.data = []
+          this.keysData = []
+          this.$root.$emit('sendData', this.keysData, this.data)
+        })
+
+        setTimeout(() => {
+          this.showSpinner = false
+        }, 300)
     }
   }
 }

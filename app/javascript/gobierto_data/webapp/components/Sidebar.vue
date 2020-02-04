99bc8741c24aa06ab8658e7d3ad5a12e4148dad8
Jorge Aznar <jorgeatgu@gmail.com>
2020-02-04 14:56:25 +0100

    reorder datasets

--- app/javascript/gobierto_data/webapp/components/Sidebar.vue
+++ app/javascript/gobierto_data/webapp/components/Sidebar.vue
@@ -99,6 +99,8 @@ export default {
 
         this.allDatasets = this.rawData.data
 
+        this.allDatasets.sort((a, b) => a.attributes.name.localeCompare(b.attributes.name));
+
         this.titleDataset = this.rawData.data[0].attributes.name
         this.slugDataset = this.rawData.data[0].attributes.slug
         this.getColumns(this.slugDataset)

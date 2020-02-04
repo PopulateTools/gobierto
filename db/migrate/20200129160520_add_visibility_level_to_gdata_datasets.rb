dd5c6abd6a7474a5919caf9f42a973703343331d
Eduardo Martínez <eduardomech@gmail.com>
2020-02-04 16:46:16 +0100

    Add task to publish datasets on migration to add visibility_level

--- db/migrate/20200129160520_add_visibility_level_to_gdata_datasets.rb
+++ db/migrate/20200129160520_add_visibility_level_to_gdata_datasets.rb
@@ -3,5 +3,7 @@
 class AddVisibilityLevelToGdataDatasets < ActiveRecord::Migration[5.2]
   def change
     add_column :gdata_datasets, :visibility_level, :integer, null: false, default: 0
+
+    GobiertoData::Dataset.update_all(visibility_level: 1)
   end
 end

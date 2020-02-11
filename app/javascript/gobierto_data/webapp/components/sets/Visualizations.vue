d3d117a2c71dfc0f17e92a04e4f5ceb58d305dd6
Jorge Aznar <jorgeatgu@gmail.com>
2020-02-11 16:26:47 +0100

    init perspective·

--- app/javascript/gobierto_data/webapp/components/sets/Visualizations.vue
+++ app/javascript/gobierto_data/webapp/components/sets/Visualizations.vue
@@ -1,11 +1,40 @@
 <template>
-  <div class="gobierto-data-sets-nav--tab-container">
-    <p>Visualizations</p>
+  <div
+    id="container"
+    class="gobierto-data-sets-nav--tab-container"
+  >
+    <perspective-viewer editable />
   </div>
 </template>
 <script>
-
 export default {
-  name: "Visualizations"
+  name: "Visualizations",
+  data() {
+    return {
+      dataPerspective: []
+    }
+  },
+  methods: {
+    loadData(table){
+      this.dataPerspective = table
+      console.log("this.dataPerspective", this.dataPerspective);
+      this.initPerspective(this.dataPerspective)
+    },
+    initPerspective(data){
+      const viewer = document.getElementsByTagName("perspective-viewer")[0];
+      viewer.load(data);
+
+
+    }
+  }
 }
 </script>
+<style>
+#container {
+    background-color: #ccc;
+}
+
+perspective-viewer {
+    overflow: hidden;
+}
+</style>

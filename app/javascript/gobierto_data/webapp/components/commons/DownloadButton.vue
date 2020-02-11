b5538c5de5dd2634bf2747b671a1cce0ae22fdc3
Jorge Aznar <jorgeatgu@gmail.com>
2020-02-11 12:55:08 +0100

    resolved changes @crashillo

--- app/javascript/gobierto_data/webapp/components/commons/DownloadButton.vue
+++ app/javascript/gobierto_data/webapp/components/commons/DownloadButton.vue
@@ -18,19 +18,19 @@
           v-show="!isHidden"
           class="gobierto-data-btn-download-data-modal"
         >
-          <div
+          <template
             v-for="(item, key, index) in arrayFormats"
-            :key="index"
-            :item="item"
           >
             <a
+              :key="index"
+              :item="item"
               :href="editor ? sqlfileCSV : item"
               :download="titleDataset"
               class="gobierto-data-btn-download-data-modal-element"
             >
               {{ key }}
             </a>
-          </div>
+          </template>
         </div>
       </transition>
     </keep-alive>

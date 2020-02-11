c9972c1667813d29d7aa1a9730d89b413d1e7ab7
Jorge Aznar <jorgeatgu@gmail.com>
2020-02-11 18:01:26 +0100

    included perspective library into sqleditor

--- app/javascript/gobierto_data/webapp/components/sets/SQLEditorVisualizations.vue
+++ app/javascript/gobierto_data/webapp/components/sets/SQLEditorVisualizations.vue
@@ -1,10 +1,56 @@
 <template>
-  <div>
-    <p>Visualizations</p>
+  <div id="container">
+    <perspective-viewer editable />
   </div>
 </template>
 <script>
 export default {
-  name: 'SQLEditorVisualizations'
+  name: 'SQLEditorVisualizations',
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      dataPerspective: []
+    }
+  },
+  watch:{
+    items: function(newValue){
+      this.dataPerspective = JSON.parse(newValue);
+    }
+  },
+  created(){
+    this.$root.$on('sendDataViz', this.updateValues)
+    this.$nextTick(() => {
+      this.dataPerspective = this.items
+      this.initPerspective(this.dataPerspective)
+    })
+  },
+  methods: {
+    updateValues(values) {
+      this.initPerspective(values)
+    },
+    initPerspective(data){
+      const viewer = document.getElementsByTagName("perspective-viewer")[0];
+      viewer.load(data);
+    }
+  }
 }
 </script>
+<style>
+#container {
+  background-color: #ccc;
+  width: 100%;
+  height: 100%;
+  letter-spacing: 0;
+}
+
+perspective-viewer {
+  overflow: hidden;
+  width: 100%;
+  height: 600px;
+}
+</style>

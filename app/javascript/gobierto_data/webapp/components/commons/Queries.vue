36c055b2eda693ce3623942ecadaa74b9d8e3e61
Jorge Aznar <jorgeatgu@gmail.com>
2020-02-12 12:29:31 +0100

    import perspective modules

--- app/javascript/gobierto_data/webapp/components/commons/Queries.vue
+++ app/javascript/gobierto_data/webapp/components/commons/Queries.vue
@@ -258,6 +258,7 @@ export default {
 
           this.$root.$emit('recordsDuration', this.queryDurationRecors)
           this.$root.$emit('sendData', this.keysData, this.data)
+          this.$root.$emit('sendDataViz', this.data)
           this.$root.$emit('showMessages', true, false)
           this.$root.$emit('sendQueryCode', this.queryCode)
 

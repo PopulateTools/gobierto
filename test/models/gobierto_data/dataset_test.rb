a239cf5c79961428698d793259f9189a2d14cf10
Eduardo Martínez <eduardomech@gmail.com>
2020-02-13 19:20:12 +0100

    Add some tests

--- test/models/gobierto_data/dataset_test.rb
+++ test/models/gobierto_data/dataset_test.rb
@@ -8,8 +8,29 @@ module GobiertoData
       @subject ||= gobierto_data_datasets(:users_dataset)
     end
 
+    def site
+      @site ||= sites(:madrid)
+    end
+
     def test_valid
       assert subject.valid?
+    end
+
+    def test_attachments_collection
+      dataset = site.datasets.create(
+        name_translations: { en: "Departments", es: "Departamentos" },
+        table_name: "gp_departments"
+      )
+
+      assert dataset.attachments_collection.present?
+      assert_equal dataset.attachments_collection, dataset.attachments_collection!
+    end
+
+    def test_attachments_collection!
+      assert_nil subject.attachments_collection
+      subject.attachments_collection!
+      assert subject.attachments_collection.present?
+      assert_equal subject.attachments_collection, subject.attachments_collection!
     end
   end
 end

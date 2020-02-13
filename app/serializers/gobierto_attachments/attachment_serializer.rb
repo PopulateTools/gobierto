c5f48911e50b7cdc2971ab497c4a450ce53b50db
Eduardo Martínez <eduardomech@gmail.com>
2020-02-13 22:53:20 +0100

    Include attachments in datasets meta response

---
+++ app/serializers/gobierto_attachments/attachment_serializer.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module GobiertoAttachments
+  class AttachmentSerializer < ActiveModel::Serializer
+    attributes(
+      :name,
+      :description,
+      :file_name,
+      :file_digest,
+      :url,
+      :human_readable_url,
+      :file_size,
+      :current_version,
+      :created_at,
+      :updated_at
+    )
+  end
+end

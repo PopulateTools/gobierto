88b8b97b41e64d96f46b8069ec146b04a9bb232f
Jorge Aznar <jorgeatgu@gmail.com>
2020-02-14 19:17:26 +0100

    resolved changes @crashillo

--- app/javascript/gobierto_data/webapp/components/commons/Queries.vue
+++ app/javascript/gobierto_data/webapp/components/commons/Queries.vue
@@ -217,9 +217,9 @@ export default {
       axios
         .get(this.url)
         .then(response => {
-          this.rawData = response.data
-          this.items = this.rawData.data
-          this.arrayQueries = this.items
+          const rawData = response.data
+          const items = rawData.data
+          this.arrayQueries = items
         })
         .catch(error => {
           const messageError = error.response
@@ -248,18 +248,18 @@ export default {
       axios
         .get(this.url)
         .then(response => {
-          this.data = []
-          this.keysData = []
-          this.rawData = response.data
-          this.meta = this.rawData.meta
-          this.data = this.rawData.data
+          let data = []
+          let keysData = []
+          const rawData = response.data
+          const meta = rawData.meta
+          data = rawData.data
 
-          this.queryDurationRecors = [this.meta.rows, this.meta.duration]
+          const queryDurationRecors = [meta.rows, meta.duration]
 
-          this.keysData = Object.keys(this.data[0])
+          keysData = Object.keys(data[0])
 
-          this.$root.$emit('recordsDuration', this.queryDurationRecors)
-          this.$root.$emit('sendData', this.keysData, this.data)
+          this.$root.$emit('recordsDuration', queryDurationRecors)
+          this.$root.$emit('sendData', keysData, data)
           this.$root.$emit('showMessages', true, false)
           this.$root.$emit('sendQueryCode', this.queryCode)
           this.$root.$emit('runSpinner')
@@ -270,9 +270,9 @@ export default {
           this.$root.$emit('apiError', messageError)
 
 
-          this.data = []
-          this.keysData = []
-          this.$root.$emit('sendData', this.keysData, this.data)
+          const data = []
+          const keysData = []
+          this.$root.$emit('sendData', keysData, data)
         })
     }
   }

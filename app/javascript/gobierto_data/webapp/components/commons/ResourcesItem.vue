19a4a63c2d910e72bc4778d3e4072252c15118d2
Jorge Aznar <jorgeatgu@gmail.com>
2020-02-17 16:04:58 +0100

    resources item data

--- app/javascript/gobierto_data/webapp/components/commons/ResourcesItem.vue
+++ app/javascript/gobierto_data/webapp/components/commons/ResourcesItem.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <i class="fas fa-file-excel" />
-    <span class="gobierto-data-resources-list-element-name"></span>
-    <span class="gobierto-data-resources-list-element-size"></span>
+    <span class="gobierto-data-resources-list-element-name">Name</span>
+    <span class="gobierto-data-resources-list-element-size">Item</span>
   </div>
 </template>
 

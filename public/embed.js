0835353fb2146f165da8d6bc574eb451c41f453f
Fernando Blat <fernando@blat.es>
2020-03-08 08:35:13 +0100

    Remove unused function

--- public/embed.js
+++ public/embed.js
@@ -1,30 +1,5 @@
 "use strict";
 
-// From https://www.abeautifulsite.net/parsing-urls-in-javascript
-function parseURL(url) {
-  var parser = document.createElement('a'),
-      searchObject = {},
-      queries, split, i;
-  // Let the browser do the work
-  parser.href = url;
-  // Convert query string to object
-  queries = parser.search.replace(/^\?/, '').split('&');
-  for (i = 0; i < queries.length; i++) {
-    split = queries[i].split('=');
-    searchObject[split[0]] = split[1];
-  }
-  return {
-    protocol: parser.protocol,
-    host: parser.host,
-    hostname: parser.hostname,
-    port: parser.port,
-    pathname: parser.pathname,
-    search: parser.search,
-    searchObject: searchObject,
-    hash: parser.hash
-  }
-}
-
 function postMessageHandler(event) {
   // Uncomment to debug
   // console.log("We've got a message!");

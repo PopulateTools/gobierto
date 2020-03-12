2fcbecda64b3afac88088e5c16fef4f0b5189dd3
Hugoren Martinako <aumpfbahn@gmail.com>
2020-03-12 15:27:05 +0100

    fix i18n/linters

--- app/javascript/gobierto_data/webapp/components/sets/SQLEditorHeader.vue
+++ app/javascript/gobierto_data/webapp/components/sets/SQLEditorHeader.vue
@@ -428,7 +428,7 @@ export default {
       this.queryEditor = encodeURI(this.codeQuery)
 
       let query = ''
-      if (this.queryEditor.includes('LIMIT')) {
+      if (queryEditorLowerCase.includes('limit')) {
         query = this.queryEditor
         this.$root.$emit('hiddeShowButtonColumns')
       } else {
@@ -443,6 +443,7 @@ export default {
       this.endPoint = `${baseUrl}/data`
       this.url = `${this.endPoint}?sql=${query}`
 
+      // TODO: use factory
       axios
         .get(url)
         .then(response => {

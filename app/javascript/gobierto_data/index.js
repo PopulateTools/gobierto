00b956332fcf6097e3cd327a1faf22976fea876a
Hugoren Martinako <aumpfbahn@gmail.com>
2020-03-12 16:19:10 +0100

    load data styles only in its module

--- app/javascript/gobierto_data/index.js
+++ app/javascript/gobierto_data/index.js
@@ -1,7 +1,5 @@
+import "../../assets/stylesheets/module-data.scss"
 import { GobiertoDataController } from "./modules/gobierto_data_controller.js";
-import "../../assets/stylesheets/comp-block-checkbox.scss"
-import "../../assets/stylesheets/comp-block-header.scss"
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const appNode = document.getElementById("gobierto-datos-app");

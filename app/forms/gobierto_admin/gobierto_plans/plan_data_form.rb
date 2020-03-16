ebb3d052c5f8bd9ffd437833d7d0aa653f110cdb
Eduardo Martínez <eduardomech@gmail.com>
2020-03-16 23:43:37 +0100

    Don't force previous data clearing importing plan data

--- app/forms/gobierto_admin/gobierto_plans/plan_data_form.rb
+++ app/forms/gobierto_admin/gobierto_plans/plan_data_form.rb
@@ -42,7 +42,6 @@ module GobiertoAdmin
       def save_plan_data
         if csv_file.present?
           ActiveRecord::Base.transaction do
-            clear_previous_data
             import_nodes
           end
         end
@@ -63,15 +62,6 @@ module GobiertoAdmin
         unless !csv_file_content || (REQUIRED_COLUMNS - csv_file_content.headers).blank? && csv_file_content.headers.any? { |header| /Level \d+/.match?(header) }
           errors.add(:base, :invalid_columns)
         end
-      end
-
-      def clear_previous_data
-        @plan.nodes.each(&:destroy)
-        if @plan.categories_vocabulary.blank?
-          @plan.create_categories_vocabulary(name_translations: @plan.title_translations, site: @plan.site)
-          @plan.save
-        end
-        @plan.categories_vocabulary.terms.destroy_all
       end
 
       def import_nodes

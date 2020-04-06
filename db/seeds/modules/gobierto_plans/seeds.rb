5fc9542728d3b97481440a807497ebc23a6d10f7
Eduardo Martínez <eduardomech@gmail.com>
2020-04-06 10:52:30 +0200

    Change plans module seed to only import SDGs vocabulary but not create custom field

--- db/seeds/modules/gobierto_plans/seeds.rb
+++ db/seeds/modules/gobierto_plans/seeds.rb
@@ -4,9 +4,6 @@ module GobiertoSeeds
   class Recipe
 
     def self.run(site)
-      # Create sdg vocabulary
-      return if site.custom_fields.vocabulary_options.where(uid: "sdgs").exists?
-
       # Create vocabulary
       vocabulary = site.vocabularies.find_or_initialize_by(slug: "sdgs-vocabulary")
       if vocabulary.new_record?
@@ -14,29 +11,14 @@ module GobiertoSeeds
         vocabulary.save
       end
 
+      return if vocabulary.terms.present?
+
       # Load terms
       import_form = GobiertoAdmin::GobiertoCommon::VocabularyTermsImportForm.new(
         vocabulary: vocabulary,
         csv_file: Rails.root.join("db/seeds/modules/gobierto_plans/sdgs_seeds.csv")
       )
       import_form.save
-
-      # Create custom field
-      class_name = "GobiertoPlans::Node"
-      custom_field = site.custom_fields.vocabulary_options.create(
-        uid: "sdgs",
-        name_translations: {
-          ca: "Objectius de Desenvolupament Sostenible",
-          en: "Sustainable Development Goals",
-          es: "Objetivos de Desarrollo Sostenible"
-        },
-        class_name: class_name,
-        options: {
-          configuration: { vocabulary_type: "multiple_select" },
-          vocabulary_id: vocabulary.id
-        }
-      )
-      custom_field.update_attribute(:position, (site.custom_fields.where(class_name: class_name).maximum(:position) || 0) + 1)
     end
   end
 end

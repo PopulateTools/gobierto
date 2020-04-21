2727c2ed6a5d5969821b0a8bbe2979904edfd1cd
Eduardo Martínez <eduardomech@gmail.com>
2020-04-21 20:08:30 +0200

    Replace concern scope

--- app/models/concerns/gobierto_common/has_api_tokens.rb
+++ app/models/concerns/gobierto_common/has_api_tokens.rb
@@ -10,11 +10,11 @@ module GobiertoCommon
     end
 
     def primary_api_token!
-      primary_api_token || api_tokens.primary.create
+      primary_api_token || api_tokens.create(primary: true)
     end
 
     def primary_api_token
-      api_tokens.primary.take
+      api_tokens.where(primary: true).take
     end
 
   end

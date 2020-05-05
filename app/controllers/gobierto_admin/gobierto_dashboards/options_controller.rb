1b74c64f484aabc98dc4b3c119ebcf4a3bc7fd05
Victor <victor@populate.tools>
2020-05-05 15:45:15 +0200

    Base Gobierto Dashboards files for the admin side
    - Controllers with empty actions
    - Empty views
    - Empty locale files

---
+++ app/controllers/gobierto_admin/gobierto_dashboards/options_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module GobiertoAdmin
+  module GobiertoDashboards
+    class OptionsController < GobiertoAdmin::GobiertoDashboards::BaseController
+
+      def index
+
+      end
+
+    end
+  end
+end

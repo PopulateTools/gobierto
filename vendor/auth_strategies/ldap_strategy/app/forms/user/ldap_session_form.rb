a80b484967794f6f2270db37d391a26128f3826e
Eduardo Martínez <eduardomech@gmail.com>
2020-05-07 17:45:50 +0200

    Allow users to authenticate with the default Gobierto email/passord strategy

--- vendor/auth_strategies/ldap_strategy/app/forms/user/ldap_session_form.rb
+++ vendor/auth_strategies/ldap_strategy/app/forms/user/ldap_session_form.rb
@@ -8,22 +8,33 @@ class User::LdapSessionForm < User::CustomSessionForm
     :user_ldap_data
   )
 
-  validates_with LdapValidator
+  validates_with LdapValidator, unless: :session_default_strategy
+
+  def initialize(attributes = {})
+    @email = attributes.dig(:data, :user_session, :email)
+    @password = attributes.dig(:data, :user_session, :password)
+
+    super(attributes)
+  end
 
   def save
-    return false if authentication_data_invalid?
+    return session_default_strategy if authentication_data_invalid?
 
-    find_or_create_user
+    find_or_create_user_by_ldap
   end
 
   def authentication_data_invalid?
     !valid? && user_ldap_data.blank?
   end
 
-  def find_or_create_user
+  def find_or_create_user_by_ldap
     @user = site.users.find_by(user_ldap_data.slice(:email)) || site.users.new(user_ldap_data)
 
     user.confirm! if user.new_record? && user.save || !user.confirmed?
     user
+  end
+
+  def session_default_strategy
+    @user ||= User::SessionForm.new(email: email, password: password, site: site).save
   end
 end

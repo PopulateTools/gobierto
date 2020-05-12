8ff4c5f98a3819bc77707bc819f19fa846cc2e1f
Víctor <victor@populate.tools>
2020-05-12 15:48:10 +0200

    Moving columns configuration to its own file
    And removing them from data()

--- app/javascript/gobierto_dashboards/webapp/containers/tender/TendersIndex.vue
+++ app/javascript/gobierto_dashboards/webapp/containers/tender/TendersIndex.vue
@@ -10,6 +10,7 @@
 
 <script>
 import Table from "../../components/Table.vue";
+import { tendersColumns } from "../../lib/config.js";
 
 export default {
   name: 'TendersIndex',
@@ -19,16 +20,12 @@ export default {
   data() {
     return {
       tendersData: this.$root.$data.tendersData,
-      items: [],
-      columns: [
-        {field: 'contractor', translation: I18n.t('gobierto_dashboards.dashboards.contracts.contractor'), format: null},
-        {field: 'status', translation: I18n.t('gobierto_dashboards.dashboards.contracts.status'), format: null},
-        {field: 'submission_date', translation: I18n.t('gobierto_dashboards.dashboards.contracts.submission_date'), format: null},
-      ]
+      items: []
     }
   },
   created() {
     this.items = this.tendersData.slice(0, 50);
+    this.columns = tendersColumns;
   }
 }
 </script>

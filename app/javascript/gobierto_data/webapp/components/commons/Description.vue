4e2b01152fcdd85554fa80becbacde3d69b6ae03
Jorge Aznar <jorgeatgu@gmail.com>
2020-05-13 18:10:38 +0200

    split columns

--- app/javascript/gobierto_data/webapp/components/commons/Description.vue
+++ app/javascript/gobierto_data/webapp/components/commons/Description.vue
@@ -9,12 +9,19 @@
       </template>
       <div class="gobierto-data-description">
         <template
-          v-for="row in numberOfRows"
+          v-for="(row, i, key) in numberOfRows"
         >
           <div
-            v-for="col in itemsPerRow"
+            v-for="(element) in arrayOfObjectsColumns.slice(i * itemsPerRow, (i + 1) * itemsPerRow)"
+            :key="key"
           >
-            {{ arrayColumns }}
+            <i
+              :class="'fas gobierto-data-columns-icon-' + Object.values(element)[0]"
+              style="color: var(--color-base)"
+            />
+            <span>
+              {{ Object.keys(element)[0] }}
+            </span>
           </div>
         </template>
       </div>
@@ -38,19 +45,49 @@ export default {
   },
   data() {
     return {
-      itemsPerRow: 6,
+      itemsPerRow: 5,
       labelDescription: I18n.t("gobierto_data.projects.description") || '',
-      showDescription: true
+      showDescription: true,
+      arrayOfObjectsColumns: []
     }
   },
   computed: {
     numberOfRows() {
-      return Math.ceil(Object.keys(this.arrayColumns).length / this.itemsPerRow);
+      return Math.ceil(Object.keys(this.arrayOfObjectsColumns).length / this.itemsPerRow);
+    },
+    chooseIcon(value) {
+      console.log("chooseIcon -> value", value)
+      if (this.arrayOfObjectsColumns === 'string') {
+        return 'fas fa-calendar'
+      }
+      return "fas fa-caret-up"
     }
+
   },
   created() {
-    const peopleArray = Object.keys(this.arrayColumns).map(i => this.arrayColumns[i])
-    console.log("created -> peopleArray", peopleArray)
+    this.arrayOfObjectsColumns = Object.entries(this.arrayColumns).map(([k, v]) => ({ [k]: v }));
   },
+  methods: {
+    // chooseIcon(icon) {
+    //   const valueIcon = Object.values(icon)
+    //   const iconText = ['jsonb', 'string','text']
+    //   const iconDate = ['time', 'date', 'datetime', 'timestamp', 'timestampz']
+    //   const iconPound = ['boolean', 'decimal', 'hstore', 'integer']
+    //   if (iconText.every(element => valueIcon[0].includes(element))) {
+    //     console.log('text')
+    //   }
+
+    //   if (iconDate.every(element => valueIcon[0].includes(element))) {
+    //     console.log('calendar')
+    //     return 'fas fa-calendar'
+    //   }
+
+    //   if (iconPound.every(element => valueIcon[0].includes(element))) {
+    //     return 'fas fa-caret-up'
+    //   }
+
+
+    // }
+  }
 }
 </script>
--- app/javascript/gobierto_data/webapp/components/sets/data/SQLEditorCode.vue
+++ app/javascript/gobierto_data/webapp/components/sets/data/SQLEditorCode.vue
@@ -129,6 +129,7 @@ export default {
       }
 
       this.$root.$emit('enableSavedButton')
+      this.$root.$emit('enabledForkPrompt')
     },
     onChange(editor) {
       this.mergeTables();

71602705ec79c4de4e1d39155dbc921701a89b12
Jorge Aznar <jorgeatgu@gmail.com>
2020-05-18 11:41:27 +0200

    remove subMenu

--- app/javascript/gobierto_investments/webapp/containers/home/Home.vue
+++ app/javascript/gobierto_investments/webapp/containers/home/Home.vue
@@ -112,7 +112,6 @@
 <script>
 import Aside from "./Aside.vue";
 import Main from "./Main.vue";
-import Nav from "./Nav.vue";
 import Article from "./Article.vue";
 import axios from "axios";
 
@@ -129,7 +128,6 @@ export default {
   components: {
     Aside,
     Main,
-    Nav,
     Article,
     Loading,
     Calendar,

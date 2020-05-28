503df52a9cae3d7679ea80a06cf2e84c118400e0
Jorge <jorgeatgu@gmail.com>
2020-05-28 18:37:36 +0200

    remove components

--- app/javascript/gobierto_dashboards/modules/costs_controller.js
+++ app/javascript/gobierto_dashboards/modules/costs_controller.js
@@ -1,13 +1,33 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import { getRemoteData } from '../webapp/lib/utils'
 import { EventBus } from '../webapp/mixins/event_bus'
 
-import { VisBubble } from "lib/visualizations";
+/*import { VisBubble } from "lib/visualizations";*/
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
+
+
+//TO-DO: create a factory to get data
+let initObject = { method: 'GET' };
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 400) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
+}
+
+const userRequest = new Request('https://mataro.gobify.net/api/v1/data/data?sql=select%20*%20from%20costes', initObject);
+
+async function getData() {
+  let response = await fetch(userRequest);
+  let dataRequest = await checkStatus(response);
+  let data = dataRequest.json()
+  return data;
+}
 
 export class CostsController {
   constructor(options) {
@@ -23,13 +43,10 @@ export class CostsController {
 
       entryPoint.innerHTML = htmlRouterBlock;
 
-      const Home = () => import("../webapp/containers/shared/Home.vue");
+      const Home = () => import("../webapp/containers/costs/Home.vue");
 
-      //FIX-ME: get
-      // JSON https://mataro.gobify.net/api/v1/data/data?sql=select%20*%20from%20costes
-      // CSV https://mataro.gobify.net/api/v1/data/data.csv?sql=select%20*%20from%20costes
-      Promise.all([getRemoteData(options.contractsEndpoint), getRemoteData(options.tendersEndpoint)]).then((rawData) => {
-        this.setGlobalVariables(rawData)
+      Promise.all([getData()]).then((rawData) => {
+        const data = rawData
 
         const router = new VueRouter({
           mode: "history",
@@ -50,7 +67,7 @@ export class CostsController {
             Vue.nextTick(() => {
               let title = baseTitle;
 
-              if (to.name === "contracts_show" || to.name === "tenders_show") {
+              if (to.name === "costs") {
                 const { item: { title: itemTitle } = {} } = to.params;
 
                 if (itemTitle) {
@@ -65,94 +82,15 @@ export class CostsController {
 
         this.vueApp = new Vue({
           router,
+          //TO-DO: factory
           data: Object.assign(options, this.data),
         }).$mount(entryPoint);
-
-        EventBus.$on('summary_ready', () => {
-          this._renderSummary();
-        });
-
-        EventBus.$on('filter_changed', (options) => {
-          this._updateChartsFromFilter(options);
-        });
-
-        // - dc charts are drawn even if the summary page is not the page where the user lands for the first time
-        //   i.e.: they could first land on a contract page, but still this page would need to render for the filters to work
-        // - dc charts sizes are calculated automatically, but if the page is not visible it won't calculate sizes properly
-        // - Given all that: when we go from a page that is not summary to summary for the first time, the sizes must
-        //   be calculated and the charts redrawn. This is why this event only needs to be listened once.
-        EventBus.$once('moved_to_summary', () => {
-          this._redrawCharts();
-        });
 
         const loadingElement = document.querySelector(".js-loading");
         if (loadingElement) {
           loadingElement.classList.add('hidden')
         }
       });
-    }
-  }
-
-  setGlobalVariables(rawData){
-    let contractsData, tendersData;
-    [contractsData, tendersData] = rawData;
-
-    const sortByField = (dateField) => {
-      return function(a, b){
-        const aDate = a[dateField],
-              bDate = b[dateField];
-
-        if (aDate == '') {
-          return 1;
-        }
-
-        if (bDate == '') {
-          return -1;
-        }
-
-        if ( aDate < bDate ){
-          return 1;
-        } else if ( aDate > bDate ){
-          return -1;
-        } else {
-          return 0;
-        }
-      }
-    }
-
-    // Contracts precalculations and normalizations
-    this._amountRange = {
-      domain: [1001, 10001, 50001, 100001],
-      range: [0, 1, 2, 3, 4]
-    };
-    var rangeFormat = d3.scaleThreshold().domain(this._amountRange.domain).range(this._amountRange.range);
-
-    for(let i = 0; i < contractsData.length; i++){
-      const contract = contractsData[i];
-      const final_amount_no_taxes = contract.final_amount_no_taxes ? parseFloat(contract.final_amount_no_taxes) : 0.0;
-      const initial_amount_no_taxes = contract.initial_amount_no_taxes ? parseFloat(contract.initial_amount_no_taxes) : 0.0 ;
-
-      contract.final_amount_no_taxes = final_amount_no_taxes;
-      contract.initial_amount_no_taxes = initial_amount_no_taxes;
-      contract.range = rangeFormat(+final_amount_no_taxes);
-      contract.start_date_year = contract.start_date ? (new Date(contract.start_date).getFullYear()) : contract.start_date;
-    }
-
-    for(let i = 0; i < tendersData.length; i++){
-      const tender = tendersData[i];
-      const initial_amount_no_taxes = tender.initial_amount_no_taxes ? parseFloat(tender.initial_amount_no_taxes) : 0.0;
-
-      tender.initial_amount_no_taxes = initial_amount_no_taxes;
-      tender.submission_date_year = tender.submission_date ? (new Date(tender.submission_date).getFullYear()) : tender.submission_date;
-
-      if(tender.submission_date_year) { tender.submission_date_year = tender.submission_date_year.toString() }
-    }
-
-    this.unfilteredTendersData = tendersData.sort(sortByField('submission_date'));
-
-    this.data = {
-      contractsData: this._formalizedContractsData(contractsData).sort(sortByField('start_date')),
-      tendersData: this.unfilteredTendersData,
     }
   }
 

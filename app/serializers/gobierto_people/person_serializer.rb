fae054bc37e9a11477a9c23c044521293d90ee07
Eduardo Martínez <eduardomech@gmail.com>
2020-06-15 21:51:35 +0200

    Remove unused serialization attribute

--- app/serializers/gobierto_people/person_serializer.rb
+++ app/serializers/gobierto_people/person_serializer.rb
@@ -37,10 +37,6 @@ module GobiertoPeople
       filtered_positions.first&.name
     end
 
-    def all_positions_html
-      object.historical_charges.reverse_sorted.join("<br>")
-    end
-
     def filtered_positions
       charges[object.id] || []
     end

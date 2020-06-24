9f6dbe50ac486cf4a20de7c03e532b1113a17cdf
Jorge <jorgeatgu@gmail.com>
2020-06-24 16:19:57 +0200

    resolved changes @crashillo

--- app/javascript/gobierto_data/lib/factories/datasets.js
+++ app/javascript/gobierto_data/lib/factories/datasets.js
@@ -12,18 +12,8 @@ const headers = {
 // Dataset-endpoint factory to get/post/put/delete API data
 export const DatasetFactoryMixin = {
   methods: {
-    async getDatasetMetadata(id) {
+    getDatasetMetadata(id) {
       return axios.get(`${endPoint}/${id}/meta`, { headers })
-        .then(response => {
-          return response
-        })
-        // eslint-disable-next-line no-unused-vars
-        .catch(error => {
-          if (error.response.status === 404) {
-            // eslint-disable-next-line no-unused-vars
-            this.$router.push('/datos/').catch(error => {})
-          }
-        })
-    },
+    }
   }
 };

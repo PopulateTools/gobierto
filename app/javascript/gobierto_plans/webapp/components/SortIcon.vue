4958494758384bb050df60eb18b9cdc79d33f348
Hugoren Martinako <aumpfbahn@gmail.com>
2020-07-03 13:06:09 +0200

    sort columns

---
+++ app/javascript/gobierto_plans/webapp/components/SortIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <i
+    v-if="direction"
+    :class="`fas fa-sort-amount-${direction}`"
+  />
+</template>
+
+<script>
+export default {
+  name: "SortIcon",
+  props: {
+    direction: {
+      type: String,
+      default: null
+    }
+  }
+};
+</script>
--- app/javascript/gobierto_plans/webapp/pages/TableTerms.vue
+++ app/javascript/gobierto_plans/webapp/pages/TableTerms.vue
@@ -1,31 +1,57 @@
 <template>
   <table>
-    <th>{{ uid }}</th>
-    <th>{{ labelProgress }}</th>
-    <th><NumberLabel :level="lastLevel" /></th>
-    <tr
-      v-for="{ key, name, slug, progress, length } in groups"
-      :key="key"
-    >
-      <td>
-        <router-link :to="{ name: 'term', params: { ...params, term: slug } }">
-          {{ name }}
-        </router-link>
-      </td>
-      <td>{{ progress | percent }}</td>
-      <td>{{ length }}</td>
-    </tr>
+    <thead>
+      <th @click="handleTableHeaderClick(1)">
+        {{ uid }}
+        <SortIcon
+          v-if="currentSortColumn === 1"
+          :direction="getDirection(1)"
+        />
+      </th>
+      <th @click="handleTableHeaderClick(2)">
+        {{ labelProgress }}
+        <SortIcon
+          v-if="currentSortColumn === 2"
+          :direction="getDirection(2)"
+        />
+      </th>
+      <th @click="handleTableHeaderClick(3)">
+        <NumberLabel :level="lastLevel" />
+        <SortIcon
+          v-if="currentSortColumn === 3"
+          :direction="getDirection(3)"
+        />
+      </th>
+    </thead>
+    <tbody>
+      <tr
+        v-for="{ key, name, slug, progress, length } in termsSorted"
+        :key="key"
+      >
+        <td>
+          <router-link
+            :to="{ name: 'term', params: { ...params, term: slug } }"
+          >
+            {{ name }}
+          </router-link>
+        </td>
+        <td>{{ progress | percent }}</td>
+        <td>{{ length }}</td>
+      </tr>
+    </tbody>
   </table>
 </template>
 
 <script>
 import NumberLabel from "../components/NumberLabel";
+import SortIcon from "../components/SortIcon";
 import { percent } from "lib/shared";
 
 export default {
   name: "TableTerms",
   components: {
-    NumberLabel
+    NumberLabel,
+    SortIcon
   },
   filters: {
     percent
@@ -33,7 +59,7 @@ export default {
   props: {
     uid: {
       type: String,
-      default: ''
+      default: ""
     },
     groups: {
       type: Array,
@@ -46,8 +72,11 @@ export default {
   },
   data() {
     return {
+      labelProgress: I18n.t("gobierto_plans.plan_types.show.progress") || "",
+      termsSorted: [],
+      map: new Map(),
       lastLevel: 0,
-      labelProgress: I18n.t("gobierto_plans.plan_types.show.progress") || ""
+      currentSortColumn: null
     };
   },
   computed: {
@@ -58,6 +87,30 @@ export default {
   created() {
     const { last_level } = this.options;
     this.lastLevel = last_level;
+    this.termsSorted = this.groups;
+
+    this.map.set(1, ["name", "up"]);
+    this.map.set(2, ["progress", "up"]);
+    this.map.set(3, ["length", "up"]);
+  },
+  methods: {
+    handleTableHeaderClick(column) {
+      const [id, order] = this.map.get(column);
+      this.currentSortColumn = column
+
+      // toggle sort order
+      const currentSort = order === "up" ? "down" : "up";
+      // update the order for the item clicked
+      this.map.set(column, [id, currentSort]);
+      // change the order based on the currentSort
+      this.termsSorted.sort(({ [id]: termA }, { [id]: termB }) =>
+        currentSort === "up" ? termA > termB : termA < termB
+      );
+    },
+    getDirection(column) {
+      const [_, order] = this.map.get(column);
+      return order;
+    }
   }
 };
 </script>

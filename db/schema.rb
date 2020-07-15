47268b977dd710fc935256ea532848c0c2045d99
Eduardo Martínez <eduardomech@gmail.com>
2020-07-15 13:59:33 +0200

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_093807) do
+ActiveRecord::Schema.define(version: 2020_07_15_172832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "hstore"
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "activities", id: :serial, force: :cascade do |t|
     t.string "action", null: false
@@ -999,6 +1001,21 @@ ActiveRecord::Schema.define(version: 2020_05_15_093807) do
     t.index ["statuses_vocabulary_id"], name: "index_gplan_plans_on_statuses_vocabulary_id"
     t.index ["title_translations"], name: "index_gplan_plans_on_title_translations", using: :gin
     t.index ["vocabulary_id"], name: "index_gplan_plans_on_vocabulary_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.bigint "site_id"
+    t.string "resource_path"
+    t.jsonb "title_translations"
+    t.jsonb "description_translations"
+    t.jsonb "meta"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+    t.index ["site_id"], name: "index_pg_search_documents_on_site_id"
   end
 
   create_table "sites", id: :serial, force: :cascade do |t|

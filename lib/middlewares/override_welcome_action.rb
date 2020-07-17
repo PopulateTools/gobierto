16f6e1122790113949b6a4fef9199a45099ecdbe
Eduardo Martínez <eduardomech@gmail.com>
2020-07-17 14:59:04 +0200

    Set site default locale when module is GobiertoPeople in override welcome action for / path

--- lib/middlewares/override_welcome_action.rb
+++ lib/middlewares/override_welcome_action.rb
@@ -20,7 +20,11 @@ class OverrideWelcomeAction
     # If the path is the homepage, the route should be the site root path
     if env["PATH_INFO"] == "/" && env["gobierto_site"].present?
       # A CMS page is handled by meta welcome controller
-      if env["gobierto_site"].configuration.home_page != "GobiertoCms"
+      home_page_module = env["gobierto_site"].configuration.home_page
+      if home_page_module != "GobiertoCms"
+        # GobiertoPeople has translations in paths, and the value in the cookie
+        # is overrided
+        I18n.locale = env["gobierto_site"].configuration.default_locale if home_page_module == "GobiertoPeople"
         env["PATH_INFO"] = env["gobierto_site"].root_path
         env["gobierto_welcome_override"] = true
       end

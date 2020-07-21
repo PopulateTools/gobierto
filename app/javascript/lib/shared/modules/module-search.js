a263aae10a15d50a084bf255ebf36dd494aacb04
Eduardo Martínez <eduardomech@gmail.com>
2020-07-21 17:54:21 +0200

    Replace algolia search with autocomplete calling search API

--- app/javascript/lib/shared/modules/module-search.js
+++ app/javascript/lib/shared/modules/module-search.js
@@ -1,6 +1,8 @@
+import { AUTOCOMPLETE_DEFAULTS } from 'lib/shared'
+import 'devbridge-autocomplete'
+
 $(document).on('turbolinks:load', function() {
   var $input = $('input#gobierto_search');
-  var $resultsContainer = $('#search_results, #search_results:hidden');
 
   function truncateOnWord(str, limit) {
     var trimmable = '\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u2028\u2029\u3000\uFEFF';
@@ -22,17 +24,18 @@ $(document).on('turbolinks:load', function() {
 
   function itemDescription(d){
     var maxLenght = 200;
-    var description = (d['bio'] || d['description'] || d['body'] || d['bio_' + I18n.locale] || d['description_' + I18n.locale] || d['body_' + I18n.locale] || '');
+    var description = (d['description'] || '');
     if(description === '' || description.length < maxLenght)
       return description;
     return truncateOnWord(description, maxLenght) + '...';
   }
 
   function itemType(d){
-    if(d['class_name'] === undefined)
+    let class_name = d['class_name'] || d['searchable_type']
+    if(class_name === undefined)
       return;
 
-    switch(d['class_name']){
+    switch(class_name){
       case 'GobiertoPeople::Person':
         return I18n.t("layouts.search.person_item");
       case 'GobiertoPeople::PersonPost':
@@ -60,73 +63,40 @@ $(document).on('turbolinks:load', function() {
     }
   }
 
-  function searchFrontCallback(err, content) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    $resultsContainer.html('');
-    var sum = 0;
-    content.results.forEach(function(indexResults){
-      sum += indexResults.nbHits;
-    });
-
-    if(sum > 0) {
-      content.results.forEach(function(indexResults){
-        indexResults.hits.forEach(function(d){
-
-          var result = '<a class="result" <a href="' + d.resource_path + '">' +
-            '<h2>' + (d['title'] || d['name'] || d['title_' + I18n.locale] || d['name_' + I18n.locale]) + '</h2>' +
-            '<div class="description">' +
-              '<div>' + itemDescription(d) + '</div>' +
-              '<span class="soft item_type">' + itemType(d) + '</span>' +
-              (itemUpdatedAt(d) ? ' · <span class="soft updated_at">' + itemUpdatedAt(d) + '</span>' : '') +
-            '</div>' +
-          '</a>';
-
-          var div = $(result);
-          div.appendTo($resultsContainer);
-        });
-      });
-    } else {
-      $('<div class="result"><p>'+I18n.t("layouts.search.no_results")+'</p></div>').appendTo($resultsContainer);
-    }
-
-    if(window.searchClient.indexes.length > 1) {
-      $('<div class="result"><small>'+I18n.t("layouts.search.powered_by")+'</small></div>').appendTo($resultsContainer);
-    }
+  function formattedResult(d){
+    return '<h2>' + d['title'] + '</h2>' +
+           '<div class="description">' +
+             '<div>' + itemDescription(d) + '</div>' +
+             '<span class="soft item_type">' + itemType(d) + '</span>' +
+             (itemUpdatedAt(d) ? ' · <span class="soft updated_at">' + itemUpdatedAt(d) + '</span>' : '') +
+           '</div>'
   }
 
-  $input.on('keyup', function(){
-    var q = $(this).val();
-    var queries = [];
-    window.searchClient.indexes.forEach(function(index){
-      queries.push({
-        indexName: index,
-        query: q,
-        params: {
-          hitsPerPage: 10,
-          filters: window.searchClient.filters
-        }
-      });
-    });
+  var searchOptions = {
+    serviceUrl: '/api/v1/search',
+    dataType: 'json',
+    params: {
+      request:
+      {
+        searchable_type: window.searchClient.searchable_types
+      }
+    },
+    onSelect: function(suggestion){
+      Turbolinks.visit(suggestion.data.resource_path);
+    },
+    transformResult: function(response) {
+      return {
+        suggestions: response.data.map(item => ({
+          value: item.attributes.title,
+          data: item.attributes
+        }))
+      }
+    },
+    formatResult: function(suggestion, current_value){
+      return formattedResult(suggestion.data)
+    },
+    noSuggestionNotice: `<div class="result"><p>${ I18n.t("layouts.search.no_results") }</p></div>`
+  };
 
-    if(q.length > 2){
-      window.searchClient.client.search(queries, searchFrontCallback);
-    } else {
-      $resultsContainer.html('');
-    }
-  });
-
-  $input.focusin(function() {
-    $resultsContainer.show();
-  });
-
-  // Hide resultsContainer if clicked outside the search input and results
-  $('body').click(function(e) {
-    if ( !$(e.target).is('#search_results *') && !$(e.target).is('input#gobierto_search') ) {
-      $resultsContainer.hide();
-    }
-  });
+  $input.devbridgeAutocomplete($.extend({}, AUTOCOMPLETE_DEFAULTS, searchOptions));
 });

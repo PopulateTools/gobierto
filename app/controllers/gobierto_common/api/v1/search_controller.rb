c830b9bd637b435ed227bad4a17930f4400d0cba
Eduardo Martínez <eduardomech@gmail.com>
2020-08-04 12:11:14 +0200

    Remove unnecessary queries in search API controller

--- app/controllers/gobierto_common/api/v1/search_controller.rb
+++ app/controllers/gobierto_common/api/v1/search_controller.rb
@@ -5,19 +5,14 @@ module GobiertoCommon
     module V1
       class SearchController < ApiBaseController
 
-        # GET /api/v1/search?query=term&filters[searchable_type][]=GobiertoPeople::Person&page[number]=1&page[size]=5
+        # GET /api/v1/search?query=term&filters[searchable_type][]=GobiertoPeople::Person
         def query
-          search_result = current_site.multisearch(params[:query]).where(filters_params).page(page_params[:number]).per(page_params[:size])
+          search_result = current_site.multisearch(params[:query]).where(filters_params).limit(25)
 
           render(
             json: search_result,
             each_serializer: GobiertoCommon::SearchDocumentSerializer,
             adapter: :json_api,
-            meta: {
-              hits_count: search_result.total_count,
-              page: search_result.current_page,
-              per_page: search_result.limit_value
-            }
           )
         end
 
@@ -25,10 +20,6 @@ module GobiertoCommon
 
         def filters_params
           @filters_params ||= params.fetch(:filters, {}).permit(searchable_type: [])
-        end
-
-        def page_params
-          @page_params ||= params.fetch(:page, {}).permit(:number, :size)
         end
       end
     end

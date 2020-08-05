951f5601e4dcac10a365126f2f1887665c365b96
Eduardo Martínez <eduardomech@gmail.com>
2020-08-05 18:30:06 +0200

    Adapt investments home filters to new API response

--- app/javascript/gobierto_investments/webapp/containers/home/Home.vue
+++ app/javascript/gobierto_investments/webapp/containers/home/Home.vue
@@ -314,7 +314,7 @@ export default {
       this.filters.splice(index, 1, filter); // To detect array mutations
 
       const checkboxFilterFn = attrs =>
-        attrs[key].find(d => checkboxesSelected.get(+d.id));
+        attrs[key] && this.convertToArrayOfIds(attrs[key]).find(d => checkboxesSelected.get(+d));
 
       const callback = size ? checkboxFilterFn : undefined;
       this.filterItems(callback, key);
@@ -374,8 +374,9 @@ export default {
         // Get the items based on these new active filters
         const __items__ = this.applyFiltersCallbacks(__activeFilters__);
 
-        return __items__.filter(({ attributes }) =>
-          attributes[key].map(g => g.id).includes(id)
+        return __items__.filter(({ attributes }) =>(
+          this.convertToArrayOfIds(attributes[key]).includes(id)
+          )
         ).length;
       };
       const { key, options = [] } = filter;

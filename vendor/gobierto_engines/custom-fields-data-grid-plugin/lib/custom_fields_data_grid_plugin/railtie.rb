28f87fdcd4077816e52e473de5e36b51e90e555e
Fernando Blat <fernando@blat.es>
2020-08-26 08:54:11 +0200

    Replace webpacker additional paths method due to deprecation warning

--- vendor/gobierto_engines/custom-fields-data-grid-plugin/lib/custom_fields_data_grid_plugin/railtie.rb
+++ vendor/gobierto_engines/custom-fields-data-grid-plugin/lib/custom_fields_data_grid_plugin/railtie.rb
@@ -30,7 +30,7 @@ else
         conf.i18n.load_path += Dir[File.join(base_path, 'config', 'locales', '**', '*.{rb,yml}')]
       end
 
-      Webpacker::Compiler.watched_paths += [
+      Webpacker::Compiler.additional_paths += [
         "app/javascript/packs/*.js",
         "app/javascript/custom_fields_data_grid_plugin/*.js",
         "app/javascript/custom_fields_data_grid_plugin/**/*.js"

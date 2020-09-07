45734c559dcc2d703f4757e1b6bb23c4d78a5621
Eduardo Martínez <eduardomech@gmail.com>
2020-09-07 16:27:49 +0200

    Load constants introduced by engines in initialized instead of using a require in railtie

--- config/initializers/gobierto_modules.rb
+++ config/initializers/gobierto_modules.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.plugins_attached_modules.each do |class_name, modules_classes|
-  modules_classes.each { |module_class| Object.const_get(class_name).prepend(module_class) }
+  modules_classes.each { |module_class| Object.const_get(class_name).prepend(module_class.constantize) }
 end
--- vendor/gobierto_engines/custom-fields-data-grid-plugin/lib/custom_fields_data_grid_plugin/railtie.rb
+++ vendor/gobierto_engines/custom-fields-data-grid-plugin/lib/custom_fields_data_grid_plugin/railtie.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "../../app/decorators/gobierto_plans/category_term_decorator_budgets_attachment"
-
 def attach_module(config, origin, module_class)
   if config.plugins_attached_modules[origin]
     config.plugins_attached_modules[origin].append(module_class)
@@ -26,7 +24,7 @@ else
         conf.autoload_paths += load_paths
         conf.eager_load_paths += load_paths
 
-        attach_module(conf, "::GobiertoPlans::CategoryTermDecorator", ::GobiertoPlans::CategoryTermDecoratorBudgetsAttachment)
+        attach_module(conf, "::GobiertoPlans::CategoryTermDecorator", "::GobiertoPlans::CategoryTermDecoratorBudgetsAttachment")
 
         conf.i18n.load_path += Dir[File.join(base_path, 'config', 'locales', '**', '*.{rb,yml}')]
       end

b1c73b295ce3d298ba520f4d7aea44d8c0796271
Eduardo Martínez <eduardomech@gmail.com>
2020-09-08 18:24:29 +0200

    Set defaults to Rails 6.0 in application config and remove new_framework_defaults initializer

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ Bundler.require(*Rails.groups)
 module Gobierto
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults "5.2"
+    config.load_defaults "6.0"
 
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
 

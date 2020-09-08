e9f7d127fe8a62addc51c74196a657a69f31485d
Eduardo Martínez <eduardomech@gmail.com>
2020-09-08 13:40:56 +0200

    Avoid deprecation warning for constants autoloaded in initialization
    Based on https://github.com/rails/rails/issues/36363 and
    Rails guides https://edgeguides.rubyonrails.org/autoloading_and_reloading_constants.html

--- config/initializers/gobierto_modules.rb
+++ config/initializers/gobierto_modules.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-Rails.application.config.plugins_attached_modules.each do |class_name, modules_classes|
-  modules_classes.each { |module_class| Object.const_get(class_name).prepend(module_class.constantize) }
+Rails.application.config.to_prepare do
+  Rails.application.config.plugins_attached_modules.each do |class_name, modules_classes|
+    modules_classes.each { |module_class| Object.const_get(class_name).prepend(module_class.constantize) }
+  end
 end
--- config/initializers/liquid.rb
+++ config/initializers/liquid.rb
@@ -2,4 +2,6 @@
 
 Liquid::Template.error_mode = :lax
 
-Dir[Rails.root.join("lib/liquid/**/*.rb")].each {|file| require file }
+Rails.application.config.to_prepare do
+  Dir[Rails.root.join("lib/liquid/**/*.rb")].each {|file| require file }
+end

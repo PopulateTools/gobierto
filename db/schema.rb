bc57a4599618bb8d1573be2a5a4495bb98d0c77d
Eduardo Martínez <eduardomech@gmail.com>
2020-09-11 10:25:35 +0200

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_102521) do
+ActiveRecord::Schema.define(version: 2020_09_10_112444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -1016,6 +1016,7 @@ ActiveRecord::Schema.define(version: 2020_08_09_102521) do
     t.datetime "updated_at", null: false
     t.string "external_id"
     t.datetime "searchable_updated_at"
+    t.index ["content"], name: "pg_search_documents_on_content_idx", opclass: :gin_trgm_ops, using: :gin
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
     t.index ["site_id"], name: "index_pg_search_documents_on_site_id"
   end

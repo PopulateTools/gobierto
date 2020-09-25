71849890e17c9e382f58f27680ae5a9e2c7f3e2c
Eduardo Martínez <eduardomech@gmail.com>
2020-09-25 10:17:43 +0200

    Simplify api_authentication_helper removing redundancies

--- app/controllers/concerns/user/api_authentication_helper.rb
+++ app/controllers/concerns/user/api_authentication_helper.rb
@@ -21,8 +21,10 @@ module User::ApiAuthenticationHelper
     raise_unauthorized unless user_authenticated?
   end
 
+  # The host is not checked if request is internal or the site is not password
+  # protected
   def check_host
-    return if internal_site_request? || !site_protected? || token.present? && valid_token_domain?
+    return if internal_site_request? || !site_protected? || api_token_with_host.present?
 
     raise_unauthorized
   end
@@ -33,25 +35,15 @@ module User::ApiAuthenticationHelper
     authenticate_user_in_site unless internal_site_request?
   end
 
-  def valid_token_domain?
-    return unless api_token.present?
-
-    api_token.domain.blank? || api_token.domain == request.host
-  end
-
   def internal_site_request?
-    Site.where(domain: request.host).exists?
+    @internal_site_request ||= Site.where(domain: request.host).exists?
   end
 
   def find_current_user
-    return unless internal_site_request? || valid_token_domain?
-
     current_site.users.confirmed.joins(:api_tokens).find_by(user_api_tokens: { token: token })
   end
 
   def find_current_admin
-    return unless internal_site_request? || valid_token_domain?
-
     @current_admin ||= ::GobiertoAdmin::Admin.joins(:api_tokens).find_by(admin_api_tokens: { token: token })
   end
 
@@ -66,7 +58,9 @@ module User::ApiAuthenticationHelper
                end
   end
 
-  def api_token
-    @api_token ||= ::User::ApiToken.find_by(token: token) || ::GobiertoAdmin::ApiToken.find_by(token: token)
+  # The ApiToken associated domain must be blank or coincident with the host
+  # request
+  def api_token_with_host
+    @api_token_with_host ||= ::User::ApiToken.where(domain: [nil, request.host]).find_by(token: token) || ::GobiertoAdmin::ApiToken.where(domain: [nil, request.host]).find_by(token: token)
   end
 end

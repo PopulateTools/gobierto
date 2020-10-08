385508574a5e61531a718382f0c66109494b76ff
juan jose lopez <juanjose.lopez@guadaltech.es>
2020-10-08 16:30:55 +0200

    add changes for to run correctly the environment production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  #config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
@@ -44,7 +44,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = ENV["GOBIERTO_FORCE_SSL"].present?
+  #config.force_ssl = ENV["GOBIERTO_FORCE_SSL"].present?
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.

128aeb45e39fb10e20f48f1de8a3b77402fd0f67
juan jose lopez <juanjose.lopez@guadaltech.es>
2020-10-15 17:00:07 +0200

    changes in check copy data in entrypoint

--- docker/entrypoint.d/00-init.sh
+++ docker/entrypoint.d/00-init.sh
@@ -5,7 +5,3 @@ echo "[INFO] Init container"
 echo "127.0.0.1 $HOST" >> /etc/hosts
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-
-cp $PWD/config/database.yml.example $PWD/config/database.yml
-cp $PWD/.env.example $PWD/.env
-ln -s $PWD/.env $PWD/.rbenv-vars
--- docker/entrypoint.d/20-data.sh
+++ docker/entrypoint.d/20-data.sh
@@ -5,6 +5,24 @@ if [ ! -d $PWD/app ]; then
     echo "[INFO] New mount volume, move content to directory with persistence"
     echo -e "[INFO] Moving the content"
     mv /app/* $PWD && mv /app/.[!.]* $PWD
+    if [ ! -f $PWD/config/database.yml ]; then
+        echo "[INFO] Change name of file database.yml.example to database.yml"
+        cd $PWD
+        ls -la config/
+        cp config/database.yml.example config/database.yml
+    fi
+
+    if [ ! -f $PWD/.env ]; then
+        echo "[INFO] Change name of file .env.example to .env"
+        cd $PWD
+        cp .env.example .env
+    fi
+
+    if [ ! -L $PWD/.rbenv-vars ]; then
+        echo "[INFO] Symbolic link of .env with name .rbenv-vars"
+        cd $PWD
+        ln -s .env .rbenv-vars
+    fi
 else
     echo -e "[INFO] Detected Volume with data"
 fi

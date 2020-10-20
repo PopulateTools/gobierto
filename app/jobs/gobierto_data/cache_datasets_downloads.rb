58b104c586f75d82345feb2f64c4059d8e165be3
Fernando Blat <fernando@blat.es>
2020-10-20 08:44:55 +0200

    Update args

--- app/jobs/gobierto_data/cache_datasets_downloads.rb
+++ app/jobs/gobierto_data/cache_datasets_downloads.rb
@@ -4,19 +4,17 @@ module GobiertoData
   class CacheDatasetsDownloads < ActiveJob::Base
     queue_as :cached_data
 
-    def perform(*datasets)
-      datasets.each do |dataset|
-        GobiertoData::Cache.dataset_cache(dataset, format: 'json', update: true) do
-          GobiertoData::Connection.execute_query(dataset.site, dataset.rails_model.all.to_sql).to_json
-        end
+    def perform(dataset)
+      GobiertoData::Cache.dataset_cache(dataset, format: 'json', update: true) do
+        GobiertoData::Connection.execute_query(dataset.site, dataset.rails_model.all.to_sql).to_json
+      end
 
-        GobiertoData::Cache.dataset_cache(dataset, format: 'csv', update: true) do
-          GobiertoData::Connection.execute_query_output_csv(dataset.site, dataset.rails_model.all.to_sql, { col_sep: "," })
-        end
+      GobiertoData::Cache.dataset_cache(dataset, format: 'csv', update: true) do
+        GobiertoData::Connection.execute_query_output_csv(dataset.site, dataset.rails_model.all.to_sql, { col_sep: "," })
+      end
 
-        GobiertoData::Cache.dataset_cache(dataset, format: 'xlsx', update: true) do
-          GobiertoData::Connection.execute_query_output_xlsx(dataset.site, dataset.rails_model.all.to_sql, { name: dataset.name })
-        end
+      GobiertoData::Cache.dataset_cache(dataset, format: 'xlsx', update: true) do
+        GobiertoData::Connection.execute_query_output_xlsx(dataset.site, dataset.rails_model.all.to_sql, { name: dataset.name })
       end
     end
   end

e8f31b89f01c44e339c78d4660cf0da484535200
juan jose lopez <juanjose.lopez@guadaltech.es>
2020-10-21 14:20:00 +0200

    update file entrypoint 20 for check file correctly and copy files correctly and changes in Dockerfile for to add the files of app in a new path

--- docker/entrypoint.d/60-dump.sh
+++ docker/entrypoint.d/60-dump.sh
@@ -21,26 +21,21 @@ if PGPASSWORD=$PG_PASSWORD psql -h $PG_HOST -U $PG_USERNAME -d postgres -c '\q';
 
     # If not exist database and file .create-finish execute setup
     if [ $( echo "$PG_DATABASE_CHECK" | grep -o "$PG_DATABASE" ) ]; then
-        echo -e "[INFO] Database found: $PG_DATABASE, will not be created"
+        echo -e "[INFO] Database found: $PG_DATABASE, will not create database PostgreSQL and ElastickSearch"
     else
         echo -e "[INFO] Create database $PG_DATABASE"
-        if [ ! -f $PWD_APP/db/.create-finish ]; then
-            set -e
-            echo -e "[INFO] Setup database PostgreSQL"
-            cd $PWD_APP
-            $PWD_APP/bin/rails db:setup
-            
-            echo -e "[INFO] Setup database ElastickSearch"
-            $PWD_APP/bin/rails gobierto_budgets:elastic_search_schemas:manage:reset
-            $PWD_APP/bin/rails gobierto_budgets:elastic_search_schemas:manage:create
-            $PWD_APP/bin/rails gobierto_budgets:fixtures:load
-            echo '::BudgetsSeeder.seed!' | $PWD_APP/bin/rails c
-            touch $PWD_APP/db/.create-finish
-            set +e
-        else
-            echo -e "[INFO] File found: $PWD_APP/db/.create-finish, \
-              will not create database PostgreSQL and ElastickSearch"
-        fi
+        set -e
+        echo -e "[INFO] Setup database PostgreSQL"
+        cd $PWD_APP
+        $PWD_APP/bin/rails db:setup
+        
+        echo -e "[INFO] Setup database ElastickSearch"
+        $PWD_APP/bin/rails gobierto_budgets:elastic_search_schemas:manage:reset
+        $PWD_APP/bin/rails gobierto_budgets:elastic_search_schemas:manage:create
+        $PWD_APP/bin/rails gobierto_budgets:fixtures:load
+        echo '::BudgetsSeeder.seed!' | $PWD_APP/bin/rails c
+        touch $PWD_APP/db/.create-finish
+        set +e
     fi
 
 else

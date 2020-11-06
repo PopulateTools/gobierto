b9159a2bec0202afb3e3de025040f5af095c1844
Hugoren Martinako <aumpfbahn@gmail.com>
2020-11-06 10:43:49 +0100

    module participation

--- app/javascript/lib/visualizations/index.js
+++ app/javascript/lib/visualizations/index.js
@@ -1,34 +1,32 @@
-import { VisAgeDistribution } from './modules/age_distribution.js'
-import { VisAgeReport } from './modules/age_report.js'
-import { VisBubbleLegend } from './modules/bubble_legend.js'
-import { VisBubbles } from './modules/bubbles.js'
-import { VisBubble } from './modules/bubble_cost.js'
-import { BarsCard } from './modules/card_bars.js'
-import { ComparisonCard } from './modules/card_comparison.js'
-import { SimpleCard } from './modules/card_simple.js'
-import { SparklineTableCard } from './modules/card_sparkline_table.js'
-import { TableCard } from './modules/card_table.js'
-import { VisIndicators } from './modules/consultation_indicators.js'
-import { VisEvoLine } from './modules/evo_line.js'
-import { VisLineasJ } from './modules/lineas_tabla.js'
-import { VisLinesExecution } from './modules/lines_execution.js'
-import { VisPopulationPyramid } from './modules/population_pyramid.js'
-import { Punchcard } from './modules/punchcard.js'
-import { VisRentDistribution } from './modules/rent_distribution.js'
-import { Rowchart } from './modules/rowchart.js'
-import { VisSlider } from './modules/slider.js'
-import { Sparkline } from './modules/sparkline.js'
-import { VisTreemap } from './modules/treemap.js'
-import { VisUnemploymentAge } from './modules/unemployment_age.js'
-import { VisUnemploymentRate } from './modules/unemployment_rate.js'
-import { VisUnemploymentSex } from './modules/unemployment_sex.js'
-import { Areachart } from './modules/areachart'
-import { AmountDistributionBars } from './modules/amount_distribution_bars'
-import { GroupPctDistributionBars } from './modules/group_pct_distribution_bars'
+import { VisAgeReport } from "./modules/age_report.js";
+import { AmountDistributionBars } from "./modules/amount_distribution_bars";
+import { Areachart } from "./modules/areachart";
+import { VisBubbles } from "./modules/bubbles.js";
+import { VisBubble } from "./modules/bubble_cost.js";
+import { VisBubbleLegend } from "./modules/bubble_legend.js";
+import { BarsCard } from "./modules/card_bars.js";
+import { ComparisonCard } from "./modules/card_comparison.js";
+import { SimpleCard } from "./modules/card_simple.js";
+import { SparklineTableCard } from "./modules/card_sparkline_table.js";
+import { TableCard } from "./modules/card_table.js";
+import { VisIndicators } from "./modules/consultation_indicators.js";
+import { VisEvoLine } from "./modules/evo_line.js";
+import { GroupPctDistributionBars } from "./modules/group_pct_distribution_bars";
+import { VisLineasJ } from "./modules/lineas_tabla.js";
+import { VisLinesExecution } from "./modules/lines_execution.js";
+import { VisPopulationPyramid } from "./modules/population_pyramid.js";
+import { Punchcard } from "./modules/punchcard.js";
+import { VisRentDistribution } from "./modules/rent_distribution.js";
+import { Rowchart } from "./modules/rowchart.js";
+import { VisSlider } from "./modules/slider.js";
+import { Sparkline } from "./modules/sparkline.js";
+import { VisTreemap } from "./modules/treemap.js";
+import { VisUnemploymentAge } from "./modules/unemployment_age.js";
+import { VisUnemploymentRate } from "./modules/unemployment_rate.js";
+import { VisUnemploymentSex } from "./modules/unemployment_sex.js";
 
 export {
   Areachart,
-  VisAgeDistribution,
   VisAgeReport,
   VisBubbleLegend,
   VisBubbles,
@@ -54,4 +52,4 @@ export {
   VisUnemploymentSex,
   AmountDistributionBars,
   GroupPctDistributionBars
-}
+};

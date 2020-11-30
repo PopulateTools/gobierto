420914c02fcce06d091e1bda921a4832a36e121e
Jorge <jorgeatgu@gmail.com>
2020-11-30 17:05:52 +0100

    use the same methodology existent in gobierto for naming css vars | remove terser config

--- config/webpack/optimization/terser.js
+++ config/webpack/optimization/terser.js
@@ -2,45 +2,10 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   optimization: {
-    removeAvailableModules: false,
-     minimizer: [
+    minimizer: [
       new TerserPlugin({
-        terserOptions: {
-          parse: {
-            // we want terser to parse ecma 8 code. However, we don't want it
-            // to apply any minfication steps that turns valid ecma 5 code
-            // into invalid ecma 5 code. This is why the 'compress' and 'output'
-            // sections only apply transformations that are ecma 5 safe
-            // https://github.com/facebook/create-react-app/pull/4234
-            ecma: 8
-          },
-          compress: {
-            ecma       : 5,
-            warnings   : false,
-            // Disabled because of an issue with Uglify breaking seemingly valid code:
-            // https://github.com/facebook/create-react-app/issues/2376
-            // Pending further investigation:
-            // https://github.com/mishoo/UglifyJS2/issues/2011
-            comparisons: false,
-            // Disabled because of an issue with Terser breaking valid code:
-            // https://github.com/facebook/create-react-app/issues/5250
-            // Pending futher investigation:
-            // https://github.com/terser-js/terser/issues/120
-            inline     : 2
-          },
-          output: {
-            ecma      : 5,
-            comments  : false,
-            // Turned on because emoji and regex is not minified properly using default
-            // https://github.com/facebook/create-react-app/issues/2488
-            ascii_only: true
-          }
-        },
-        cache: true,
-        parallel: true,
-        sourceMap: true, // Must be set to true if using source-maps in production
+        parallel: 35
       })
-  ],
-  runtimeChunk: true
-  },
+    ]
+  }
 };

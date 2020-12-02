4914226dfe438c27becc9fdcb5bdbc3bbf326514
Hugoren Martinako <aumpfbahn@gmail.com>
2020-12-02 11:18:42 +0100

    widget table bars relative

--- app/javascript/gobierto_TEMP/webapp/components/WidgetIndicatorTable.vue
+++ app/javascript/gobierto_TEMP/webapp/components/WidgetIndicatorTable.vue
@@ -6,11 +6,27 @@
     <table class="graph-table__table">
       <thead>
         <th
-          v-for="({ year, percentage }, ix) in years"
+          v-for="({ year, percentageRelative, percentageAbsolute }, ix) in years"
           :key="year"
           class="graph-table__table--th"
         >
           <template v-if="ix !== 0">
+            <div class="graph-table__barchart--wrapper">
+              <div class="graph-table__barchart--wrapper-relative">
+                <div
+                  class="graph-table__barchart"
+                  :style="{ height: `${percentageAbsolute}%` }"
+                >
+                  <div
+                    class="graph-table__barchart--active"
+                    :style="{ height: `${percentageRelative}%` }"
+                  />
+                </div>
+              </div>
+              <span>{{ year }}</span>
+            </div>
+          </template>
+          <!-- <template v-if="ix !== 0">
             <div class="graph-table__barchart--wrapper">
               <div class="graph-table__barchart">
                 <div
@@ -20,7 +36,7 @@
               </div>
               <span>{{ year }}</span>
             </div>
-          </template>
+          </template> -->
         </th>
       </thead>
       <tbody>
@@ -71,7 +87,7 @@ export default {
   },
   data() {
     return {
-      years: [{ year: "metric", percentage: 0 }],
+      years: [{ year: "metric" }],
       metrics: [],
       dateLabel: "Fecha: ",
       valueLabel: "Alcanzado",
@@ -81,17 +97,40 @@ export default {
   },
   created() {
     // filter the first four
-    const [y1, y2, y3, y4] = this.values.sort(({ date: a }, { date: b }) => new Date(a) < new Date(b));
-    const years = [...[y1, y2, y3, y4]]
+    const [y1, y2, y3, y4] = this.values.sort(
+      ({ date: a }, { date: b }) => new Date(a) < new Date(b)
+    );
+    const years = [...[y1, y2, y3, y4]];
 
-    // add percentage
-    const valuesCalculated = years.map(x => ({ ...x, percentage: x.objective ? 100 * (x.value / x.objective) : 0 })).reverse()
+    // add percentage absolutes and relative. And reverse array to show years ascendently
+    const highestObjective = Math.max(
+      ...years.map(({ objective }) => objective)
+    );
+    const valuesCalculated = years
+      .map(x => ({
+        ...x,
+        percentageRelative: x.objective ? 100 * (x.value / x.objective) : 0,
+        percentageAbsolute: highestObjective ? 100 * (x.objective / highestObjective) : 100,
+      }))
+      .reverse();
 
-    this.years.push(...valuesCalculated.map(({ date: d, percentage }) => ({ year: date(d, { year: 'numeric' }), percentage })));
+    this.years.push(
+      ...valuesCalculated.map(({ date: d, percentageRelative, percentageAbsolute }) => ({
+        year: date(d, { year: "numeric" }),
+        percentageRelative,
+        percentageAbsolute
+      }))
+    );
     this.metrics = [
       { metric: this.valueLabel, ...formatValues(valuesCalculated, "value") },
-      { metric: this.objectiveLabel, ...formatValues(valuesCalculated, "objective") },
-      { metric: this.percentageLabel, ...formatValues(valuesCalculated, "percentage", percent) },
+      {
+        metric: this.objectiveLabel,
+        ...formatValues(valuesCalculated, "objective")
+      },
+      {
+        metric: this.percentageLabel,
+        ...formatValues(valuesCalculated, "percentageRelative", percent)
+      }
     ];
   }
 };

51779938d9b2ad56abb0849a17e82e43b667628d
Fernando Blat <fernando@blat.es>
2020-12-08 05:16:44 +0100

    Move iframe call to commons

--- app/javascript/lib/commons/index.js
+++ app/javascript/lib/commons/index.js
@@ -31,3 +31,20 @@ document.addEventListener("DOMContentLoaded", () => {
     Turbolinks.start()
   }
 })
+
+$(document).on("turbolinks:load", () => {
+
+  // iFrame communication
+  console.log(window.location !== window.parent.location, window.location, window.parent.location);
+  if (window.location !== window.parent.location) {
+    // Send a message to the iframe container with the URL (href + hash) and
+    // the title of the document
+    let message = {
+      href: window.location.href,
+      hash: window.location.hash,
+      title: window.document.title
+    };
+    console.log("Posting message:", message);
+    window.parent.postMessage(message, '*');
+  }
+})

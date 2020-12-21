13bc16fc9010e39001140be98f3d26d3084e9b19
Jorge <jorgeatgu@gmail.com>
2020-12-21 18:47:35 +0100

    resolved changes @crashillo

--- app/javascript/gobierto_visualizations/webapp/visualizations/treeMapNested.vue
+++ app/javascript/gobierto_visualizations/webapp/visualizations/treeMapNested.vue
@@ -237,11 +237,6 @@ export default {
         const g1 = svg
           .append("g")
           .datum(d)
-          .attr('fill', d => {
-            const { depth } = d
-            const valueColor = depth === 2 ? d.color = colors(d.parent.data.name) : ''
-            return valueColor
-          })
           .attr('class', 'depth')
 
         const g = g1.selectAll(".children")
@@ -252,8 +247,8 @@ export default {
           .join('rect')
           .attr('class', 'children')
           .attr('fill', d => {
-            const { depth } = d
-            const valueColor = depth === 1 ? d.color = colors(d.data.name) : d.color = colors(d.parent.data.name)
+            const { depth, data: { name = '' }, parent: { data: { name: parentName = '' } } } = d
+            const valueColor = depth === 1 ? d.color = colors(name) : d.color = colors(parentName)
             return valueColor
           })
           .on("click", transition);
@@ -391,20 +386,9 @@ export default {
 
           if (depth === 2) {
             let totalContracts = d.children === undefined ? '' : d.children
-            let contractsString = ''
-            if (totalContracts) {
-              totalContracts = totalContracts.filter(contract => typeof contract.data !== "function")
-              while (i < totalContracts.length) {
-                let contractAmount = selected_size === 'final_amount_no_taxes' ? `${totalContracts[i].data.value}` : `${totalContracts[i].data.final_amount_no_taxes}`
-                contractsString = `${contractsString}
-                <div class="depth-second-container">
-                  <p class="depth-second-title">${totalContracts[i].data.title}</p>
-                  <p class="text-depth-third">${I18n.t('gobierto_visualizations.visualizations.contracts.contract_amount')}: <b>${money(contractAmount)}</b></p>
-                  <p class="text-depth-third">${I18n.t('gobierto_visualizations.visualizations.contracts.tender_amount')}: <b>${money(totalContracts[i].data.initial_amount_no_taxes)}</b></p>
-                </div>`
-                i++
-              }
-            }
+            let contractsString = totalContracts
+              .filter(contract => typeof contract.data !== "function")
+              .map(contract => createDepthSecondHTML(contract))
 
             tooltipAssignee
               .style("display", "block")
@@ -418,7 +402,7 @@ export default {
                   <span class="treemap-nested-tooltip-header-title">
                     ${I18n.t('gobierto_visualizations.visualizations.contracts.contracts')}
                   </span>
-                  ${contractsString}
+                  ${contractsString.join('')}
                 `
               })
               .style('top', positionTop)
@@ -469,6 +453,18 @@ export default {
             .style("opacity", 0)
             .style("display", "none")
         })
+
+        function createDepthSecondHTML(contract) {
+          const { data: { title, initial_amount_no_taxes, value, final_amount_no_taxes } } = contract
+          let contractAmount = selected_size === 'final_amount_no_taxes' ? `${value}` : `${final_amount_no_taxes}`
+          let contractsString = `
+          <div class="depth-second-container">
+            <p class="depth-second-title">${title}</p>
+            <p class="text-depth-third">${I18n.t('gobierto_visualizations.visualizations.contracts.contract_amount')}: <b>${money(contractAmount)}</b></p>
+            <p class="text-depth-third">${I18n.t('gobierto_visualizations.visualizations.contracts.tender_amount')}: <b>${money(initial_amount_no_taxes)}</b></p>
+          </div>`
+          return contractsString
+        }
 
         function transition(d) {
           if (transitioning || !d) return;
@@ -558,19 +554,12 @@ export default {
       }
 
       function checkSizeBreadcumbs() {
-        const treeMapSidebar = document.querySelector('.treemap-nested-sidebar');
-        const treeMapSidebarWidth = treeMapSidebar.offsetWidth
+        const sidebarAvailableWidth = (document.querySelector('.treemap-nested-sidebar').offsetWidth - document.querySelector('.treemap-nested-sidebar-button-group').offsetWidth)
 
-        const treeMapSidebarButton = document.querySelector('.treemap-nested-sidebar-button-group');
-        const treeMapSidebarButtonWidth = treeMapSidebarButton.offsetWidth
-
-        const treeMapAvailableWidth = treeMapSidebarWidth - treeMapSidebarButtonWidth
-
-        const treeMapSidebarNav = document.querySelector('.treemap-nested-sidebar-nav');
-        const treeMapSidebarNavWidth = treeMapSidebarNav.offsetWidth
+        const sidebarNav = document.querySelector('.treemap-nested-sidebar-nav').offsetWidth;
 
         const treeMapSidebarNavContainer = document.querySelector('.treemap-nested-sidebar-nav-breadcumb');
-        if (treeMapSidebarNavWidth > treeMapAvailableWidth) treeMapSidebarNavContainer.classList.add('ellipsis')
+        if (sidebarNav > sidebarAvailableWidth) treeMapSidebarNavContainer.classList.add('ellipsis')
       }
     },
     resizeListener() {

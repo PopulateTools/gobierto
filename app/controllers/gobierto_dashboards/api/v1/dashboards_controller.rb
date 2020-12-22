4c5621f81752b395b8264e545cff3182fb4a1305
Eduardo Martínez <eduardomech@gmail.com>
2020-12-22 21:29:40 +0100

    Add API actions to create, update and delete a dashboard

--- app/controllers/gobierto_dashboards/api/v1/dashboards_controller.rb
+++ app/controllers/gobierto_dashboards/api/v1/dashboards_controller.rb
@@ -44,6 +44,58 @@ module GobiertoDashboards
           )
         end
 
+        # GET /api/v1/dashboards/new
+        # GET /api/v1/dashboards/new.json
+        def new
+          render(
+            json: base_relation.new,
+            adapter: :json_api
+          )
+        end
+
+        # POST /api/v1/dashboards
+        # POST /api/v1/dashboards.json
+        def create
+          @form = form_class.new(resource_params.merge(site_id: current_site.id, admin_id: current_admin.id))
+
+          if @form.save
+            render(
+              json: @form.resource,
+              status: :created,
+              adapter: :json
+            )
+          else
+            api_errors_render(@form, adapter: :json_api)
+          end
+        end
+
+        # PATCH/PUT /api/v1/dashboards/1
+        # PATCH/PUT /api/v1/dashboards/1.json
+        def update
+          find_resource
+
+          @form = form_class.new(resource_params.merge(site_id: current_site.id, admin_id: current_admin.id, id: @resource.id))
+
+          if @form.save
+            render(
+              json: @form.resource,
+              adapter: :json
+            )
+          else
+            api_errors_render(@form, adapter: :json_api)
+          end
+        end
+
+        # DELETE /api/v1/dashboards/1
+        # DELETE /api/v1/dashboards/1.json
+        def destroy
+          find_resource
+
+          @resource.destroy
+
+          head :no_content
+        end
+
         private
 
         def find_resource
@@ -52,6 +104,14 @@ module GobiertoDashboards
 
         def base_relation
           current_site.dashboards.active
+        end
+
+        def form_class
+          DashboardForm
+        end
+
+        def resource_params
+          ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [:title_translations, :title, :context, :widgets_configuration, :visibility_level])
         end
       end
     end

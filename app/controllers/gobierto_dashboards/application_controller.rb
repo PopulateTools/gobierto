19e8fb963db619d14370361f2ecdf4ed108a1f15
Eduardo Martínez <eduardomech@gmail.com>
2020-12-23 11:44:55 +0100

    Remove unused controllers and translations

--- app/controllers/gobierto_dashboards/application_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class GobiertoDashboards::ApplicationController < ApplicationController
-  include User::SessionHelper
-
-  layout "gobierto_dashboards/layouts/application"
-end

0f1431427ecc95897d0fa5864799589ec3eadc6d
Hugoren Martinako <aumpfbahn@gmail.com>
2021-01-14 16:13:27 +0100

    purge unwanted props

--- app/javascript/gobierto_dashboards/webapp/Maker.vue
+++ app/javascript/gobierto_dashboards/webapp/Maker.vue
@@ -65,7 +65,7 @@ import HeaderForm from "./components/HeaderForm";
 import Aside from "./layouts/Aside";
 import Main from "./layouts/Main";
 import SmallCard from "./components/SmallCard";
-import { Widgets } from "./lib/widgets";
+import { Widgets, RequiredFields } from "./lib/widgets";
 import { FactoryMixin } from "./lib/factories";
 import { TextEditable } from "lib/vue-components";
 
@@ -208,10 +208,23 @@ export default {
     handleViewerReady() {
       this.viewerLoaded = true;
     },
+    subset(x) {
+      // purge those unwanted props
+      return Object.keys(x).reduce((acc, key) => { if (RequiredFields.includes(key)) acc[key] = x[key]; return acc }, {})
+    },
     handleSave() {
+      const widgetsConfigurationReduced = this.configuration?.attributes?.widgets_configuration.map(this.subset)
+      const configuration = {
+        ...this.configuration,
+        attributes: {
+          ...this.configuration?.attributes,
+          widgets_configuration: widgetsConfigurationReduced
+        }
+      }
+
       this.id
-        ? this.putDashboard(this.id, this.configuration)
-        : this.postDashboard(this.configuration);
+        ? this.putDashboard(this.id, configuration)
+        : this.postDashboard(configuration);
 
       this.dirty = false;
     },

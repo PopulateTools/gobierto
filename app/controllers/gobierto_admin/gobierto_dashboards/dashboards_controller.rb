f3f0b6f127c45dbccd6f7b0ec912ab3e5de65d3a
Eduardo Martínez <eduardomech@gmail.com>
2021-01-15 12:19:37 +0100

    Clean admin/dashboards controller

--- app/controllers/gobierto_admin/gobierto_dashboards/dashboards_controller.rb
+++ app/controllers/gobierto_admin/gobierto_dashboards/dashboards_controller.rb
@@ -4,25 +4,8 @@ module GobiertoAdmin
   module GobiertoDashboards
     class DashboardsController < GobiertoAdmin::BaseController
       layout false
-      helper_method :index_path
 
-      def modal
-
-      end
-
-      private
-
-      def find_dashboard
-        @dashboard = current_site.dashboards.find(params[:id])
-      end
-
-      def dashboard_params
-        params.require(:dashboard).permit(:visibility_level, :context, title_translations: [*I18n.available_locales])
-      end
-
-      def index_path
-        params.require(:index_path)
-      end
+      def modal; end
     end
   end
 end

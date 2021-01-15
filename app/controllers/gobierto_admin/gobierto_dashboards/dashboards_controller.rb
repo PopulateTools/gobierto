c4b0403800d589553dd16dd96442038ad35a482f
Eduardo Martínez <eduardomech@gmail.com>
2021-01-15 12:42:11 +0100

    Check permissions for dashboards modal action

--- app/controllers/gobierto_admin/gobierto_dashboards/dashboards_controller.rb
+++ app/controllers/gobierto_admin/gobierto_dashboards/dashboards_controller.rb
@@ -3,6 +3,10 @@
 module GobiertoAdmin
   module GobiertoDashboards
     class DashboardsController < GobiertoAdmin::BaseController
+      before_action { module_enabled!(current_site, "GobiertoDashboards") }
+      before_action { module_allowed!(current_admin, "GobiertoDashboards") }
+      before_action -> { module_allowed_action!(current_admin, current_admin_module, [:manage_dashboards, :view_dashboards]) }
+
       layout false
 
       def modal; end

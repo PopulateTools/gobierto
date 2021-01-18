ad63e5e17e7c7947a88a10a324b7ce783aedfe91
Hugoren Martinako <aumpfbahn@gmail.com>
2021-01-18 14:17:24 +0100

    add select2 behaviour to modal instance

--- app/javascript/gobierto_admin/modules/admin_groups_admins_controller.js
+++ app/javascript/gobierto_admin/modules/admin_groups_admins_controller.js
@@ -7,7 +7,23 @@ window.GobiertoAdmin.AdminGroupsAdminsController = (function() {
   };
 
   function _handleSelectBehaviors() {
-    $("[data-behavior=select2]").select2()
+    $("[data-behavior=select2]")
+      .select2()
+      .on("select2:select", ({ params }) => {
+        // this callback only applies to list_modal.html.erb
+        const { element } = params?.data
+        if (element) {
+          const { dataset, value } = element
+          const btn = document.getElementById("add-current-dashboard-btn")
+          if (btn) {
+            const url = new URL(btn.href)
+            url.searchParams.set("id", value)
+            url.searchParams.set("preview_path", dataset?.previewPath)
+
+            btn.href = url
+          }
+        }
+      })
   }
 
   return AdminGroupsAdminsController;

2d00addf6b48b3ec9a30990d5a3c31bc9d67adfa
Hugoren Martinako <aumpfbahn@gmail.com>
2021-02-03 11:47:11 +0100

    Update ViewerManager.vue

--- app/javascript/gobierto_dashboards/webapp/ViewerManager.vue
+++ app/javascript/gobierto_dashboards/webapp/ViewerManager.vue
@@ -70,6 +70,8 @@ export default {
 
     if (this.id) {
       this.currentDashboard = this.dashboards.find(({ id }) => +id === +this.id)
+    } else if (this.dashboards.length === 1) {
+      this.currentDashboard = this.dashboards[0]
     }
 
     // Emit to his parent, if there was any

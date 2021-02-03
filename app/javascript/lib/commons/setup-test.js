12a2aef0223ca4794180e597d8a4f873e7ef0616
Jorge <jorgeatgu@gmail.com>
2021-02-03 17:46:54 +0100

    remove test file

--- app/javascript/lib/commons/setup-test.js
+++
@@ -1,9 +0,0 @@
-import $ from "jquery";
-
-window.jQuery = $;
-require('webpack-jquery-ui/slider');
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import * as I18n from 'i18n-js'
-
-global.I18n = I18n

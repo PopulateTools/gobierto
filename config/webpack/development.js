98f563cb1acac0c6cdf8fc10dcbb9985a6b61017
Hugoren Martinako <aumpfbahn@gmail.com>
2021-02-17 14:15:46 +0100

    test: remove speed-measure-webpack-plugin

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,12 +3,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 const stats = require('./config/stats')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 
 environment.config.merge(stats)
 environment.plugins.append('BundleAnalyzerPlugin', new BundleAnalyzerPlugin({
   openAnalyzer: false
 }))
 
-const smp = new SpeedMeasurePlugin();
-module.exports = smp.wrap(environment.toWebpackConfig())
+module.exports = environment.toWebpackConfig()

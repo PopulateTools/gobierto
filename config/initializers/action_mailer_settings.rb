5d51ab4a83d4de3cc2c4ce43bfd384250b943ef0
Fernando Blat <fernando@blat.es>
2021-03-21 15:20:11 +0100

    Upgrade Amazon SES delivery to v4 signature

--- config/initializers/action_mailer_settings.rb
+++ config/initializers/action_mailer_settings.rb
@@ -7,6 +7,7 @@ if Rails.application.secrets.mailer_delivery_method == "ses"
                                          access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID") { "" },
                                          secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY") { "" },
                                          server: "email.eu-west-1.amazonaws.com",
+                                         region: 'eu-west-1',
                                          signature_version: 4
 
 elsif Rails.application.secrets.mailer_delivery_method == "smtp"

ab6add7f8210fa4694f31ef48bd167d256aa9c03
Hugoren Martinako <aumpfbahn@gmail.com>
2021-03-30 18:25:12 +0200

    Update terser.js

--- config/webpack/config/terser.js
+++ config/webpack/config/terser.js
@@ -2,6 +2,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   optimization: {
+    minimize: false,
     minimizer: [
       new TerserPlugin({
         terserOptions: {

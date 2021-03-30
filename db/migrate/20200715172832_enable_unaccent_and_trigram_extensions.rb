e9b7c26bb0a37261a7aa8fdffd9077ae70fc4b97
Fernando Blat <fernando@blat.es>
2021-03-30 11:03:13 +0200

    Only enable extensions if not enabled

--- db/migrate/20200715172832_enable_unaccent_and_trigram_extensions.rb
+++ db/migrate/20200715172832_enable_unaccent_and_trigram_extensions.rb
@@ -2,7 +2,7 @@
 
 class EnableUnaccentAndTrigramExtensions < ActiveRecord::Migration[5.2]
   def change
-    enable_extension("unaccent")
-    enable_extension("pg_trgm")
+    enable_extension("unaccent") unless extension_enabled?("unaccent")
+    enable_extension("pg_trgm") unless extension_enabled?("pg_trgm")
   end
 end

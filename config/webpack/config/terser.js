d1492d568574f31b140502ed43f91dcb12ca6304
Hugoren Martinako <aumpfbahn@gmail.com>
2021-04-05 16:58:12 +0200

    test medium+

--- config/webpack/config/terser.js
+++ config/webpack/config/terser.js
@@ -2,18 +2,9 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   optimization: {
-    minimize: false,
     minimizer: [
       new TerserPlugin({
-        terserOptions: {
-          format: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-        parallel: 2,
-        cache: true,
-        sourceMap: false,
+        parallel: 3,
       })
     ]
   }

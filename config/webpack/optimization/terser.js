df6159e0ccd37db237c9ceb2b6b4d3f407cb326a
Hugoren Martinako <aumpfbahn@gmail.com>
2021-04-05 15:25:48 +0200

    don't minimize

--- config/webpack/optimization/terser.js
+++ config/webpack/optimization/terser.js
@@ -1,11 +1,12 @@
-const TerserPlugin = require("terser-webpack-plugin");
+// const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   optimization: {
-    minimizer: [
-      new TerserPlugin({
-        parallel: 8
-      })
-    ]
+    minimize: false
+    // minimizer: [
+    //   new TerserPlugin({
+    //     parallel: 8
+    //   })
+    // ]
   }
 };

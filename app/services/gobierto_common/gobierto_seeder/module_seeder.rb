2ba5088dc0dd1574e67ec2d1f5ef973ecdd1d864
stbn <stbnrivas@gmail.com>
2021-04-19 19:09:34 +0200

    fixing test after refactor services gobierto seeder

--- app/services/gobierto_common/gobierto_seeder/module_seeder.rb
+++ app/services/gobierto_common/gobierto_seeder/module_seeder.rb
@@ -2,14 +2,10 @@ module GobiertoCommon
   module GobiertoSeeder
     class ModuleSeeder
       def self.seed(module_name, site)
-        module_path = module_name.underscore
-        module_seeds_path = Rails.root.join("db/seeds/modules/#{module_path}")
-        return unless Dir.exist?(module_seeds_path)
-
         return unless site.configuration.modules.include?(module_name)
 
-        require "#{module_seeds_path}/seeds"
-        GobiertoSeeds::Recipe.run(site)
+        seeder = "GobiertoSeeds::#{module_name.camelize}::Recipe".safe_constantize
+        seeder.run(site) if seeder && defined?(seeder)
       end
     end
   end

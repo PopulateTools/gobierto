903364719614d6a741ce438cd0dbce1ea3eaf5aa
Hugoren Martinako <aumpfbahn@gmail.com>
2021-04-26 16:29:41 +0200

    edit btn in maptab

--- app/javascript/gobierto_data/webapp/components/sets/MapTab.vue
+++ app/javascript/gobierto_data/webapp/components/sets/MapTab.vue
@@ -1,23 +1,34 @@
 <template>
-  <div class="gobierto-data-visualization--aspect-ratio-16-9">
-    <Visualizations
-      ref="viewer"
-      class="gobierto-data-visualization--item"
-      :items="items"
-      :config="config"
-      :object-columns="objectColumns"
-      :config-map="configMapZoom"
-      @showSaving="handleConfig"
+  <div class="gobierto-data-sets-nav--tab-container">
+    <Button
+      :text="labelEdit"
+      class="btn-sql-editor btn-sql-revert-query"
+      :class="{ 'isDisabled': !isDirty }"
+      icon="chart-area"
+      background="#fff"
+      @click.native="handleClick"
     />
+    <div class="gobierto-data-visualization--aspect-ratio-16-9">
+      <Visualizations
+        ref="viewer"
+        :items="items"
+        :config="config"
+        :object-columns="objectColumns"
+        :config-map="configMapZoom"
+        @showSaving="handleConfig"
+      />
+    </div>
   </div>
 </template>
 <script>
 import Visualizations from "../commons/Visualizations.vue";
+import Button from "../commons/Button.vue";
 
 export default {
   name: "MapTab",
   components: {
-    Visualizations
+    Visualizations,
+    Button
   },
   props: {
     items: {
@@ -38,7 +49,9 @@ export default {
       config: {
         plugin: 'map'
       },
-      configMapZoom: { ...this.configMap, zoom: true }
+      configMapZoom: { ...this.configMap, zoom: true },
+      labelEdit: I18n.t("gobierto_data.projects.edit") || "",
+      isDirty: false
     }
   },
   mounted() {
@@ -54,6 +67,11 @@ export default {
     handleConfig() {
       const config = this.$refs.viewer.getConfig()
       sessionStorage.setItem("map-tab", JSON.stringify(config))
+
+      this.isDirty = true
+    },
+    handleClick() {
+      this.$router.push({ path: `/datos/${this.$route.params.id}/editor` })
     }
   }
 };

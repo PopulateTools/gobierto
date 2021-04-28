54e4522f0baa07315ba228a89721d4ce43f62b33
Hugoren Martinako <aumpfbahn@gmail.com>
2021-04-28 14:24:13 +0200

    [skip ci] performance: dynamic imports observatory

--- app/javascript/gobierto_observatory/index.js
+++ app/javascript/gobierto_observatory/index.js
@@ -1,17 +1,21 @@
-import './modules/application.js'
-import { DemographyMapController } from "./modules/demography_map_controller.js";
+import "../../assets/stylesheets/module-observatory.scss"
+import "./modules/application.js"
 
 document.addEventListener('DOMContentLoaded', () => {
   const demographyMapAppNode = document.getElementById("gobierto-observatory-demography-map-app");
+
   if (demographyMapAppNode !== null) {
-    new DemographyMapController({
-      selector: demographyMapAppNode.id,
-      sectionsEndpoint: demographyMapAppNode.dataset.endpointSections,
-      studiesEndpoint: demographyMapAppNode.dataset.endpointStudies,
-      originEndpoint: demographyMapAppNode.dataset.endpointOrigin,
-      mapLat: demographyMapAppNode.dataset.mapLat,
-      mapLon: demographyMapAppNode.dataset.mapLon,
-      ineCode: demographyMapAppNode.dataset.ineCode
-    });
+    // load only the module in the "/observatorio/mapa" path
+    import("./modules/demography_map_controller.js").then(({ DemographyMapController }) => {
+      new DemographyMapController({
+        selector: demographyMapAppNode.id,
+        sectionsEndpoint: demographyMapAppNode.dataset.endpointSections,
+        studiesEndpoint: demographyMapAppNode.dataset.endpointStudies,
+        originEndpoint: demographyMapAppNode.dataset.endpointOrigin,
+        mapLat: demographyMapAppNode.dataset.mapLat,
+        mapLon: demographyMapAppNode.dataset.mapLon,
+        ineCode: demographyMapAppNode.dataset.ineCode
+      });
+    })
   }
 });

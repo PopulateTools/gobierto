4349764c4f412f6ca4024b0074125826e692414e
Jorge <jorgeatgu@gmail.com>
2021-06-03 15:51:24 +0200

    extract getVisualizationList

--- app/javascript/gobierto_embeds/index.js
+++ app/javascript/gobierto_embeds/index.js
@@ -1,8 +1,8 @@
-import perspective from "@finos/perspective";
 import "@finos/perspective-viewer-datagrid";
 import "@finos/perspective-viewer-d3fc";
 import "@finos/perspective-viewer/themes/all-themes.css";
 import "../../assets/stylesheets/comp-perspective-viewer.scss";
+import { getVisualizationList } from "./getVisualizationList.js";
 
 const appendStyle = async () => {
   const { src } = document.querySelector('script[src*="embeds.js"]');
@@ -17,66 +17,6 @@ const appendStyle = async () => {
   link.rel = "stylesheet";
   link.media = "screen,print";
   document.getElementsByTagName("head")[0].appendChild(link);
-};
-
-export const getVisualizationList = async container => {
-  const { gobiertoVisualization: id, site, token } = container.dataset;
-  const headers = token ? { Authorization: `Bearer ${token}` } : {};
-
-  const { data: visualization } =
-    (await fetch(`${site}/api/v1/data/visualizations/${id}`, {
-      headers
-    }).then(r => r.json())) || {};
-  if (visualization) {
-    const {
-      attributes: { query_id, sql, spec }
-    } = visualization || {};
-
-    const url = query_id
-      ? `${site}/api/v1/data/queries/${query_id}.csv`
-      : sql
-      ? `${site}/api/v1/data/data.csv?sql=${encodeURIComponent(sql)}`
-      : null;
-
-    if (url) {
-      const data = await fetch(url, { headers }).then(r => r.text());
-
-      if (data) {
-        const viewer = document.createElement("perspective-viewer");
-
-        if (spec.column_pivots) {
-          viewer.setAttribute(
-            "column-pivots",
-            JSON.stringify(spec.column_pivots)
-          );
-        }
-
-        if (spec.row_pivots) {
-          viewer.setAttribute("row-pivots", JSON.stringify(spec.row_pivots));
-        }
-
-        if (spec.computed_columns) {
-          viewer.setAttribute(
-            "computed-columns",
-            JSON.stringify(spec.computed_columns)
-          );
-        }
-
-        container.parentNode.replaceChild(viewer, container);
-
-        // hide configuration
-        const configButtonPerspective = viewer.shadowRoot.getElementById(
-          "config_button"
-        );
-        configButtonPerspective.style.display = "none";
-
-        // run perspective
-        const table = perspective.worker().table(data);
-        viewer.restore(spec);
-        viewer.load(table);
-      }
-    }
-  }
 };
 
 appendStyle();

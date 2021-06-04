9bf32778de3991aa477de3e23f4c5da5b21db32e
Jorge <jorgeatgu@gmail.com>
2021-06-04 07:35:09 +0200

    token

--- test/javascript/gobierto_data/embed.spec.js
+++ test/javascript/gobierto_data/embed.spec.js
@@ -6,13 +6,15 @@ import { getVisualizationList } from "../../../app/javascript/gobierto_embeds/ge
 import "@finos/perspective-viewer/dist/umd/perspective-viewer.js";
 import "@finos/perspective-viewer-datagrid/dist/umd/perspective-viewer-datagrid.js";
 
+const token = process.env.GOBIERTO_DATA_TOKEN
+
 beforeAll(async () => {
   const container = document.createElement('section')
   document.body.appendChild(container)
   const element = document.createElement('div');
-  element.setAttribute('data-gobierto-visualization', '183');
+  element.setAttribute('data-gobierto-visualization', '192');
   element.setAttribute('data-site', 'https://getafe.gobify.net');
-  element.setAttribute('data-token', '1yvuaff8H2Ta5XGTYCXBhzUV');
+  element.setAttribute('data-token', token);
   container.appendChild(element)
   await getVisualizationList(element)
 });

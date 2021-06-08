1267f56c4be978118f3c6d8309bd7c0eca89f4b4
Jorge <jorgeatgu@gmail.com>
2021-06-08 11:09:58 +0200

    add another test

--- test/javascript/gobierto_data/embed.test.js
+++ test/javascript/gobierto_data/embed.test.js
@@ -7,11 +7,22 @@ import { renderPerspective } from "../../../app/javascript/gobierto_embeds/rende
 import "@finos/perspective-viewer/dist/umd/perspective-viewer.js";
 import "@finos/perspective-viewer-datagrid/dist/umd/perspective-viewer-datagrid.js";
 
-test('render perspective', () => {
+describe('visualization', () => {
   const container = document.createElement('section')
   document.body.appendChild(container)
   const element = document.createElement('div');
   container.appendChild(element)
-  renderPerspective(data, element)
-  expect(document.querySelector('perspective-viewer')).not.toBeNull();
+
+  test('render perspective', () => {
+    renderPerspective(data, element)
+    expect(document.querySelector('perspective-viewer')).not.toBeNull();
+  })
+
+  test('waits 1 second for Perspective to add the columns to the web component', (done) => {
+    setTimeout(() => {
+      expect(JSON.parse(document.querySelector('perspective-viewer').getAttribute('columns'))).toHaveLength(2);
+      done();
+    }, 1000);
+  });
+
 })

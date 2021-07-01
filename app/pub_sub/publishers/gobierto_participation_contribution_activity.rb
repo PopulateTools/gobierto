0bf4287669078bcbca7bb265cc26ee6a307ca834
stbn <stbnrivas@gmail.com>
2021-07-01 08:18:39 +0200

    WIP removing files [skip ci]

--- app/pub_sub/publishers/gobierto_participation_contribution_activity.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Publishers
-  class GobiertoParticipationContributionActivity
-    include Publisher
-    self.pub_sub_namespace = "activities/gobierto_participation_contribution"
-  end
-end
--- app/pub_sub/publishers/gobierto_participation_process_activity.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Publishers
-  class GobiertoParticipationProcessActivity
-    include Publisher
-    self.pub_sub_namespace = 'activities/gobierto_participation_processes'
-  end
-end

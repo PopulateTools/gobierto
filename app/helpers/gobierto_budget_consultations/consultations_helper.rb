ac41caa81bc4c29dad92beda4e00ee415261b467
stbn <stbnrivas@gmail.com>
2021-07-09 09:17:04 +0200

    WIP removing files

--- app/helpers/gobierto_budget_consultations/consultations_helper.rb
+++
@@ -1,7 +0,0 @@
-module GobiertoBudgetConsultations
-  module ConsultationsHelper
-    def budget_amount_to_human(budget_amount)
-      number_to_currency(budget_amount, :precision => (budget_amount.round == budget_amount) ? 0 : 2)
-    end
-  end
-end
--- app/models/gobierto_admin/permission/gobierto_budget_consultations.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module GobiertoAdmin
-  class Permission::GobiertoBudgetConsultations < GroupPermission
-    default_scope -> { where(namespace: "site_module", resource_type: "gobierto_budget_consultations") }
-  end
-end

8b9924db9be755ae440c1d64168f82aac2cbb749
stbn <stbnrivas@gmail.com>
2021-07-13 12:26:45 +0200

    fixing test

--- test/models/gobierto_core/site_template_test.rb
+++ test/models/gobierto_core/site_template_test.rb
@@ -5,7 +5,7 @@ require "test_helper"
 module GobiertoCore
   class SiteTemplateTest < ActiveSupport::TestCase
     def site_template
-      @site_template ||= gobierto_core_site_templates(:cortegada_site_template_index)
+      @site_template ||= gobierto_core_site_templates(:site_with_template_index)
     end
 
     def test_valid

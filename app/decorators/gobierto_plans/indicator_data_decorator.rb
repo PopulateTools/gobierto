9d46fd34fe4cbc8f02a1d2d05af05f66e180be3c
stbn <stbnrivas@gmail.com>
2021-07-20 12:47:00 +0200

    refactor csv generation

--- app/decorators/gobierto_plans/indicator_data_decorator.rb
+++ app/decorators/gobierto_plans/indicator_data_decorator.rb
@@ -11,6 +11,23 @@ module GobiertoPlans
       @columns = 0
     end
 
+    def csv
+      lines = []
+      CSV.generate do |csv|
+        collection.each do |item|
+          lines.concat(csv_row(item))
+        end
+        # need iterate again because header length is dynamic
+        csv << csv_header
+        lines.each do |line|
+          # TODO: add extra commas ?
+          csv << line
+        end
+      end.force_encoding('utf-8')
+    end
+
+    private
+
     def csv_header
       header = %w(Node.Title Node.external_id custom_field)
       1.upto(@columns) do |index|
@@ -19,32 +36,21 @@ module GobiertoPlans
       header
     end
 
-    def dynamic_columns(object)
-      object.payload[object.custom_field.uid.to_s].values
-    end
-
     def csv_row(object)
-      @columns = object.payload[object.custom_field.uid.to_s].size if object.payload[object.custom_field.uid.to_s].size > @columns
-      [
-        plan.title_translations["en"],
-        object.item.external_id,        # N+1 because it is polymorphic
-        object.custom_field.uid,
-        object.payload[object.custom_field.uid.to_s].first.values.first,
-      ]
-    end
+      current_columns = object.payload[object.custom_field.uid.to_s].first&.size
+      values = object.payload[object.custom_field.uid.to_s].map(&:values)
+      @columns = current_columns if current_columns > @columns
 
-    def csv
-      lines = []
-      CSV.generate do |csv|
-        collection.each do |item|
-          lines << csv_row(item)
-        end
-        # need iterate again because header length is dynamic
-        csv << csv_header
-        lines.each do |line|
-          csv << line
-        end
-      end.force_encoding('utf-8')
+      values = object.payload[object.custom_field.uid.to_s].map(&:values)
+
+      values.map do |value|
+        [
+          plan.title_translations["en"],
+          object.item.external_id,        # N+1 because it is polymorphic
+          object.custom_field.uid
+        ].concat(value)
+      end
+
     end
 
   end
--- test/decorators/gobierto_plans/indicator_data_decorator_test.rb
+++ test/decorators/gobierto_plans/indicator_data_decorator_test.rb
@@ -8,10 +8,39 @@ module GobiertoPlans
     def setup
     end
 
-    def test_csv_with_zero_columns
+    def test_export_plan_to_csv_without_indicators
+      plan = gobierto_plans_plans(:strategic_plan) # plan_categories_vocabulary [people_and_families_plan_term welfare_payments_plan_term center_scholarships_plan_term center_basic_needs_plan_term economy_plan_term city_plan_term]
+      assert_equal "Node.Title,Node.external_id,custom_field\n", IndicatorDataDecorator.new(plan).csv
     end
 
-    def test_csv_with_same_columns_length
+    def test_export_plan_to_csv_with_single_indicator
+      plan = gobierto_plans_plans(:dashboards_plan) # dashboards_plan_categories_vocabulary and with 2 projects aka nodes
+      custom_field = ::GobiertoCommon::CustomField.where(instance: plan)
+
+      dashboard_project = gobierto_plans_nodes(:dashboard_project) # economy_plan_term
+      records_dashboard = ::GobiertoCommon::CustomFieldRecord.where(item: dashboard_project)
+      # without indicators
+      dashboard_alternative_project = gobierto_plans_nodes(:political_agendas) # center_basic_needs_plan_term
+      records_dashboard_alternative = ::GobiertoCommon::CustomFieldRecord.where(item: dashboard_alternative_project)
+
+      expected = [
+          "Node.Title,Node.external_id,custom_field,col_1,col_2,col_3,col_4",
+          "Dashboards plan,,project-metrics,2018-07-30,28,Decretos Emitidos,55",
+          "Dashboards plan,,project-metrics,2017-08-10,13,decretos emitidos,95",
+          "Dashboards plan,,project-metrics,2016-08-04,37,decretos   emitidos,74",
+          "Dashboards plan,,project-metrics,2015-05-08,32,Notificaciones enviadas,98",
+          "Dashboards plan,,project-metrics,2019-12-07,48,Expedientes cerrados,57",
+          "Dashboards plan,,project-metrics,2020-01-01,10,Expedientes cerrados,20",
+          "Dashboards plan,,project-metrics,2019-08-10,15,Camas UCI,20",
+          "Dashboards plan,,project-metrics,2018-08-04,50,Expedientes cerrados,40",
+          "Dashboards plan,,project-metrics,2021-12-31,356,camas uci,270",
+          ""
+        ]
+      assert_equal expected.join("\n"), IndicatorDataDecorator.new(plan).csv
+    end
+
+    def test_export_plan_to_csv_with_multiples_indicator_and_different_length_of_columns
+      plan = gobierto_plans_plans(:dashboards_plan)
     end
 
     def test_csv_with_different_columns_length

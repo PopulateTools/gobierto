b5ae370e56deeeb91da399125cde9245b9da076c
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2021-09-08 17:32:09 +0200

    Allow setting of visualizations root path from module settings

--- app/models/gobierto_visualizations.rb
+++ app/models/gobierto_visualizations.rb
@@ -2,7 +2,11 @@
 
 module GobiertoVisualizations
   def self.root_path(current_site)
-    Rails.application.routes.url_helpers.gobierto_visualizations_root_path
+    return unless (module_settings = current_site.gobierto_visualizations_settings)
+
+    home_key = find_home_key(module_settings.settings&.dig("visualizations_config", "visualizations"))
+
+    Rails.application.routes.url_helpers.send("gobierto_visualizations_#{home_key}_path")
   end
 
   def self.default_visualizations_configuration_settings
@@ -25,5 +29,11 @@ module GobiertoVisualizations
       }
     }
 JSON
+  end
+
+  def self.find_home_key(settings)
+    return unless settings.present?
+
+    settings.find { |_key, config| config["home"] }[0]
   end
 end

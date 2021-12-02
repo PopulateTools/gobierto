0eb306a50063485d35840426483372605cbab536
Fernando Blat <fernando@blat.es>
2021-12-02 05:10:46 +0100

    Configure with environment variables

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,8 +47,13 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :info
-  config.lograge.enabled = true
+  if ENV["ENABLE_LOG_RAGE"].present?
+    config.log_level = :info
+    config.lograge.enabled = true
+  else
+    config.log_tags = [:host, :request_id]
+    config.log_level = :debug
+  end
 
   config.cache_store = :redis_cache_store, { url: ENV["REDIS_CACHE_URL"] }
 

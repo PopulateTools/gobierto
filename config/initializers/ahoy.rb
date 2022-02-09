113964f8a063e32d10e0f48afe1171405ec98bf0
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-02-09 15:56:15 +0100

    Add GDPR compliance settings

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,5 +1,11 @@
 class Ahoy::Store < Ahoy::DatabaseStore
+  def authenticate(data)
+    # disables automatic linking of visits and users
+  end
 end
+
+Ahoy.mask_ips = true
+Ahoy.cookies = false
 
 # set to true for JavaScript tracking
 Ahoy.api = false

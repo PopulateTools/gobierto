6512c30cdee04c5f4017d880a07d97f776d9342d
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-02-16 18:24:59 +0100

    Make association of events whith visits optional

--- app/models/ahoy/event.rb
+++ app/models/ahoy/event.rb
@@ -3,7 +3,7 @@ class Ahoy::Event < ApplicationRecord
 
   self.table_name = "ahoy_events"
 
-  belongs_to :visit
+  belongs_to :visit, optional: true
   belongs_to :user, optional: true
   belongs_to :site, optional: true
 end

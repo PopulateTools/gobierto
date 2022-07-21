e60e84fd281ff087ec0dbbec7faaf051687b97cb
Jorge <jorgeatgu@gmail.com>
2022-07-21 15:57:50 +0200

    browserlist

--- babel.config.js
+++ babel.config.js
@@ -29,7 +29,7 @@ module.exports = function(api) {
         '@babel/preset-env',
         {
           modules: false,
-          targets: [ 'last 2 versions' ],
+          targets: ['last 2 versions'],
           exclude: ['transform-typeof-symbol']
         }
       ]

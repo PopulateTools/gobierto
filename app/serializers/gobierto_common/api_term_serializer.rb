b102a54901e4e453ce14f4fa78b2d8c895685813
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-09-13 17:10:57 +0200

    Define serializers for API of vocabularies and terms

---
+++ app/serializers/gobierto_common/api_term_serializer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module GobiertoCommon
+  class ApiTermSerializer < ActiveModel::Serializer
+    attributes(
+      :id,
+      :name_translations,
+      :slug,
+      :position,
+      :level,
+      :term_id,
+      :parent_external_id,
+      :external_id
+    )
+  end
+end

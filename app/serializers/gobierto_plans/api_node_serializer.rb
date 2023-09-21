ed53b8f33bb78488271bf6ac4b1ea8ec8a60d793
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-09-21 19:26:23 +0200

    Define API serializers for admin plans including projects

---
+++ app/serializers/gobierto_plans/api_node_serializer.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module GobiertoPlans
+  class ApiNodeSerializer < BaseSerializer
+    include ::GobiertoCommon::Versionable
+    # include ::GobiertoCommon::HasCustomFieldsAttributes
+
+    attributes(
+      :id,
+      :external_id,
+      :visibility_level,
+      :moderation_stage,
+      :name_translations,
+      :category_id,
+      :category_external_id,
+      :status_id,
+      :status_external_id,
+      :progress,
+      :starts_at,
+      :ends_at,
+      :position
+    )
+
+    def category
+      @category ||= object.categories.find_by(gplan_categories_nodes: { category_id: instance_options[:plan].categories })
+    end
+
+    def category_id
+      return if category.blank?
+
+      category.id
+    end
+
+    def category_external_id
+      return if category.blank?
+
+      category.external_id
+    end
+
+    def status_external_id
+      object.status&.external_id
+    end
+
+    # def custom_fields
+    #   instance_options[:custom_fields]
+    # end
+  end
+end
---
+++ app/serializers/gobierto_plans/api_plan_serializer.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module GobiertoPlans
+  class ApiPlanSerializer < BaseSerializer
+    attributes(
+      :id,
+      :slug,
+      :title_translations,
+      :introduction_translations,
+      :configuration_data,
+      :year,
+      :visibility_level,
+      :css,
+      :footer_translations
+    )
+
+    belongs_to :plan_type, unless: :exclude_relationships?
+    attribute :categories_vocabulary_terms, unless: :exclude_relationships? do
+      serialize_terms(object.categories_vocabulary.terms.sorted)
+    end
+    attribute :statuses_vocabulary_terms, unless: :exclude_relationships? do
+      if object.statuses_vocabulary.present?
+        serialize_terms(object.statuses_vocabulary.terms.sorted)
+      end
+    end
+    attribute :projects, unless: :exclude_relationships?
+
+    def projects
+      return if object.nodes.blank?
+
+      serialize_projects(object.nodes)
+    end
+
+    def serialize_projects(nodes_relation)
+      ActiveModelSerializers::SerializableResource.new(
+        nodes_relation,
+        each_serializer: ::GobiertoPlans::ApiNodeSerializer,
+        plan: object,
+        # custom_fields: object.front_available_custom_fields,
+        serialize_for_search_engine: true,
+        include_draft: true
+      ).as_json
+    end
+  end
+end

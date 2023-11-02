b547001486b181d4c694b472e53a06dbbac7f4b5
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-11-02 15:43:23 +0100

    Add GobiertoCommon::Location model for locations caching

---
+++ db/migrate/20231102111229_create_gobierto_common_locations.rb
@@ -0,0 +1,9 @@
+class CreateGobiertoCommonLocations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :locations do |t|
+      t.string :external_id
+      t.jsonb :meta
+      t.string :names, array: true
+    end
+  end
+end

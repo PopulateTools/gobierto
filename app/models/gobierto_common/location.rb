e5eb21dc5ac8773816a0a92ae7be79853d002404
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-11-02 19:20:35 +0100

    Update cached locations with blank external ids

--- app/models/gobierto_common/location.rb
+++ app/models/gobierto_common/location.rb
@@ -25,30 +25,47 @@ module GobiertoCommon
       location = Geocoder.search(name).first
 
       if location.present?
-        existing_location = find_by(external_id: location.place_id.to_s)
+        existing_location = find_by(external_id: location.place_id.to_s) || find_by_meta(location)
         if existing_location.present?
+          if existing_location.external_id.blank?
+            existing_location.update_attribute(:external_id, location.place_id.to_s)
+            existing_location.update_attribute(:meta, metadata(location))
+          end
+
           existing_location.add_name(name)
 
           existing_location
         else
           create(
             names: [name],
-            meta: {
-              lat: location.latitude,
-              lon: location.longitude,
-              city_name: location.city,
-              country_code: location.country_code.upcase,
-              country_name: location.country,
-              types: (location.try(:types) || [location.try(:type)]).compact
-            },
+            meta: metadata(location),
             external_id: location.place_id.to_s
           )
         end
       end
     end
 
+    def self.find_by_meta(location)
+      where(external_id: nil).where("meta @> ?", metadata(location).slice(:lat, :lon, :city_name).to_json).first
+    end
+
+    def self.metadata(location)
+      {
+        lat: location.latitude,
+        lon: location.longitude,
+        city_name: location.city,
+        country_code: location.country_code.upcase,
+        country_name: location.country,
+        types: (location.try(:types) || [location.try(:type)]).compact
+      }
+    end
+
     def add_name(name)
-      names << name
+      if names.is_a?(Array)
+        names << name
+      else
+        self.names = [name]
+      end
       save
     end
   end

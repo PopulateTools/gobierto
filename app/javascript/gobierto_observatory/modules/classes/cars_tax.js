915db2712f05ccce61e83d8e07a522c73ec18e2c
Hugoren Martinako <aumpfbahn@gmail.com>
2023-11-14 18:44:51 +0100

    fix query cars_tax

--- app/javascript/gobierto_observatory/modules/classes/cars_tax.js
+++ app/javascript/gobierto_observatory/modules/classes/cars_tax.js
@@ -1,35 +1,59 @@
 import { ComparisonCard } from "lib/visualizations";
 import { Card } from "./card.js";
+import { getMetadataFields } from "../helpers.js";
 
 export class CarsTaxCard extends Card {
   constructor(divClass, city_id) {
     super(divClass);
 
-    this.placeTaxUrl =
+    // NOTE: the place_id consists of five numbers,
+    // where the first two are the province code, so,
+    // to get the province average we need to filter
+    // for all the elements sharing that numbers.
+    //
+    // Example:
+    // when, city_id = 15500
+    // then the province, place_id BETWEEN 15000 AND 15999
+    var lower = Math.trunc(parseInt(city_id) / 1e3) * 1e3
+    var upper = Math.ceil(parseInt(city_id) / 1e3) * 1e3 - 1
+
+    this.url =
       window.populateData.endpoint +
-      "/datasets/ds-impuestos-turismos-municipal.json?sort_desc_by=date&with_metadata=true&limit=1&filter_by_location_id=" +
-      city_id;
-    this.provinceTaxUrl =
-      window.populateData.endpoint +
-      "/datasets/ds-impuestos-turismos-municipal/average.json?sort_desc_by=date&with_metadata=true&limit=1&filter_by_province_id=" +
-      window.populateData.provinceId;
+      `
+      SELECT cars_taxes::integer as value
+      FROM tasas
+      WHERE
+        place_id = ${city_id}
+        AND year = (SELECT max(year) FROM tasas)
+      UNION
+      SELECT avg(cars_taxes ::integer) as value
+      FROM tasas
+      WHERE
+        place_id BETWEEN ${lower} AND ${upper}
+        AND year = (SELECT max(year) FROM tasas)
+      `;
+
+    this.metadata = window.populateData.endpoint.replace(
+      "data.json?sql=",
+      "datasets/tasas/meta"
+    );
   }
 
   getData() {
-    var placeTax = this.handlePromise(this.placeTaxUrl);
-    var provinceTax = this.handlePromise(this.provinceTaxUrl);
+    var data = this.handlePromise(this.url);
+    var metadata = this.handlePromise(this.metadata);
 
-    Promise.all([placeTax, provinceTax]).then(([placeTax, provinceTax]) => {
-      var valueOne = placeTax.data[0].value;
-      var valueTwo = provinceTax.average;
+    Promise.all([data, metadata]).then(([jsonData, jsonMetadata]) => {
+      var [placeTax, provinceTax] = jsonData.data;
 
-      new ComparisonCard(
-        this.container,
-        placeTax,
-        valueOne,
-        valueTwo,
-        "cars_tax"
-      );
+      var opts = {
+        metadata: getMetadataFields(jsonMetadata),
+        value_1: Number(placeTax.value),
+        value_2: Number(provinceTax.value),
+        cardName: "cars_tax"
+      };
+
+      new ComparisonCard(this.container, jsonData.data, opts);
     });
   }
 }

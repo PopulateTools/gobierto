0f28e36bc332339413cd7a1d8362b518eb53e574
Hugoren Martinako <aumpfbahn@gmail.com>
2024-04-17 11:41:49 +0200

    add plugin windows events

--- app/javascript/lib/shared/modules/appsignal.js
+++ app/javascript/lib/shared/modules/appsignal.js
@@ -1,5 +1,11 @@
 import Appsignal from "@appsignal/javascript"
+import { plugin } from "@appsignal/plugin-window-events"
 
-export default new Appsignal({
+const appsignal = new Appsignal({
   key: process.env.APPSIGNAL_PUSH_API_KEY
-})
+});
+
+// https://docs.appsignal.com/front-end/plugins/plugin-window-events.html
+appsignal.use(plugin())
+
+export default appsignal;

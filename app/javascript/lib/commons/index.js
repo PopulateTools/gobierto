80dc18e9bc6d6b90143ac7e22ef50411a16a2b1a
Hugoren Martinako <aumpfbahn@gmail.com>
2024-05-10 16:58:40 +0200

    hide external deps warnings

--- app/javascript/lib/commons/index.js
+++ app/javascript/lib/commons/index.js
@@ -1,12 +1,8 @@
-import 'regenerator-runtime';
+// Run this files BEFORE everything to avoid JS "imports" hoisting
+import './modules/jquery';
+import './modules/i18n';
 
-// https://rossta.net/blog/importing-images-with-webpacker.html#images-in-rails-views
-require.context('../../images', true)
-
-// LEGACY DEPENDENCIES
-import $ from 'jquery'
-import 'jquery-ujs'
-import * as I18n from 'i18n-js'
+// import 'jquery-ujs' // TODO: possible useless
 
 // NOTE: these modules are required for the legacy views
 import './modules/module-search';
@@ -20,7 +16,3 @@ import './modules/shareContent';
 import './modules/separate-tabs';
 import './modules/check-start-turbolinks';
 import './modules/iframe_handler';
-
-// NOTE: jQuery exposed to global (window for node environment) due to script directly in the view
-global.$ = global.jQuery = $
-global.I18n = I18n

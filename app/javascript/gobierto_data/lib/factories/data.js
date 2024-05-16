39ee601823dedd8f1131977dc437377c8589417c
Hugoren Martinako <aumpfbahn@gmail.com>
2024-05-16 12:48:22 +0200

    remove legacy way to get the token

--- app/javascript/gobierto_data/lib/factories/data.js
+++ app/javascript/gobierto_data/lib/factories/data.js
@@ -1,17 +1,17 @@
 import { baseUrl } from '../commons.js';
-import { getToken } from '../helpers';
 import axios from 'axios';
 
 const endPoint = `${baseUrl}/data.csv`;
-const token = getToken();
-const headers = {
-  Authorization: token
-};
 
 // Data-endpoint factory to get/post/put/delete API data
 export const DataFactoryMixin = {
   methods: {
     getData(params) {
+      const headers = {
+        "Content-type": "application/json",
+        Authorization: window.populateData.token
+      };
+
       const qs = new URLSearchParams(params)
       return axios.get(`${endPoint}?${qs.toString()}`, { headers });
     },
--- app/javascript/gobierto_data/lib/factories/queries.js
+++ app/javascript/gobierto_data/lib/factories/queries.js
@@ -1,31 +1,50 @@
 import { baseUrl } from '../commons.js';
-import { getToken } from '../helpers';
 import axios from 'axios';
 
 const endPoint = `${baseUrl}/queries`;
-const token = getToken();
-const headers = {
-  "Content-type": "application/json",
-  Authorization: token
-};
 
 // Queries-endpoint factory to get/post/put/delete API data
 export const QueriesFactoryMixin = {
   methods: {
     getQuery(id) {
+      const headers = {
+        "Content-type": "application/json",
+        Authorization: window.populateData.token
+      };
+
       return axios.get(`${endPoint}/${id}`, { headers });
     },
     getQueries(params) {
+      const headers = {
+        "Content-type": "application/json",
+        Authorization: window.populateData.token
+      };
+
       const qs = new URLSearchParams(params)
       return axios.get(`${endPoint}?${qs.toString()}`, { headers });
     },
     postQuery(data) {
+      const headers = {
+        "Content-type": "application/json",
+        Authorization: window.populateData.token
+      };
+
       return axios.post(endPoint, data, { headers });
     },
     putQuery(id, data) {
+      const headers = {
+        "Content-type": "application/json",
+        Authorization: window.populateData.token
+      };
+
       return axios.put(`${endPoint}/${id}`, data, { headers });
     },
     deleteQuery(id) {
+      const headers = {
+        "Content-type": "application/json",
+        Authorization: window.populateData.token
+      };
+
       return axios.delete(`${endPoint}/${id}`, { headers });
     },
   }

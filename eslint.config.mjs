c7e78f24a22a583499632b43776083cf61388868
Hugoren Martinako <aumpfbahn@gmail.com>
2024-05-21 12:32:20 +0200

    update eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jquery,
+        ...globals.jest,
+        ...globals.es2020,
+        "Vue": true,
+        "I18n": true,
+        "jest/globals": true,
+        "GobiertoAdmin": true,
+        "User": true,
+        "GobiertoPeople": true,
+        "GobiertoBudgets": true,
+        "GobiertoIndicators": true,
+      }
+    },
+    "rules": {
+      "no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 1 }],
+      "no-multi-spaces": "error",
+      "keyword-spacing": ["error", { "after": true, "before": true }],
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "no-console": "off"
+    }
+  }
+]

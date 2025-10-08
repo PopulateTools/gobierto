85db83e1b200288515b447e3b727624d7f8b8fa2
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-10-08 18:22:16 +0200

    Do not add permissions to assigned resources groups

--- app/forms/concerns/gobierto_admin/permissions_group_helpers.rb
+++ app/forms/concerns/gobierto_admin/permissions_group_helpers.rb
@@ -5,15 +5,17 @@ module GobiertoAdmin
     extend ActiveSupport::Concern
 
     def set_permissions_group(resource, options = {})
-      action_name = options[:action_name] || "manage"
+      action_name = options.key?(:action_name) ? options[:action_name] : "manage"
 
       group = site.admin_groups.find_or_create_by(resource: resource, name: "resource_group_#{resource.class.name}_#{resource.id}", group_type: :system)
 
-      group.permissions.find_or_create_by(
-        namespace: resource.class.name.deconstantize.underscore,
-        resource: resource,
-        action_name: action_name
-      )
+      if action_name.present?
+        group.permissions.find_or_create_by(
+          namespace: resource.class.name.deconstantize.underscore,
+          resource: resource,
+          action_name: action_name
+        )
+      end
 
       yield(group) if block_given?
     end

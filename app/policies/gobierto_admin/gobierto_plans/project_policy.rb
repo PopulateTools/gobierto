10843f0f2ba70fa8f88a1bf9037a8df3a643869b
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-10-10 13:19:41 +0200

    Include update in the admin actions mapping for moderate and publish

--- app/policies/gobierto_admin/gobierto_plans/project_policy.rb
+++ app/policies/gobierto_admin/gobierto_plans/project_policy.rb
@@ -6,13 +6,13 @@ module GobiertoAdmin
       attr_reader :project, :actions_manager
 
       # This constant maps the available controller actions of a project with
-      # the actions defined in the actions manager
+      # the admin actions defined in the actions manager
       ALLOWED_ACTIONS_MAPPING = {
         create_projects: [:index, :new, :create],
         view_projects: [:index, :show],
         edit_projects: [:index, :edit, :update], #, :update_attributes], TODO what happens with update_attributes?
-        moderate_projects: [:index, :edit, :moderate],
-        publish_projects: [:index, :edit, :publish],
+        moderate_projects: [:index, :edit, :moderate, :update],
+        publish_projects: [:index, :edit, :publish, :update],
         delete_projects: [:index, :edit, :destroy],
         manage: [:manage],
         manage_dashboards: [:index_dashboards, :manage_dashboards],

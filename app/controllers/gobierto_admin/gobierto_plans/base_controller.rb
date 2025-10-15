2d6469d1adf435b83def8c5c2beedfaf6829040d
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-10-15 13:30:43 +0200

    Extend use of calculated permissions

--- app/controllers/gobierto_admin/gobierto_plans/base_controller.rb
+++ app/controllers/gobierto_admin/gobierto_plans/base_controller.rb
@@ -6,7 +6,7 @@ module GobiertoAdmin
       before_action { module_enabled!(current_site, "GobiertoPlans") }
       before_action { module_allowed!(current_admin, "GobiertoPlans") }
 
-      helper_method :gobierto_plans_plan_type_preview_url, :current_admin_can_manage_plans?, :current_admin_can_edit_projects?
+      helper_method :gobierto_plans_plan_type_preview_url, :current_admin_can_manage_plans?, :current_admin_can_edit_projects?, :admin_projects_actions
 
       protected
 
@@ -23,6 +23,10 @@ module GobiertoAdmin
           options.merge!(preview_token: current_admin.preview_token)
         end
         gobierto_plans_plan_url(plan.plan_type.slug, plan.year, options)
+      end
+
+      def admin_projects_actions
+        @admin_projects_actions ||= admin_actions_manager.admin_actions(admin: current_admin, resource: @plan.nodes) if @plan
       end
     end
   end

96d7cd76508ba4a17627829579d74b8f326bd18a
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-10-21 15:49:15 +0200

    Define a notification builder for admins associated to trackable concern

---
+++ app/services/gobierto_admin/admin_notification_builder.rb
@@ -0,0 +1,38 @@
+module GobiertoAdmin
+  class AdminNotificationBuilder
+    attr_reader :event_name, :module_name, :subject, :payload, :site, :admin
+
+    def initialize(event)
+      @subject = GlobalID::Locator.locate(event.payload[:gid])
+      @event_name = event.name.split(".").last
+      @module_name = subject.class.name.split("::").first
+      @payload = event.payload
+      @admin = GobiertoAdmin::Admin.find(payload[:admin_id])
+      @site = Site.find(payload[:site_id])
+    end
+
+    def call
+      recipients.each do |recipient|
+        deliver_notification(recipient)
+      end
+    end
+
+    def recipients
+      assigned_users = GobiertoAdmin::Admin.joins(:admin_group_memberships).where(admin_groups_admins: { admin_group_id: GobiertoAdmin::AdminGroup.where(resource: subject) })
+      author = subject.try(:author)
+      return assigned_users.distinct if author.blank?
+
+      assigned_users.or(GobiertoAdmin::Admin.where(id: author.id)).distinct
+    end
+
+    private
+
+    def deliver_notification(recipient)
+      mailer_class.send(event_name, subject, subject.plan, recipient, payload).deliver_later
+    end
+
+    def mailer_class
+      ::GobiertoAdmin.const_get("#{module_name}::AdminMailer")
+    end
+  end
+end

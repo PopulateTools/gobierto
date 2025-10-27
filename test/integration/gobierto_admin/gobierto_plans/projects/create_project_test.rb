afab94b7d1c7383fa0440c0c6a4d7f50d64eee92
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-10-27 14:23:50 +0100

    Refactor fixtures and tests related with plans permissions

--- test/integration/gobierto_admin/gobierto_plans/projects/create_project_test.rb
+++ test/integration/gobierto_admin/gobierto_plans/projects/create_project_test.rb
@@ -100,7 +100,7 @@ module GobiertoAdmin
         def test_regular_editor_creator_admin_create_valid_project
           allow_regular_admin_create_projects
           # The manage allows admin to manage permissions
-          allow_regular_admin_manage_projects
+          allow_regular_admin_edit_all_projects
 
           with_javascript do
             with_signed_in_admin(regular_admin) do

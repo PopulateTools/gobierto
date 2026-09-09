32e77c042216cec0a96d6a1d12d1c0fb07cff44c
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2026-09-09 17:46:35 +0200

    Change serialization of custom fields vocabulary_terms

--- app/serializers/gobierto_common/custom_field_serializer.rb
+++ app/serializers/gobierto_common/custom_field_serializer.rb
@@ -9,13 +9,9 @@ module GobiertoCommon
     def vocabulary_terms
       return unless object.has_vocabulary?
 
-      if object.vocabulary.present?
-      serialize_terms(object.vocabulary.terms.sorted)
-      elsif object.vocabularies.present?
-        object.vocabularies.map do |vocabulary|
-          serialize_terms(vocabulary.terms.sorted)
-        end
-      end.flatten
+      object.vocabularies.flat_map do |vocabulary|
+        serialize_terms(vocabulary.terms.sorted)
+      end
     end
   end
 end
